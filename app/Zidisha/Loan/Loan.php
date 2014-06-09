5cccd2a9738785e174c57839b555185725529ac8
SingularityGuy <hardikformail@gmail.com>
2014-06-09 12:23:00 +0530

    add loan status to Loan model

--- app/Zidisha/Loan/Loan.php
+++ app/Zidisha/Loan/Loan.php
@@ -7,4 +7,12 @@ use Zidisha\Loan\Base\Loan as BaseLoan;
 class Loan extends BaseLoan
 {
 
+    const OPEN      = 0;
+    const FUNDED    = 1;
+    const ACTIVE    = 2;
+    const REPAID    = 3;
+    const DEFAULTED = 5;
+    const CANCELED  = 6;
+    const EXPIRED   = 7;
+
 }
