64f90f02e25d0db83d9105511271a0706bda9a81
Jonas De Taeye <jonasdt@fastmail.fm>
2014-06-10 15:28:55 +0530

    Remove custom alert message

--- app/views/auth/login.blade.php
+++ app/views/auth/login.blade.php
@@ -3,18 +3,6 @@
 @section('content')
 <div class="row">
     <div class="col-md-offset-3 col-md-6">
-        @if(Session::has('error'))
-        <div class="alert alert-danger">
-            {{ Session::get('error') }}
-        </div>
-        @endif
-
-        @if(Session::has('success'))
-        <div class="alert alert-success">
-            {{ Session::get('success') }}
-        </div>
-        @endif
-
         <div style="text-align: center">
             <a href="{{$facebookLoginUrl}}" class="btn btn-lg btn-primary">Login with facebook</a>
         </div>
