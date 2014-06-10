a3dab86b7dd59236d8142eef90575aec58f68322
Jonas De Taeye <jonasdt@fastmail.fm>
2014-06-10 15:31:34 +0530

    Use BootstrapForm on join page

---
+++ app/lang/en/join.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'form' => [
+        'username' => 'Display Username',
+        'email' => 'Email',
+        'password' => 'Password',
+        'password_confirmation' => 'Confirm Password',
+        'submit' => 'Submit',
+    ]
+];
--- app/views/auth/join.blade.php
+++ app/views/auth/join.blade.php
@@ -3,43 +3,19 @@
 @section('content')
 <div class="row">
     <div class="col-md-offset-3 col-md-6">
-        @if(Session::has('error'))
-        <div class="alert alert-danger">
-            {{ Session::get('error') }}
-        </div>
-        @endif
-
         <div style="text-align: center">
             <a href="{{$facebookJoinUrl}}" class="btn btn-lg btn-primary">Join with facebook</a>
         </div>
 
-        {{ Form::open(array('url' => 'join')) }}
-        <div class="form-group">
-            {{ Form::label('username', 'Display Username') }}
-            {{ Form::text('username', null, array('class' => 'form-control')) }}
-            {{ $errors->first('username', '<div class="alert alert-danger">:message</div>') }}
-        </div>
+        {{ BootstrapForm::open(array('url' => 'join', 'translationDomain' => 'join.form')) }}
+        
+        {{ BootstrapForm::text('username') }}
+        {{ BootstrapForm::text('email') }}
+        {{ BootstrapForm::password('password') }}
+        {{ BootstrapForm::password('password_confirmation') }}
+        {{ BootstrapForm::submit('submit') }}
 
-        <div class="form-group">
-            {{ Form::label('email', 'Email') }}
-            {{ Form::text('email', null, array('class' => 'form-control')) }}
-            {{ $errors->first('email', '<div class="alert alert-danger">:message</div>') }}
-        </div>
-
-        <div class="form-group">
-            {{ Form::label('password', 'Password') }}
-            {{ Form::password('password', array('class' => 'form-control')) }}
-            {{ $errors->first('password',  '<div class="alert alert-danger">:message</div>') }}
-        </div>
-
-        <div class="form-group">
-            {{ Form::label('password_confirmation', 'Confirm Password') }}
-            {{ Form::password('password_confirmation', array('class' => 'form-control')) }}
-        </div>
-
-        <button type="submit" class="btn btn-default">Submit</button>
-
-        {{ Form::close() }}
+        {{ BootstrapForm::close() }}
 
         <div>
             {{ link_to_route('login', 'login' ) }}
