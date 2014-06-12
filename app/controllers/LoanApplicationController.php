cc5382b35fc457473e4af3305d366a460fea7a38
Jonas De Taeye <jonasdt@fastmail.fm>
2014-06-12 13:46:28 +0530

    Add StepController Trait

--- app/controllers/LoanApplicationController.php
+++ app/controllers/LoanApplicationController.php
@@ -3,69 +3,26 @@ use Zidisha\Borrower\Form\Loan\Profile;
 
 class LoanApplicationController extends BaseController
 {
+    use StepController;
 
-    protected $steps = array();
+    protected $steps = [
+        'instructions',
+        'profile',
+        'application',
+        'publish',
+    ];
 
     private $editForm;
 
     public function __construct(Profile $form )
     {
-        $this->beforeFilter('@validateSteps');
+        $this->beforeFilter('@stepsBeforeFilter');
         $this->editForm = $form;
-    }
-    
-    protected function getStepFromRoute($route)
-    {
-        $controllerMethod = $route->getAction()['controller'];
-        list($controller, $method) = explode('@', $controllerMethod);
-        
-        return preg_replace('/^(get|post)_/', '', snake_case($method));
-    }
-    
-    public function validateSteps($route)
-    {
-        $steps = [
-            'instructions',
-            'profile',
-            'application',
-            'publish',
-        ];
-        
-        $routeStep = $this->getStepFromRoute($route);
-        
-        $currentStep = Session::get('loanapplication.currentStep', $steps[0]);
-
-        $state = 'complete';
-        $this->steps = array();
-        foreach ($steps as $step) {
-            if ($step == $routeStep) {
-                if ($state == 'disabled') {
-                    Flash::error('Not Allowed');
-                    return Redirect::action('LoanApplicationController@getInstructions');
-                }
-                $class = 'active';
-                $state ='disabled';
-            }
-            if ($step == $currentStep && $state != 'disabled') {
-                $class = 'active';
-                $state ='disabled';
-            } elseif ($step != $routeStep) {
-                $class = $state;
-            }
-            $this->steps[$step] = ['class' => $class];
-        }
-        
-        $this->setCurrentStep($routeStep);
-    }
-    
-    protected function setCurrentStep($step)
-    {
-        Session::set('loanapplication.currentStep', $step);
     }
 
     protected function stepView($step, $params = array())
     {
-        return View::make("borrower.loan.$step", ['steps' => $this->steps] + $params);
+        return View::make("borrower.loan.$step", ['steps' => $this->stepsData] + $params);
     }
 
     public function getInstructions()
@@ -82,8 +39,6 @@ class LoanApplicationController extends BaseController
 
     public function getProfile()
     {
-
-
         return $this->stepView('profile', ['form' => $this->editForm,]);
     }
 
---
+++ app/controllers/StepController.php
@@ -0,0 +1,78 @@
+<?php
+
+use Illuminate\Routing\Route;
+
+trait StepController
+{
+    protected $stepsData = [];
+    protected $stepsSessionKey = '';
+    
+    protected function getStepFromRoute(Route $route)
+    {
+        $controllerMethod = $route->getAction()['controller'];
+        list($controller, $method) = explode('@', $controllerMethod);
+
+        return preg_replace('/^(get|post)_/', '', snake_case($method));
+    }
+
+    protected function stepSessionKey(Route $route)
+    {
+        if (!$this->stepsSessionKey) {
+            $controller = get_class();
+            $controller = substr($controller, 0, strlen($controller) - 10);
+
+            $this->stepsSessionKey = str_replace('_', '-', snake_case($controller));
+        }
+    }
+    
+    protected function getStepsSession($key, $default = null) {
+        return Session::get($this->stepsSessionKey . '.' . $key, $default);
+    }
+    
+    protected function putStepsSession($key, $value) {
+        return Session::put($this->stepsSessionKey . '.' . $key, $value);
+    }
+    
+    protected function getCurrentStep() {
+        return $this->getStepsSession('currentStep', $this->steps[0]);
+    }
+
+    protected function setCurrentStep($step)
+    {
+        $this->putStepsSession('currentStep', $step);
+    }
+    
+    protected function stepNotAllowed() {
+        $method = camel_case('get_' . $this->steps[0]);
+        return Redirect::action(get_class() . '@' . $method);
+    }
+
+    public function stepsBeforeFilter(Route $route)
+    {
+        $routeStep = $this->getStepFromRoute($route);
+        $this->stepSessionKey($route);
+        
+        $currentStep = $this->getCurrentStep();
+        
+        $state = 'complete';
+        $this->stepsData = [];
+        foreach ($this->steps as $step) {
+            if ($step == $routeStep) {
+                if ($state == 'disabled') {
+                    return $this->stepNotAllowed();
+                }
+                $class = 'active';
+                $state ='disabled';
+            }
+            if ($step == $currentStep && $state != 'disabled') {
+                $class = 'active';
+                $state ='disabled';
+            } elseif ($step != $routeStep) {
+                $class = $state;
+            }
+            $this->stepsData[$step] = ['class' => $class];
+        }
+
+        $this->setCurrentStep($routeStep);
+    }
+}
