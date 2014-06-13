c7c3bd892afd8d39b7b07be54e61f63557abae26
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-06-13 11:00:51 +0530

    Add auth service class

---
+++ app/Zidisha/Auth/AuthService.php
@@ -0,0 +1,19 @@
+<?php
+namespace Zidisha\Auth;
+
+
+use Zidisha\User\User;
+
+class AuthService
+{
+    public function login(User $user)
+    {
+        \Auth::login($user);
+    }
+
+
+    public function attempt($credentials, $rememberMe)
+    {
+        return Auth::attempt($credentials, $rememberMe);
+    }
+} 
