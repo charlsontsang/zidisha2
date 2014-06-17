56cf5a7a0a94ec8b87ab6aa54f6d19bf4bd326c3
SingularityGuy <hardikformail@gmail.com>
2014-06-17 20:22:20 +0530

    Add loan-bid model

---
+++ app/Zidisha/Loan/BidQuery.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Zidisha\Loan;
+
+use Zidisha\Loan\Base\BidQuery as BaseBidQuery;
+
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'loan_bids' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class BidQuery extends BaseBidQuery
+{
+
+} // BidQuery
