bd1c15d7f9b4d88e8b7871058e3e0df6e5db920b
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-06-17 10:55:10 +0530

    Add localisation file for comments

---
+++ app/lang/en/comments.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'flash' => [
+        'post' => 'Your comments was posted',
+        'edit' => 'Your comment was edited',
+        'reply' => 'Your reply was posted',
+        'delete' => 'Your comment was deleted',
+        'translate' => 'Your comment was translated'
+    ],
+    'actions' => [
+        'edit' => 'Edit',
+        'delete' => 'Delete',
+        'translate' => 'Translate',
+        'reply' => 'Reply',
+        'share' => 'Share',
+        'show-original' => 'Display posting in original language',
+        'translated-by' => 'Translated by'
+    ],
+];
