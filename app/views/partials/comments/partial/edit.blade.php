f75a52715bbb97746a1413db883e7c9e0000c46d
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-06-17 11:05:44 +0530

    Show original message when editing a comment

--- app/views/partials/comments/partial/edit.blade.php
+++ app/views/partials/comments/partial/edit.blade.php
@@ -1,7 +1,7 @@
 <div class="comment-form" data-comment-action="edit" style="display: none;">
 {{ BootstrapForm::open(array('route' => 'comment:edit', 'translationDomain' => 'comments')) }}
 
-{{ BootstrapForm::textarea('message') }}
+{{ BootstrapForm::textarea('message', $comment->getMessage()) }}
 {{ BootstrapForm::hidden('comment_id', $comment->getId()) }}
 {{ BootstrapForm::submit('edit') }}
 
