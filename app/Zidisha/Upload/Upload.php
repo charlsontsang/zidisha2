c78edb08414cbec423a76797f9b08392a9665a1f
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-06-18 15:08:50 +0530

    Add resize method in upload model

--- app/Zidisha/Upload/Upload.php
+++ app/Zidisha/Upload/Upload.php
@@ -2,9 +2,12 @@
 
 namespace Zidisha\Upload;
 
+use Config;
+use Illuminate\Filesystem\Filesystem;
 use Propel\Runtime\Connection\ConnectionInterface;
 use Symfony\Component\HttpFoundation\File\File;
 use Zidisha\Upload\Base\Upload as BaseUpload;
+use Zidisha\Upload\Exceptions\ConfigurationNotFoundException;
 
 class Upload extends BaseUpload
 {
@@ -17,21 +20,35 @@ class Upload extends BaseUpload
         return $this->getType() == 'image' ? true : false;
     }
 
-    public function getUrl()
+    public function getImageUrl()
+    {
+        $file = new Filesystem();
+        if ($file->exists(public_path() . '/uploads/cache/' . $this->getFilename())) {
+            return asset('uploads/cache/' . $this->getFilename());
+        } else {
+            return route('image:resize', ['upload_id' => $this->getId(), 'format' => 'small_profile_pic']);
+        }
+    }
+
+    public function getFileUrl()
     {
         return asset('uploads/' . $this->getFilename());
     }
 
     public function getPath()
     {
+        return $this->getBasePath() . $this->getFilename();
+    }
+
+    protected function getBasePath()
+    {
         return public_path() . '/uploads/';
     }
 
     public function postDelete(ConnectionInterface $con = null)
     {
-        $filename = $this->getFilename();
-        $file = new \Illuminate\Filesystem\Filesystem();
-        $file->delete($this->getPath() . $filename);
+        $file = new Filesystem();
+        $file->delete($this->getPath());
         return true;
     }
 
@@ -62,7 +79,7 @@ class Upload extends BaseUpload
 
     public function postSave(ConnectionInterface $con = null)
     {
-        $this->file = $this->file->move($this->getPath(), $this->getFilename());
+        $this->file = $this->file->move($this->getBasePath(), $this->getFilename());
     }
 
     public function getFile()
@@ -72,5 +89,48 @@ class Upload extends BaseUpload
         }
 
         return $this->file;
+    }
+
+    public function resize($format)
+    {
+        $file = new Filesystem();
+        $cachePath = $this->getCachePath($format);
+        if (!$file->exists($cachePath)) {
+            $img = \Image::make($this->getPath());
+
+            if (!Config::get('image.formats.' . $format)) {
+                throw new ConfigurationNotFoundException();
+            }
+
+            $img->resize(
+                Config::get("image.formats.$format.width"),
+                Config::get("image.formats.$format.height"),
+                function ($constraint) {
+                    $constraint->aspectRatio();
+                }
+            );
+
+            $file->makeDirectory($this->getCacheBasePath($format), 0755, true);
+            $img->save($cachePath);
+        }
+
+        return new File($cachePath);
+    }
+
+    protected function getCachePath($format)
+    {
+        if (!Config::get('image.formats.' . $format)) {
+            throw new ConfigurationNotFoundException();
+        }
+
+        return public_path() . '/uploads/cache/' . $format . '/' . $this->getFilename();
+    }
+
+    protected function getCacheBasePath($format)
+    {
+        if (!Config::get('image.formats.' . $format)) {
+            throw new ConfigurationNotFoundException();
+        }
+        return public_path() . '/uploads/cache/' . $format . '/';
     }
 }
