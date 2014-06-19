4be51552e810f2ce10980b79ce3089ac4b4b6514
SingularityGuy <hardikformail@gmail.com>
2014-06-19 16:51:14 +0530

    Add settings model for admin

---
+++ app/Zidisha/Admin/Setting.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Zidisha\Admin;
+
+use Zidisha\Admin\Base\Setting as BaseSetting;
+
+class Setting extends BaseSetting
+{
+
+}
---
+++ app/Zidisha/Admin/SettingQuery.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Zidisha\Admin;
+
+use Zidisha\Admin\Base\SettingQuery as BaseSettingQuery;
+
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'settings' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SettingQuery extends BaseSettingQuery
+{
+
+} // SettingQuery
