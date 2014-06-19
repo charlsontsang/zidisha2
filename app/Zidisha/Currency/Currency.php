b89cd82efd2f6dd619e5de876fec1b5453b3255d
SingularityGuy <hardikformail@gmail.com>
2014-06-19 15:44:13 +0530

    add query and model files to new tables

---
+++ app/Zidisha/Currency/Currency.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Zidisha\Currency;
+
+use Zidisha\Currency\Base\Currency as BaseCurrency;
+
+class Currency extends BaseCurrency
+{
+
+}
---
+++ app/Zidisha/Currency/ExchangeRate.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Zidisha\Currency;
+
+use Zidisha\Currency\Base\ExchangeRate as BaseExchangeRate;
+
+class ExchangeRate extends BaseExchangeRate
+{
+
+}
