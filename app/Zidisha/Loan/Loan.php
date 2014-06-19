c483f9cedf0e1099cba6b51f0b82368fc87ae4a1
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-06-19 16:04:59 +0530

    Update getters and setters methods

--- app/Zidisha/Loan/Loan.php
+++ app/Zidisha/Loan/Loan.php
@@ -2,6 +2,8 @@
 
 namespace Zidisha\Loan;
 
+use SupremeNewMedia\Finance\Core\Currency;
+use SupremeNewMedia\Finance\Core\Money;
 use Zidisha\Loan\Base\Loan as BaseLoan;
 
 class Loan extends BaseLoan
@@ -15,4 +17,33 @@ class Loan extends BaseLoan
     const CANCELED  = 6;
     const EXPIRED   = 7;
 
+    public function getAmount()
+    {
+        return Money::valueOf(parent::getAmount(), Currency::valueOf($this->getCurrencyCode()));
+    }
+
+    public function setAmount(Money $money)
+    {
+        parent::setAmount($money->getAmount());
+    }
+
+    public function getInstallmentAmount()
+    {
+        return Money::valueOf(parent::getInstallmentAmount(), Currency::valueOf($this->getCurrencyCode()));
+    }
+
+    public function setInstallmentAmount(Money $money)
+    {
+        parent::setInstallmentAmount($money->getAmount());
+    }
+
+    public function getUsdAmount()
+    {
+        return Money::valueOf(parent::getUsdAmount(), Currency::valueOf(Currency::CODE_USD));
+    }
+
+    public function setUsdAmount(Money $money)
+    {
+        parent::setUsdAmount($money->getAmount());
+    }
 }
