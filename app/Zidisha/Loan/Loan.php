d99d4bb67f12364e6709032751811cf5d5caf775
Jonas De Taeye <jonasdt@fastmail.fm>
2014-06-24 19:09:35 +0530

    Improve Loan::calculateExtraDays

--- app/Zidisha/Loan/Loan.php
+++ app/Zidisha/Loan/Loan.php
@@ -2,6 +2,7 @@
 
 namespace Zidisha\Loan;
 
+use Carbon\Carbon;
 use Zidisha\Currency\Money;
 use Zidisha\Loan\Base\Loan as BaseLoan;
 
@@ -76,17 +77,20 @@ class Loan extends BaseLoan
 
     public function calculateExtraDays($disbursedDate)
     {
-        $installmentDay = $this->getInstallmentDay();
-        $day = $disbursedDate->format('d');
-
-        if ($day <= $installmentDay) {
-            $diff = $installmentDay - $day;
+        $date = Carbon::instance($disbursedDate);
+        
+        if ($this->isWeeklyInstallment()) {
+            $extraDays = ($this->getInstallmentDay() - $date->dayOfWeek + 7) % 7;
         } else {
-            $next = date("Y-m-$installmentDay", strtotime("+1 months"));
-            $diff = date('d', strtotime($next) - strtotime($day));
+            if ($date->day > $this->getInstallmentDay()) {
+                // TODO installmentDay = 15, date = jan 30 => march 2 => march 15 => extra days > month
+                $date->addMonth();
+            }
+            $date->day($this->getInstallmentDay());
+            $extraDays = $date->diffInDays(Carbon::instance($disbursedDate));
         }
-
-        return $this->setExtraDays($diff);
+        
+        return $this->setExtraDays($extraDays);
     }
 
     public function calculateInstallmentCount(Money $nativeInstallmentAmount)
