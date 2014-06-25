ec532fa353b7d2f1fceb70e51be83fb80ddc736b
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-06-25 14:59:23 +0530

    Add Paypal Transactions

---
+++ app/Zidisha/Vendor/Paypal/PaypalTransaction.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Zidisha\Vendor\Paypal;
+
+use Zidisha\Vendor\Paypal\Base\PaypalTransaction as BasePaypalTransaction;
+
+class PaypalTransaction extends BasePaypalTransaction
+{
+
+}
