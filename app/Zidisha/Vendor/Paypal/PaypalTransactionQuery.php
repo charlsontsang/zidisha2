a4af7ceb37cd48c124ae0d01232af04a955021c9
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-06-25 14:59:23 +0530

    Add Paypal Transactions

---
+++ app/Zidisha/Vendor/Paypal/PaypalTransactionQuery.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Zidisha\Vendor\Paypal;
+
+use Zidisha\Vendor\Paypal\Base\PaypalTransactionQuery as BasePaypalTransactionQuery;
+
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'paypal_transactions' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class PaypalTransactionQuery extends BasePaypalTransactionQuery
+{
+
+} // PaypalTransactionQuery
