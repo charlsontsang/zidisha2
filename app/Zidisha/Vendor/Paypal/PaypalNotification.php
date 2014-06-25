f6b9cd90ccd597eb658dca347b8cbbdadc515e9b
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-06-25 14:59:02 +0530

    Add Paypal Notifications

---
+++ app/Zidisha/Vendor/Paypal/PaypalNotification.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Zidisha\Vendor\Paypal;
+
+use Zidisha\Vendor\Paypal\Base\PaypalNotification as BasePaypalNotification;
+
+class PaypalNotification extends BasePaypalNotification
+{
+
+}
