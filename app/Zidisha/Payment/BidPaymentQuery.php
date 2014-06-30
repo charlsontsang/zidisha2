d0a38afa31d86a114aa1ac6e29ed1da0d31fc0d8
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-06-30 18:29:47 +0530

    Add bid payment propel model

---
+++ app/Zidisha/Payment/BidPaymentQuery.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Zidisha\Payment;
+
+use Zidisha\Payment\Base\BidPaymentQuery as BaseBidPaymentQuery;
+
+
+/**
+ * Skeleton subclass for representing a query for one of the subclasses of the 'payments' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class BidPaymentQuery extends BaseBidPaymentQuery
+{
+
+} // BidPaymentQuery
