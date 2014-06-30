27852951035169ab3f274aa435f9a517310d7b9d
SingularityGuy <hardikformail@gmail.com>
2014-06-30 19:39:42 +0530

    Form in Blade file

--- app/views/lender/gift-cards-terms.blade.php
+++ app/views/lender/gift-cards-terms.blade.php
@@ -26,8 +26,101 @@ Join the global P2P microlending movement
     website
     .</p>
 
-<a href="{{ route('lender:gift-cards:terms-accept') }}" class="btn btn-primary">
+
+{{ BootstrapForm::open(array('route' => 'lender:gift-cards:terms-accept', 'translationDomain' => 'fund', 'id' => 'funds-upload')) }}
+
+@if($enoughBalance == 0)
+
+{{ BootstrapForm::text('amount', null, ['id' => 'amount']) }}
+{{ BootstrapForm::text('donationAmount', null, ['id' => 'donation-amount']) }}
+
+{{ BootstrapForm::hidden('transactionFee', null, ['id' => 'transaction-fee-amount']) }}
+{{ BootstrapForm::hidden('transactionFeeRate', null, ['id' => 'fee-amount-rate']) }}
+{{ BootstrapForm::hidden('totalAmount', null, ['id' => 'total-amount']) }}
+
+{{ BootstrapForm::hidden('stripeToken', null, ['id' => 'stripe-token']) }}
+{{ BootstrapForm::hidden('paymentMethod', null, ['id' => 'payment-method']) }}
+
+{{ BootstrapForm::label("Payment Transfer Cost") }}:
+USD <span id="fee-amount-display"></span>
+
+<br/>
+
+{{ BootstrapForm::label("Total amount to be charged to your account") }}
+USD <span id="total-amount-display"></span>
+
+<br/>
+<button id="stripe-payment" class="btn btn-primary">Pay With Card</button>
+<input type="submit" id="paypal-payment" class="btn btn-primary" value="Pay With Paypal" name="submit_paypal">
+
+
+@else
+<a class="btn btn-primary">
     Accept and Continue
 </a>
+@endif
+{{ BootstrapForm::close() }}
+@stop
 
+
+@section('script-footer')
+<script src="https://checkout.stripe.com/checkout.js"></script>
+<script type="text/javascript">
+    $(function () {
+
+        var handler = StripeCheckout.configure({
+            key: "{{ \Config::get('stripe.public_key') }}",
+            token: function (token, args) {
+                $("#stripe-token").val(token.id);
+                $("#payment-method").val("stripe");
+                $('#funds-upload').submit();
+            }
+        });
+        $(function () {
+            $('#stripe-payment').click(function (e) {
+                handler.open({
+                    name: 'Zidisha',
+                    description: 'Payment to Zidisha',
+                    amount: parseFloat($("#total-amount").val()) * 100,
+                    email: "{{ \Auth::user()->getEmail() }}",
+                    panelLabel: "Pay @{{amount}}"
+                });
+                e.preventDefault();
+            });
+        });
+
+        var $donationAmount = $('#donation-amount'),
+            $amount = $('#amount'),
+            $transactionFeeAmount = $('#transaction-fee-amount'),
+            $totalAmount = $('#total-amount'),
+            $transactionFeeAmountDisplay = $('#fee-amount-display'),
+            $totalAmountDisplay = $('#total-amount-display'),
+            feePercentage = Number($('#fee-amount-rate').val());
+
+        function parseMoney(value) {
+            return Number(value.replace(/[^0-9\.]+/g, ""));
+        }
+
+        function formatMoney(value) {
+            return value.toFixed(2);
+        }
+
+        function calculateAmounts() {
+            var donationAmount = parseMoney($donationAmount.val()),
+                amount = parseMoney($amount.val()),
+                transactionFeeAmount = amount * feePercentage,
+                totalAmount = amount + transactionFeeAmount + donationAmount;
+
+            $transactionFeeAmount.val(formatMoney(transactionFeeAmount));
+            $totalAmount.val(formatMoney(totalAmount));
+            $transactionFeeAmountDisplay.text(formatMoney(transactionFeeAmount));
+            $totalAmountDisplay.text(formatMoney(totalAmount));
+        }
+
+        $donationAmount.on('keyup', calculateAmounts);
+        $amount.on('keyup', calculateAmounts);
+
+        calculateAmounts();
+    });
+</script>
 @stop
