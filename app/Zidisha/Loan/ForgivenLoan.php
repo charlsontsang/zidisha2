939e2acdb6a4340f36ea44a0ab074ba1b9c2edc8
Jonas De Taeye <jonasdt@fastmail.fm>
2014-06-30 12:03:05 +0530

    Add forgivenLoan model

---
+++ app/Zidisha/Loan/ForgivenLoan.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Zidisha\Loan;
+
+use Zidisha\Loan\Base\ForgivenLoan as BaseForgivenLoan;
+
+class ForgivenLoan extends BaseForgivenLoan
+{
+
+}
---
+++ app/Zidisha/Loan/ForgivenLoanQuery.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Zidisha\Loan;
+
+use Zidisha\Currency\Money;
+use Zidisha\Loan\Base\ForgivenLoanQuery as BaseForgivenLoanQuery;
+
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'forgiven_loans' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class ForgivenLoanQuery extends BaseForgivenLoanQuery
+{
+    public function getTotalAmount()
+    {
+        $total = $this
+            ->select(array('total'))
+            ->withColumn('SUM(amount)', 'total')
+            ->findOne();
+
+        return Money::create($total, 'USD');
+    }
+
+    public function getNativeTotalAmount()
+    {
+        $total = $this
+            ->select(array('total'))
+            ->withColumn('SUM(native_amount)', 'total')
+            ->findOne();
+
+        // Todo currency
+        return Money::create($total, 'USD');
+    }
+} // ForgivenLoanQuery
