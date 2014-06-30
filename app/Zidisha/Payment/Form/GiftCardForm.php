1d89664a432308e108a909c55276d0b4d7e5e7d0
SingularityGuy <hardikformail@gmail.com>
2014-06-30 19:39:06 +0530

    Payment form for GiftCard

---
+++ app/Zidisha/Payment/Form/GiftCardForm.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Zidisha\Payment\Form;
+
+
+use Zidisha\Currency\Money;
+use Zidisha\Payment\GiftCardPayment;
+
+class GiftCardForm extends AbstractPaymentForm
+{
+
+    public function getPayment()
+    {
+
+        if (!\Auth::user()) {
+            \App::abort(404, 'Fatal Error');
+        }
+
+        $lender = \Auth::user()->getLender();
+
+        $data = $this->getData();
+
+        $giftCardPayment = new GiftCardPayment();
+        $giftCardPayment
+            ->setAmount(Money::create($data['amount']))
+            ->setDonationAmount(Money::create($data['donationAmount']))
+            ->setTransactionFee(Money::create($data['transactionFee']))
+            ->setTotalAmount(Money::create($data['totalAmount']))
+            ->setInterestRate($data['interestRate'])
+            ->setLender($lender);
+
+        return $giftCardPayment;
+    }
+} 
