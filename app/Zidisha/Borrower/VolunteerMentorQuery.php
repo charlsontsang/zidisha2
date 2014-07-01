6e8dcd12f99edd216b3aa52320b7a051b2d5d8ee
SingularityGuy <hardikformail@gmail.com>
2014-07-01 21:36:33 +0530

    Add validation in borrower join form

--- app/Zidisha/Borrower/VolunteerMentorQuery.php
+++ app/Zidisha/Borrower/VolunteerMentorQuery.php
@@ -2,7 +2,10 @@
 
 namespace Zidisha\Borrower;
 
+use Propel\Runtime\Propel;
+use Zidisha\Balance\Map\TransactionTableMap;
 use Zidisha\Borrower\Base\VolunteerMentorQuery as BaseVolunteerMentorQuery;
+use Zidisha\Country\CountryQuery;
 
 
 /**
@@ -17,5 +20,44 @@ use Zidisha\Borrower\Base\VolunteerMentorQuery as BaseVolunteerMentorQuery;
  */
 class VolunteerMentorQuery extends BaseVolunteerMentorQuery
 {
+
+    public function getVolunteerMentorCity()
+    {
+        $countryCode = \Session::get('BorrowerJoin.countryCode');
+        $country = CountryQuery::create()
+            ->filterByCountryCode($countryCode)
+            ->findOne();
+
+        $con = Propel::getWriteConnection(TransactionTableMap::DATABASE_NAME);
+        $sql = "SELECT DISTINCT city FROM borrower_profiles WHERE borrower_id IN "
+            . "(SELECT borrower_id FROM volunteer_mentor WHERE country_id = :country_id AND status = :status
+            AND mentee_count < :mentee_count)";
+        $stmt = $con->prepare($sql);
+        //TODO to make mentee_count = 50
+        $stmt->execute(array(':country_id' => $country->getId(), ':status' => '1', ':mentee_count' => '25'));
+        $cities = $stmt->fetchAll(\PDO::FETCH_COLUMN);
+        return array_combine($cities, $cities);
+    }
+
+    public function getVolunteerMentorByCity($city)
+    {
+        $list = [];
+        $volunteerMentors = VolunteerMentorQuery::create()
+            ->filterByStatus(1)
+            ->filterByMenteeCount(array('max' => '25'))
+            ->useBorrowerVolunteerQuery()
+            ->useProfileQuery()
+            ->filterByCity($city)
+            ->endUse()
+            ->endUse()
+            ->joinWith('VolunteerMentor.BorrowerVolunteer')
+            ->find();
+
+        foreach ($volunteerMentors as $volunteerMentor) {
+            $list[$volunteerMentor->getBorrowerId()] = $volunteerMentor->getBorrowerVolunteer()->getName();
+        }
+
+        return $list;
+    }
 
 } // VolunteerMentorQuery
