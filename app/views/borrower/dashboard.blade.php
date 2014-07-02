947ce6d20e2183510011f87ea44d21346371213f
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-07-02 15:51:01 +0530

    Add a verification reminder in borrower dashboard

--- app/views/borrower/dashboard.blade.php
+++ app/views/borrower/dashboard.blade.php
@@ -9,6 +9,12 @@ Dashboard
     <h1>Dashboard</h1>
 </div>
 
+@if(!$verified)
+    <div class="alert alert-warning">
+        Your email is not verified. Please verify your email. Click {{ link_to_route('borrower:resend:verification', 'here') }} to resend your verification mail.
+    </div>
+@endif
+
 <h2>Dashboard for borrowers</h2>
 
 @stop
