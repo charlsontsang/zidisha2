5ab5c7f9586decc0ccdc8d739994f9f5736a2b4d
SingularityGuy <hardikformail@gmail.com>
2014-07-03 17:35:07 +0530

    small fixes

--- app/views/emails/borrower/join-confirmation.blade.php
+++ app/views/emails/borrower/join-confirmation.blade.php
@@ -1,9 +1,13 @@
-
-
-'Dear {{ $borrower->getName() }}, <br/><br/>Thank you for your application to join Zidisha.<br/><br/>A Zidisha staff member will
+Dear {{ $borrower->getName() }},
+<br/><br/>
+Thank you for your application to join Zidisha.
+<br/><br/>
+A Zidisha staff member will
 now review your account, a process that normally takes up to one week.  You will be notified by email when the review is
-complete.  You may also log in to Zidisha to check the status of your account at any time.<br/><br/>
+complete.  You may also log in to Zidisha to check the status of your account at any time.
+<br/><br/>
 
-Regards,<br/><br/>
+Regards,
+<br/><br/>
 
-The Zidisha Team';
+The Zidisha Team
--- app/views/emails/borrower/volunteer-mentor-notify.blade.php
+++
@@ -1,9 +0,0 @@
-
-
-"Dear {{ $borrower->getVolunteerMentor()->getBorrowerVolunteer()->getName() }},<br/><br/>
-{{ $borrower->getName() }} has applied to join Zidisha and has selected you as a Volunteer Mentor. We encourage you to review
-{{ $borrower->getName() }}'s profile here: <li><a href="{{ route('borrower:public-profile',
-$borrower->getUser()->getUsername()) }}">View</a></li><br/><br/>
-If you have any concerns about the information {{ $borrower->getVolunteerMentor()->getBorrowerVolunteer()->getName() }} has provided, please let us know by replying to this email.<br/><br/>
-Thank you,<br/><br/>
-The Zidisha Team";
