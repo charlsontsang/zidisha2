744233f1346ccefa9327138d1fd258c8dbf835d1
Jonas De Taeye <jonasdt@fastmail.fm>
2014-07-04 13:30:57 +0530

    Add loanValidator

---
+++ app/Zidisha/Borrower/Form/Validator/LoanValidator.php
@@ -0,0 +1,30 @@
+<?php
+namespace Zidisha\Borrower\Form\Validator;
+
+use Illuminate\Validation\Validator;
+
+class LoanValidator extends Validator
+{
+    public function validateGreaterThan($attribute, $value, $parameters)
+    {
+        return $value > $parameters[0];
+    }
+
+    protected function replaceGreaterThan($message, $attribute, $rule, $parameters)
+    {
+        return $attribute . ' should be greater than zero.';
+    }
+    
+    public function validateMinimumInstallmentAmount($attribute, $value, $parameters)
+    {
+        return array_get($this->data, 'amount') / $value <= $parameters[0];
+    }
+
+    protected function replaceMinimumInstallmentAmount($message, $attribute, $rule, $parameters)
+    {
+        $maximumPeriod = $parameters[0];
+        $period = $parameters[1];
+        
+        return "Please increase the installment amount until the repayment period is at maximum $maximumPeriod $period";
+    }
+}
