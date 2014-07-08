2411136f3afef2b844a8968290edf08a2d8b382b
SingularityGuy <hardikformail@gmail.com>
2014-07-08 16:43:09 +0530

    small fix

--- app/Zidisha/Lender/Lender.php
+++ app/Zidisha/Lender/Lender.php
@@ -6,5 +6,7 @@ use Zidisha\Lender\Base\Lender as BaseLender;
 
 class Lender extends BaseLender
 {
-
+    public function getName(){
+        return $this->getFirstName() . " " . $this->getLastName();
+    }
 }
