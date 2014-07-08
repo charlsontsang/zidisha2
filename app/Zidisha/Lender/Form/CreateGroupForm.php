a5e6c37937b2c024c5c25a777d0541c2d7c1493e
SingularityGuy <hardikformail@gmail.com>
2014-07-08 19:14:44 +0530

    add view for create group

---
+++ app/Zidisha/Lender/Form/CreateGroupForm.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Zidisha\Lender\Form;
+
+
+use Illuminate\Http\Request;
+use Zidisha\Form\AbstractForm;
+
+class CreateGroupForm extends  AbstractForm
+{
+
+    public function getRules($data)
+    {
+        return [
+            'name'               => 'required|max:20|unique:lender_groups,name',
+            'website'            => 'unique:lender_groups,website',
+            'about'              => 'required|min:100',
+            'profile_picture_id' => 'image|max:2048'
+        ];
+    }
+
+    public function getDataFromRequest(Request $request) {
+        $data = parent::getDataFromRequest($request);
+        $data['profile_picture_id'] = $request->file('profile_picture_id');
+
+        return $data;
+    }
+}
