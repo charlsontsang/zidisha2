4cda9446ba82413aaa2b822e1822e4eaea95afe0
SingularityGuy <hardikformail@gmail.com>
2014-07-08 16:42:26 +0530

    add CategoryTranslation model

---
+++ app/Zidisha/Loan/CategoryTranslation.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Zidisha\Loan;
+
+use Zidisha\Loan\Base\CategoryTranslation as BaseCategoryTranslation;
+
+class CategoryTranslation extends BaseCategoryTranslation
+{
+
+}
---
+++ app/Zidisha/Loan/CategoryTranslationQuery.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Zidisha\Loan;
+
+use Zidisha\Loan\Base\CategoryTranslationQuery as BaseCategoryTranslationQuery;
+
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'category_translations' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class CategoryTranslationQuery extends BaseCategoryTranslationQuery
+{
+
+} // CategoryTranslationQuery
