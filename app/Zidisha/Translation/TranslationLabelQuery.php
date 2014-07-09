41a841bac2211729b2370b8c559333a41a6d9bbb
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-07-09 17:26:44 +0530

    Add TranslationLabel model and query class

---
+++ app/Zidisha/Translation/TranslationLabelQuery.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Zidisha\Translation;
+
+use Zidisha\Translation\Base\TranslationLabelQuery as BaseTranslationLabelQuery;
+
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'translation_labels' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class TranslationLabelQuery extends BaseTranslationLabelQuery
+{
+
+} // TranslationLabelQuery
