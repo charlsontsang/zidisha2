b1f24e5e436c0df06b4747ae239f0816c74ba116
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-07-14 18:36:42 +0530

    Add LendingGroupComment model and query class

---
+++ app/Zidisha/Comment/LendingGroupComment.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Zidisha\Comment;
+
+use Zidisha\Comment\Base\LendingGroupComment as BaseLendingGroupComment;
+
+class LendingGroupComment extends BaseLendingGroupComment
+{
+    public function setCommentReceiverId($comment, $id)
+    {
+        $comment->setLendingGroupId($id);
+    }
+
+    public function getCommentReceiverId()
+    {
+        return $this->getLendingGroupId();
+    }
+
+    public function setCommentReceiver($comment, $receiver)
+    {
+        $comment->setLendingGroup($receiver);
+    }
+
+    public function getCommentReceiver()
+    {
+        return $this->getLendingGroup();
+    }
+}
