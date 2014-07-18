4264067727b69355e4202b4c956eed311c2c11d8
SingularityGuy <hardikformail@gmail.com>
2014-07-18 19:49:08 +0530

    move labels in lang file

--- app/views/password/remind.blade.php
+++ app/views/password/remind.blade.php
@@ -5,7 +5,7 @@
     <div class="col-md-offset-3 col-md-6">
         {{ Form::open(array('url' => 'password/remind')) }}
         <div class="form-group">
-            {{ Form::label('username', 'Enter your email or username.') }}
+            {{ Form::label('username', \Lang::get('borrower.reminders.username-or-password')) }}
             {{ Form::text('username', null, array('class' => 'form-control')) }}
         </div>
         <button type="submit" class="btn btn-default">Submit</button>
