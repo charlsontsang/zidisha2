b5804282c3d339de6cf8a95bf6b38527014beef9
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-07-18 16:31:45 +0530

    Add Mailer::queue and Mailer::later Implementation

--- app/Zidisha/Mail/Mailer.php
+++ app/Zidisha/Mail/Mailer.php
@@ -1,7 +1,9 @@
 <?php
 namespace Zidisha\Mail;
 
+use Closure;
 use Config;
+use Illuminate\Support\SerializableClosure;
 use Mail;
 use Zidisha\Admin\Setting;
 
@@ -37,7 +39,7 @@ class Mailer
         }
     }
 
-    public function queue($view, $data)
+    public function addToQueue($view, $data)
     {
         $data += [
             'from' => Setting::get('site.replyTo'),
@@ -56,11 +58,12 @@ class Mailer
         }
     }
 
-    public function later($delay, $view, $data)
+    public function processLater($delay, $view, $data)
     {
         $data += [
             'from' => Setting::get('site.replyTo'),
         ];
+
         if ($this->driver == 'laravel' && $this->enabled) {
             \Mail::later(
                 $delay,
@@ -74,5 +77,81 @@ class Mailer
                 }
             );
         }
+    }
+
+    /**
+     * Queue a new e-mail message for sending on the given queue.
+     *
+     * @param  string  $queue
+     * @param  string|array  $view
+     * @param  array   $data
+     * @param  Closure|string  $callback
+     * @return void
+     */
+    public function queue($view, array $data, $callback, $queue = null)
+    {
+        $callback = $this->buildQueueCallable($callback);
+
+        \Queue::push('Zidisha\Mail\mailer@handleQueuedMessage', compact('view', 'data', 'callback'), $queue);
+    }
+
+    /**
+     * Queue a new e-mail message for sending after (n) seconds.
+     *
+     * @param  int  $delay
+     * @param  string|array  $view
+     * @param  array  $data
+     * @param  Closure|string  $callback
+     * @param  string  $queue
+     * @return void
+     */
+    public function later($delay, $view, array $data, $callback, $queue = null)
+    {
+        $callback = $this->buildQueueCallable($callback);
+
+        \Queue::later($delay, 'mailer@handleQueuedMessage', compact('view', 'data', 'callback'), $queue);
+    }
+
+    /**
+     * Build the callable for a queued e-mail job.
+     *
+     * @param  mixed  $callback
+     * @return mixed
+     */
+    protected function buildQueueCallable($callback)
+    {
+        if ( ! $callback instanceof Closure) return $callback;
+
+        return serialize(new SerializableClosure($callback));
+    }
+
+    /**
+     * Handle a queued e-mail message job.
+     *
+     * @param  \Illuminate\Queue\Jobs\Job  $job
+     * @param  array  $data
+     * @return void
+     */
+    public function handleQueuedMessage($job, $data)
+    {
+        $this->send($data['view'], $data['data'], $this->getQueuedCallable($data));
+
+        $job->delete();
+    }
+
+    /**
+     * Get the true callable for a queued e-mail message.
+     *
+     * @param  array  $data
+     * @return mixed
+     */
+    protected function getQueuedCallable(array $data)
+    {
+        if (str_contains($data['callback'], 'SerializableClosure'))
+        {
+            return with(unserialize($data['callback']))->getClosure();
+        }
+
+        return $data['callback'];
     }
 }
