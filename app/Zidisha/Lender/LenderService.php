a916d37a471769dd14c84f2a768eea266f65f724
SingularityGuy <hardikformail@gmail.com>
2014-07-19 12:40:18 +0530

    sync user and lender active colums

--- app/Zidisha/Lender/LenderService.php
+++ app/Zidisha/Lender/LenderService.php
@@ -190,6 +190,7 @@ class LenderService
                     ->setAdminDonate(true)
                     ->setActive(false);
                 $lender->save($con);
+                $lender->getUser()->setActive(false)->save($con);
             });
         }
 
