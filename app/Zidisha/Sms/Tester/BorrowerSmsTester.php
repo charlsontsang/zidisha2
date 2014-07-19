ee8e603c68b01106925a29e222dd5708244085cd
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-07-19 16:02:22 +0530

    Fix borrower sms tester

--- app/Zidisha/Sms/Tester/BorrowerSmsTester.php
+++ app/Zidisha/Sms/Tester/BorrowerSmsTester.php
@@ -4,16 +4,17 @@ namespace Zidisha\Sms\Tester;
 use Zidisha\Borrower\Borrower;
 use Zidisha\Borrower\Contact;
 use Zidisha\Borrower\Profile;
+use Zidisha\Sms\BorrowerSmsService;
 use Zidisha\Sms\dummySms;
 use Zidisha\Sms\SmsService;
 
 class BorrowerSmsTester {
 
-    private $sms;
+    private $borrowerSmsService;
 
-    public function __construct(SmsService $sms)
+    public function __construct(BorrowerSmsService $sms)
     {
-        $this->sms = $sms;
+        $this->borrowerSmsService = $sms;
     }
 
     public function sendBorrowerJoinedContactConfirmationSms()
@@ -30,13 +31,6 @@ class BorrowerSmsTester {
         $contact->setBorrower($borrower);
         $contact->setPhoneNumber('123123123');
 
-        $arguments = [
-            'borrowerName'        => $contact->getBorrower()->getName(),
-            'borrowerPhoneNumber' => $contact->getBorrower()->getProfile()->getPhoneNumber(),
-            'contactName'         => $contact->getName(),
-        ];
-
-        $text = \Lang::get('borrower.join.sms.contact-confirmation', $arguments);
-        $this->sms->queue($contact->getPhoneNumber(), $text);
+        $this->borrowerSmsService->sendBorrowerJoinedContactConfirmationSms($contact);
     }
 }
