1619c39362d1bee1ba250c70910220949dcf2cae
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-07-21 13:07:04 +0530

    Add LendingGroupNotification model

---
+++ app/Zidisha/Lender/LendingGroupNotification.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Zidisha\Lender;
+
+use Zidisha\Lender\Base\LendingGroupNotification as BaseLendingGroupNotification;
+
+class LendingGroupNotification extends BaseLendingGroupNotification
+{
+
+}
