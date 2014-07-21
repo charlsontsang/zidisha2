c4f81d533e8caf8c4a3b6b4e3fee56f4a938e514
SingularityGuy <hardikformail@gmail.com>
2014-07-21 17:54:25 +0530

    show repayment schedule in loan page

--- app/views/pages/loan.blade.php
+++ app/views/pages/loan.blade.php
@@ -200,11 +200,11 @@
                     {{ $totalInterest }} ({{ $loan->getInterestRate() }}%)</p>
 
                 <p><b>Borrower Transaction Fees: </b> <a href="#" class="transactionFee" data-toggle="tooltip">(?)</a>USD
-                    {{ $transactionFee }} (5.00%)</p>
+                                        {{ $serviceFee->getAmount() }} (5.00%)</p>
 
                 <p><b>Total Amount (Including Interest and Transaction Fee) to be Repaid: </b> <a href="#" class="repaidAmount"
                                                                                                   data-toggle="tooltip">(?)</a>
-                    USD {{ $totalInterest+$transactionFee }} ({{5.00 + $loan->getInterestRate() }}%)
+                    USD {{ $totalInterest->add($serviceFee)->getAmount() }} ({{5.00 + $loan->getInterestRate() }}%)
                 </p>
             </div>
         </div>
@@ -292,6 +292,46 @@
         </table>
         <strong>Raised: </strong> USD {{ $loan->getRaisedUsdAmount()->getAmount() }}
         <strong>Still Needed: </strong> USD {{ $loan->getStillNeededUsdAmount() }}
+
+        @if($loan->getStatus() >= Zidisha\Loan\Loan::ACTIVE)
+        <br>
+        <div>
+            <strong>REPAYMENT SCHEDULE</strong>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>Date Due</th>
+                    <th>Amount Due</th>
+                    <th>Date Paid</th>
+                    <th>Paid Amount</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($repaymentSchedules as $repaymentSchedule)
+                <tr>
+                    <td>{{ $repaymentSchedule['installment']->getAmount() }}</td>
+                    <td>{{ $repaymentSchedule['installment']->getDueDate()->format('d-m-Y') }}</td>
+                    <?php $i = 0; ?>
+                    @foreach($repaymentSchedule['payments'] as $payment)
+                        @if($i > 0)
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td>{{ $payment['payment']->getPaidDate()->format('d-m-Y') }}</td>
+                                <td>{{ $payment['amount']->getAmount() }}</td>
+                            </tr>
+                        @else
+                            <td>{{ $payment['payment']->getPaidDate()->format('d-m-Y') }}</td>
+                            <td>{{ $payment['amount']->getAmount() }}</td>
+                        @endif
+                    <?php $i++; ?>
+                    @endforeach
+                </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+        @endif
 
         @if(Auth::check() && Auth::getUser()->isAdmin())
         <br><br>
