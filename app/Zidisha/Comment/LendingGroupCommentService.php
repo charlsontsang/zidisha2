7d7a9f65cea045c7726082b708b8ae8dd6e61bbc
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-07-21 17:17:40 +0530

    Implement LendingGroupCommentService::notify

--- app/Zidisha/Comment/LendingGroupCommentService.php
+++ app/Zidisha/Comment/LendingGroupCommentService.php
@@ -2,8 +2,26 @@
 namespace Zidisha\Comment;
 
 
+use Zidisha\Lender\LendingGroup;
+use Zidisha\Mail\AdminMailer;
+use Zidisha\Mail\UserMailer;
+
 class LendingGroupCommentService extends CommentService
 {
+    /**
+     * @var \Zidisha\Mail\UserMailer
+     */
+    private $userMailer;
+    /**
+     * @var \Zidisha\Mail\AdminMailer
+     */
+    private $adminMailer;
+
+    public function __construct(UserMailer $userMailer, AdminMailer $adminMailer)
+    {
+        $this->userMailer = $userMailer;
+        $this->adminMailer = $adminMailer;
+    }
 
     /**
      * @return Comment
@@ -20,6 +38,23 @@ class LendingGroupCommentService extends CommentService
 
     protected function notify(Comment $comment)
     {
-        // TODO: Implement notify() method.
+        /** @var LendingGroup $lendingGroup */
+        $lendingGroup = $comment->getCommentReceiver();
+
+        $groupSubscribers = $lendingGroup->getLendingGroupNotificationsJoinUser();
+
+        $users = [];
+        foreach ($groupSubscribers as $groupSubscriber) {
+            $users[] = $groupSubscriber->getUser();
+        }
+
+        foreach ($users as $user) {
+            if ($comment->getUser() != $user) {
+                $this->userMailer->sentLendingGroupCommentNotification($user);
+            }
+        }
+
+        $this->adminMailer->sendLendingGroupCommentNotification($comment);
+
     }
 }
