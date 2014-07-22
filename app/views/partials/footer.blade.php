77620f3c1d0a2d7f71459f45697fb67ca05e9713
Jonas De Taeye <jonasdt@fastmail.fm>
2014-07-22 12:03:00 +0530

    Add footer style

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -1,102 +1,103 @@
-<div class="container">
+<div class="page-section page-section-inverted footer">
     <br/>
-    <hr/>
-    <div class="row">
-        <div class="col-md-6">
-            <div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div>
+                    <ul class="list-unstyled list-inline pull-right">
+                        <li>
+                            <a href="http://p2p-microlending-blog.zidisha.org/" target="_blank">
+                                <span class="fa fa-rss-square fa-2x"></span>
+                                <span class="sr-only">RSS</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://www.facebook.com/ZidishaInc?sk=wall" target="_blank">
+                                <span class="fa fa-facebook-square fa-2x"></span>
+                                <span class="sr-only">Facebook</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/ZidishaInc" target="_blank">
+                                <span class="fa fa-twitter-square fa-2x"></span>
+                                <span class="sr-only">Twitter</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="h2">Zidisha</div>
+                </div>
+                <div class="clearfix"></div>
+                
+                <p>
+                    Zidisha is an online community that enables direct person-to-person lending
+                    to entrepreneurs in developing countries.
+                </p>
+                <div class="dropdown">
+                    <a data-toggle="dropdown" href="#">
+                        English
+                        <b class="caret"></b>
+                    </a>
+                    <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+                        <li role="presentation">
+                            <a role="menuitem" tabindex="-1" href="#">Français</a>
+                            <a role="menuitem" tabindex="-1" href="#">Bahasa Indonesia</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-xs-4">
+                        <div class="h3">Explore</div>
+                        <ul class="list-unstyled">
+                            <li><a href="#">Our Story</a></li>
+                            <li><a href="#">Why Zidisha?</a></li>
+                            <li><a href="#">How It Works</a></li>
+                            <li><a href="#">Trust & Security</a></li>
+                            <li><a href="#">FAQ</a></li>
+                            <li><a href="#">Team</a></li>
+                            <li><a href="#">Press</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-4">
+                        <div class="h3">Act</div>
+                        <ul class="list-unstyled">
+                            <li><a href="#">Lend</a></li>
+                            <li><a href="#">Borrow</a></li>
+                            <li><a href="#">Volunteer</a></li>
+                            <li><a href="#">Gift Cards</a></li>
+                            <li><a href="#">Donate</a></li>
+                            <li><a href="#">Book</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-4">
+                        <div class="h3">Connect</div>
+                        <ul class="list-unstyled">
+                            <li><a href="#">Featured Comments</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Facebook</a></li>
+                            <li><a href="#">Twitter</a></li>
+                            <li><a href="{{ route('lender:groups') }}">Groups</a></li>
+                            <li><a href="#">Forum</a></li>
+                        </ul>
+                    </div>
+                </div>  
+            </div>
+        </div>
+        <hr/>
+        <div class="row">
+            <div class="col-md-8">
+                <p style="padding: 8px">Copyright &copy; 2009 - {{ date('Y') }} Zidisha Inc. Zidisha is a 501(c)(3) nonprofit organization.</p>
+            </div>
+            <div class="col-md-4">
                 <ul class="list-unstyled list-inline pull-right">
+                    <li><a href="#">Terms of Use</a></li>
+                    <li><a href="#">Contact</a></li>
                     <li>
-                        <a href="http://p2p-microlending-blog.zidisha.org/" target="_blank">
-                            <span class="fa fa-rss-square fa-2x"></span>
-                            <span class="sr-only">RSS</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="http://www.facebook.com/ZidishaInc?sk=wall" target="_blank">
-                            <span class="fa fa-facebook-square fa-2x"></span>
-                            <span class="sr-only">Facebook</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://twitter.com/ZidishaInc" target="_blank">
-                            <span class="fa fa-twitter-square fa-2x"></span>
-                            <span class="sr-only">Twitter</span>
-                        </a>
-                    </li>
-                </ul>
-                <div class="h2">Zidisha</div>
-            </div>
-            <div class="clearfix"></div>
-            
-            <p>
-                Zidisha is an online community that enables direct person-to-person lending
-                to entrepreneurs in developing countries.
-            </p>
-            <div class="dropdown">
-                <a data-toggle="dropdown" href="#">
-                    English
-                    <b class="caret"></b>
-                </a>
-                <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
-                    <li role="presentation">
-                        <a role="menuitem" tabindex="-1" href="#">Français</a>
-                        <a role="menuitem" tabindex="-1" href="#">Bahasa Indonesia</a>
+                        <a href="https://mixpanel.com/f/partner"><img src="//cdn.mxpnl.com/site_media/images/partner/badge_light.png" alt="Mobile Analytics" /></a>
                     </li>
                 </ul>
             </div>
-        </div>
-        <div class="col-md-6">
-            <div class="row">
-                <div class="col-xs-4">
-                    <div class="h4">Explore</div>
-                    <ul class="list-unstyled">
-                        <li><a href="#">Our Story</a></li>
-                        <li><a href="#">Why Zidisha?</a></li>
-                        <li><a href="#">How It Works</a></li>
-                        <li><a href="#">Trust & Security</a></li>
-                        <li><a href="#">FAQ</a></li>
-                        <li><a href="#">Team</a></li>
-                        <li><a href="#">Press</a></li>
-                    </ul>
-                </div>
-                <div class="col-xs-4">
-                    <div class="h4">Act</div>
-                    <ul class="list-unstyled">
-                        <li><a href="#">Lend</a></li>
-                        <li><a href="#">Borrow</a></li>
-                        <li><a href="#">Volunteer</a></li>
-                        <li><a href="#">Gift Cards</a></li>
-                        <li><a href="#">Donate</a></li>
-                        <li><a href="#">Book</a></li>
-                    </ul>
-                </div>
-                <div class="col-xs-4">
-                    <div class="h4">Connect</div>
-                    <ul class="list-unstyled">
-                        <li><a href="#">Featured Comments</a></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">Facebook</a></li>
-                        <li><a href="#">Twitter</a></li>
-                        <li><a href="{{ route('lender:groups') }}">Groups</a></li>
-                        <li><a href="#">Forum</a></li>
-                    </ul>
-                </div>
-            </div>  
-        </div>
-    </div>
-    <hr/>
-    <div class="row">
-        <div class="col-md-8">
-            <p style="padding: 8px">Copyright &copy; 2009 - {{ date('Y') }} Zidisha Inc. Zidisha is a 501(c)(3) nonprofit organization.</p>
-        </div>
-        <div class="col-md-4">
-            <ul class="list-unstyled list-inline pull-right">
-                <li><a href="#">Terms of Use</a></li>
-                <li><a href="#">Contact</a></li>
-                <li>
-                    <a href="https://mixpanel.com/f/partner"><img src="//cdn.mxpnl.com/site_media/images/partner/badge_light.png" alt="Mobile Analytics" /></a>
-                </li>
-            </ul>
         </div>
     </div>
 </div>
