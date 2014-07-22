26d9f92f2577e90bdc98db09182310affaad14d9
SingularityGuy <hardikformail@gmail.com>
2014-07-22 17:18:04 +0530

    add amount methods in BorrowerRefund Class

--- app/Zidisha/Repayment/BorrowerRefund.php
+++ app/Zidisha/Repayment/BorrowerRefund.php
@@ -2,9 +2,19 @@
 
 namespace Zidisha\Repayment;
 
+use Zidisha\Currency\Money;
 use Zidisha\Repayment\Base\BorrowerRefund as BaseBorrowerRefund;
 
 class BorrowerRefund extends BaseBorrowerRefund
 {
 
+    public function getAmount()
+    {
+        return Money::create(parent::getAmount(), 'USD');
+    }
+
+    public function setAmount($money)
+    {
+        return parent::setAmount($money->getAmount());
+    }
 }
