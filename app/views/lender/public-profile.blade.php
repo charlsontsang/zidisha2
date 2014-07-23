0c2ca8a6680b93fb10c749dbaaef744c73ddc52a
SingularityGuy <hardikformail@gmail.com>
2014-07-23 11:36:58 +0530

    add Karma in blade file

--- app/views/lender/public-profile.blade.php
+++ app/views/lender/public-profile.blade.php
@@ -14,4 +14,12 @@ Join the global P2P microlending movement
 <p><strong>Username: </strong> {{ $lender->getUser()->getUsername() }} </p> <br>
 
 <p><strong>About me: </strong> {{ $lender->getProfile()->getAboutMe() }} </p> <br>
+
+<p><strong>Karma: </strong><a href="#" class="karma" data-toggle="tooltip">(?)</a> {{ $karma }} </p> <br>
+@stop
+
+@section('script-footer')
+<script type="text/javascript">
+    $('.karma').tooltip({placement: 'bottom', title: 'Karma is calculated on the basis of the total amount lent by the new members a member has recruited to Zidisha via email invites or gift cards, the number of comments a member has posted in the Zidisha website, and the total amount lent by a member.'})
+</script>
 @stop
