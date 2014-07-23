c24138b991b1f4a08c9a093b9d47e00d0305ab6c
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-07-23 16:09:33 +0530

    show rating in front of message

--- app/views/partials/comments/partial/post.blade.php
+++ app/views/partials/comments/partial/post.blade.php
@@ -1,16 +1,20 @@
 <div class="comment">
     {{ BootstrapForm::open(array('action' => [ $controller.'@postComment', 'id' => $receiver->getId()  ], 'translationDomain' => 'comments', 'files' => true)) }}
     {{ BootstrapForm::hidden('receiver_id', $receiver->getId()) }}
-    {{ BootstrapForm::textarea('message') }}
 
     @if($controller == 'LoanFeedbackController')
         {{ BootstrapForm::select('rating', array('positive' => 'Positive', 'neutral' => 'Neutral', 'negative' => 'Negative'), 'positive') }}
-    @else
+    @endif
+
+    {{ BootstrapForm::textarea('message') }}
+
+    @if($controller != 'LoanFeedbackController')
         <div class="comment-upload-inputs">
             {{ BootstrapForm::file('file[]', ['label' => 'comments.upload-file']) }}
         </div>
         <button class="btn btn-primary btn-success comment-upload-add-more">@lang('comments.add-more')</button>
     @endif
+
     {{ BootstrapForm::submit('submit') }}
     {{ BootstrapForm::close() }}
 </div>
