842747ec71942d86c2caf76d56b70f836f5d29e9
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-07-23 13:30:48 +0530

    Add LoanFeedbackPostCommentForm

---
+++ app/Zidisha/Comment/Form/LoanFeedbackPostCommentForm.php
@@ -0,0 +1,13 @@
+<?php
+namespace Zidisha\Comment\Form;
+
+class LoanFeedbackPostCommentForm extends PostCommentForm
+{
+    public function getRules($data)
+    {
+        return [
+            'message' => 'required',
+            'rating'  => 'required',
+        ];
+    }
+} 
