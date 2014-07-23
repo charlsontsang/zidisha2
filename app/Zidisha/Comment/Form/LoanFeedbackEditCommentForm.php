eecca437590e0eb57d7cccc579bdff653f556aba
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-07-23 19:16:13 +0530

    Use in validator

--- app/Zidisha/Comment/Form/LoanFeedbackEditCommentForm.php
+++ app/Zidisha/Comment/Form/LoanFeedbackEditCommentForm.php
@@ -1,14 +1,23 @@
 <?php
 namespace Zidisha\Comment\Form;
 
+use Zidisha\Comment\LoanFeedbackComment;
+
 class LoanFeedbackEditCommentForm extends EditCommentForm
 {
     public function getRules($data)
     {
         return [
             'message'    => 'required',
-            'rating'     => 'required|checkCommentRating',
+            'rating'     => 'required|in:' . $this->getRatingTypes(),
             'comment_id' => 'required',
         ];
+    }
+
+    public function getRatingTypes()
+    {
+        return LoanFeedbackComment::NEUTRAL . ',' .
+        LoanFeedbackComment::NEGATIVE . ',' .
+        LoanFeedbackComment::POSITIVE;
     }
 } 
--- app/Zidisha/Comment/Form/LoanFeedbackPostCommentForm.php
+++ app/Zidisha/Comment/Form/LoanFeedbackPostCommentForm.php
@@ -1,13 +1,22 @@
 <?php
 namespace Zidisha\Comment\Form;
 
+use Zidisha\Comment\LoanFeedbackComment;
+
 class LoanFeedbackPostCommentForm extends PostCommentForm
 {
     public function getRules($data)
     {
         return [
             'message' => 'required',
-            'rating'  => 'required|checkCommentRating',
+            'rating'  => 'required|in:' . $this->getRatingTypes(),
         ];
+    }
+
+    public function getRatingTypes()
+    {
+        return LoanFeedbackComment::NEUTRAL . ',' .
+               LoanFeedbackComment::NEGATIVE . ',' .
+               LoanFeedbackComment::POSITIVE;
     }
 } 
