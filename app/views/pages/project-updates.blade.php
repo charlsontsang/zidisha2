99e3c3cd819effeb7fafa0296e0c7f416b7b7394
jkurnia <julia@zidisha.org>
2014-07-25 12:50:28 +0000

    static page formatting

--- app/views/pages/project-updates.blade.php
+++ app/views/pages/project-updates.blade.php
@@ -5,21 +5,23 @@ Publish Comments
 @stop
 
 @section('content')
-<div class="info-page">
-    <h1>
-        Project Updates
-    </h1>
-    <p>
-    	We’re not just about spreading the wealth — we’re all about spreading friendship, too! That’s why we make it easy for lenders and borrowers to communicate directly with each other throughout the duration of their loans and beyond. Here, lenders can see their seed money grow into something life changing. And borrowers can show off their workspaces and update everyone on their goals and progress. Scroll down to see ambition in action!
-    </p>
-</div>
-
 <div class="row">
-    @foreach($comments as $comment)
-        @include('partials.comments.display-comment-partial', ['comment' => $comment])
-    <br/>
-    @endforeach
+    <div class="col-md-8 col-md-offset-2">
+        <div class="info-page">
+            <h1>
+                Project Updates
+            </h1>
+            <p>
+            	We’re not just about spreading the wealth — we’re all about spreading friendship, too! That’s why we make it easy for lenders and borrowers to communicate directly with each other throughout the duration of their loans and beyond. Here, lenders can see their seed money grow into something life changing. And borrowers can show off their workspaces and update everyone on their goals and progress. Scroll down to see ambition in action!
+            </p>
+        </div>
 
-    {{ BootstrapHtml::paginator($comments)->links() }}
+        @foreach($comments as $comment)
+            @include('partials.comments.display-comment-partial', ['comment' => $comment])
+        <br/>
+        @endforeach
+
+        {{ BootstrapHtml::paginator($comments)->links() }}
+    </div>
 </div>
 @stop
