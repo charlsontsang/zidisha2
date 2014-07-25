0fd02bfd92be8b8065b646bfcd6371d65463da4f
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-07-25 21:38:30 +0530

    Add comments language files

---
+++ app/lang/en/borrower/comments.php
@@ -0,0 +1,30 @@
+<?php
+return [
+    'loading-text'   => [
+        'post' => 'Posting comment...',
+        'edit' => 'Editing comment...',
+        'reply' => 'Replying comment...',
+        'delete' => 'Delete comment...',
+        'translate' => 'Translating comment...'
+    ],
+    'actions' => [
+      'translate'  => 'Translate',
+      'edit'  => 'Edit',
+      'delete'  => 'Delete',
+      'reply'  => 'Reply',
+      'share'  => 'Share',
+    ],
+    'flash'=>[
+        'post-success' => 'Comment posted',
+        'file-not-valid' => 'File is not valid',
+        'edit-success' => 'Comment edited',
+        'reply-success' => 'Reply posted',
+        'delete-success' => 'Comment deleted',
+        'translate-success' => 'Comment translation posted',
+        'file-deleted' => 'File deleted',
+    ],
+    'upload-file' => 'Upload Images or files',
+    'add-more' => 'Add more',
+    'submit' => 'Submit',
+    'message' => 'Message'
+];
