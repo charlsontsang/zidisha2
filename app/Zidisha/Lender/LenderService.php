9713494e0fa764c1437c9f02c8583c46b50fbfc4
SingularityGuy <hardikformail@gmail.com>
2014-07-31 19:36:41 +0530

    calculate group members total impacts

--- app/Zidisha/Lender/LenderService.php
+++ app/Zidisha/Lender/LenderService.php
@@ -377,6 +377,29 @@ class LenderService
         return Money::create($totalAmount, 'USD')->multiply(-1);
     }
 
+    public function getGroupMembersTotalImpact($ids)
+    {
+        $groupMembersIds = implode(',', $ids->getData());
+
+        $sql = "SELECT SUM(Amount)
+                        FROM transactions
+                        WHERE type IN (:loanBid, :loanOutbid)
+                        AND user_id IN ( SELECT invitee_id FROM lender_invites
+                                          WHERE invitee_id NOT IN(".$groupMembersIds.")
+                                          AND lender_id IN(".$groupMembersIds.")
+                                          UNION
+                                          SELECT recipient_id FROM gift_cards
+                                          WHERE recipient_id NOT IN (".$groupMembersIds.")
+                                          AND lender_id IN(".$groupMembersIds."))";
+
+        $totalAmount = ( PropelDB::fetchNumber($sql, [
+                'loanBid' => Transaction::LOAN_BID,
+                'loanOutbid' => Transaction::LOAN_OUTBID,
+            ]));
+
+        return Money::create($totalAmount, 'USD')->multiply(-1);
+    }
+
     public function getTotalAmountLentByInvitee(Lender $lender)
     {
         $sql = 'SELECT SUM(Amount)
