c679e1bcc869a9e295f75e334c12fe0ad09b6849
SingularityGuy <hardikformail@gmail.com>
2014-08-01 20:49:58 +0530

    helper methods for borrower_payments schema

--- app/Zidisha/Repayment/BorrowerPayment.php
+++ app/Zidisha/Repayment/BorrowerPayment.php
@@ -2,9 +2,20 @@
 
 namespace Zidisha\Repayment;
 
+use Zidisha\Currency\Money;
 use Zidisha\Repayment\Base\BorrowerPayment as BaseBorrowerPayment;
 
 class BorrowerPayment extends BaseBorrowerPayment
 {
 
+    public function getAmount()
+    {
+        return Money::create(parent::getAmount(), 'USD');
+    }
+
+    public function setAmount($money)
+    {
+        return parent::setAmount($money->getAmount());
+    }
 }
+
