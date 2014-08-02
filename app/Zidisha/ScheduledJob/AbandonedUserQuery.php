204997afdf6d82ff19ddef373f53000fd26b53b2
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-08-02 12:09:01 +0530

    Add AbandonedUser models

---
+++ app/Zidisha/ScheduledJob/AbandonedUserQuery.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Zidisha\ScheduledJob;
+
+use Zidisha\ScheduledJob\Base\AbandonedUserQuery as BaseAbandonedUserQuery;
+
+
+/**
+ * Skeleton subclass for representing a query for one of the subclasses of the 'scheduled_jobs' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class AbandonedUserQuery extends BaseAbandonedUserQuery
+{
+
+} // AbandonedUserQuery
