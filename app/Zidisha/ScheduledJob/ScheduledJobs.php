d9c658f305b4b4164bc7d3a82ac0eb0d3e6d97d4
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-08-03 21:43:27 +0530

    Add ScheduledJobs::process, ScheduledJobs::fire

--- app/Zidisha/ScheduledJob/ScheduledJobs.php
+++ app/Zidisha/ScheduledJob/ScheduledJobs.php
@@ -1,10 +1,36 @@
 <?php
-
 namespace Zidisha\ScheduledJob;
 
 use Zidisha\ScheduledJob\Base\ScheduledJobs as BaseScheduledJobs;
 
 class ScheduledJobs extends BaseScheduledJobs
 {
+    /**
+     * @var ScheduledJobService
+     */
+    private $scheduledJobService;
 
+    public function fire($job, $data)
+    {
+        $jobsId = $data['jobId'];
+        $jobCount = $data['jobCount'];
+
+        $scheduleJobs = ScheduledJobsQuery::create()
+            ->findOneById($jobsId);
+
+        $scheduleJobs->process($job, $data);
+
+        $job->delete();
+
+        if ($job->isDeleted()) {
+            /** @var  ScheduledJobService $scheduleJobService */
+            $scheduleJobService = \App::make('Zidisha\ScheduledJob\ScheduledJobService');
+            $scheduleJobService->updateJob($scheduleJobs, $jobsId, $jobCount);
+        }
+    }
+
+    public function process($job, $data)
+    {        
+        exit;
+    }
 }
