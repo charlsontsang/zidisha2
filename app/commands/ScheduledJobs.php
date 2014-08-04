5c9714b9875f79d4db628f488abad24505f17cf4
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-08-04 13:32:48 +0530

    Improve SceduledJobs Query

--- app/commands/ScheduledJobs.php
+++ app/commands/ScheduledJobs.php
@@ -21,10 +21,19 @@ class ScheduledJobs extends Command
         $abandonedUser = \App::make('Zidisha\ScheduledJob\AbandonedUser');
         $query = $abandonedUser->getQuery()
             ->leftJoin('scheduled_jobs AS s', 'u.id', '=', 's.user_id')
-            ->whereRaw('s.type = 0')
             ->whereRaw('s.start_date = u.last_login_at')
-            ->whereRaw('s.user_id = u.id');
-        $query->whereRaw("s.last_processed_at IS NULL OR (s.last_processed_at + (s.count || ' months')::interval) > NOW()");
+            ->whereRaw('s.user_id = u.id')
+            ->whereRaw("s.last_processed_at IS NULL OR (s.last_processed_at + (s.count || ' months')::interval) > NOW()")
+            ->whereRaw("
+                    ( 
+                        (
+                            s.last_processed_at + (s.COUNT || ' months') :: INTERVAL
+                        ) > NOW() OR  
+                        (
+                            s.last_processed_at + (s.COUNT || ' months') :: INTERVAL
+                        ) IS NULL
+                    )
+                ");
         
 //        dd($query->toSql());
         $users = $query->get();
