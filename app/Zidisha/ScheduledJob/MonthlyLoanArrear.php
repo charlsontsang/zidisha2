c1a9fc44ff4be37bc698c2f3e840d4de8563c63c
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-08-04 12:54:46 +0530

    Add schedule job sub classes and query classes

---
+++ app/Zidisha/ScheduledJob/MonthlyLoanArrear.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Zidisha\ScheduledJob;
+
+use Zidisha\ScheduledJob\Map\ScheduledJobsTableMap;
+
+
+/**
+ * Skeleton subclass for representing a row from one of the subclasses of the 'scheduled_jobs' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class MonthlyLoanArrear extends ScheduledJobs
+{
+
+    /**
+     * Constructs a new MonthlyLoanArrear class, setting the class_key column to ScheduledJobsTableMap::CLASSKEY_4.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setClassKey(ScheduledJobsTableMap::CLASSKEY_4);
+    }
+
+
+    public function getQuery()
+    {
+
+    }
+
+    public function process($job, $data)
+    {
+        $scheduleJobs = ScheduledJobsQuery::create()
+            ->findOneById($data['jobId']);
+
+        $user = $scheduleJobs->getUser();
+    }
+} // MonthlyLoanArrear
---
+++ app/Zidisha/ScheduledJob/RepaymentReminder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Zidisha\ScheduledJob;
+
+use Zidisha\ScheduledJob\Map\ScheduledJobsTableMap;
+
+
+/**
+ * Skeleton subclass for representing a row from one of the subclasses of the 'scheduled_jobs' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class RepaymentReminder extends ScheduledJobs
+{
+
+    /**
+     * Constructs a new RepaymentReminder class, setting the class_key column to ScheduledJobsTableMap::CLASSKEY_5.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setClassKey(ScheduledJobsTableMap::CLASSKEY_5);
+    }
+
+    public function getQuery()
+    {
+
+    }
+
+    public function process($job, $data)
+    {
+        $scheduleJobs = ScheduledJobsQuery::create()
+            ->findOneById($data['jobId']);
+
+        $user = $scheduleJobs->getUser();
+    }
+} // RepaymentReminder
