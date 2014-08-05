3d4f222f848984051e6647d45faa77675a59e2b7
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-08-05 17:56:37 +0530

    Add class constants to Notification

--- app/Zidisha/Notification/Notification.php
+++ app/Zidisha/Notification/Notification.php
@@ -6,5 +6,7 @@ use Zidisha\Notification\Base\Notification as BaseNotification;
 
 class Notification extends BaseNotification
 {
+    const ABANDONED = 0;
+    const UNUSED_FUNDS_NOTIFICATION = 1;
 
 }
