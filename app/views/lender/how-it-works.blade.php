2005f1f3729dd2fa54a6f73775edbda0231e0930
jkurnia <julia@zidisha.org>
2014-08-05 21:47:40 +0000

    style lender invite page

--- app/views/lender/how-it-works.blade.php
+++ app/views/lender/how-it-works.blade.php
@@ -5,41 +5,42 @@ Invite Your Friends: How It Works
 @stop
 
 @section('content')
-<h1>Invite Your Friends: How It Works</h1>
-<br/>
-<h4>Sending Invite Credits</h4>
-<p>
-    Any Zidisha lender may send complimentary lending credits to new lenders.  You may send as many invites and lending credits as you like.  You are also welcome to share your invite link at Facebook, Twitter or other social media.
-</p>
-<p>
-    Once your invitee makes a loan, you will receive a $25 matching invite credit in your own lending account.  The same terms and conditions apply to this matching invite credit as to other invite credits.
-</p>
-<br/>
-<h4>Receiving Invite Credits</h4>
-<p>
-    Invitees may receive invite credits by clicking the "Redeem" button in the invite email, or by following the invite link that is provided by the inviting member.
-</p>
-<br/>
-<h4>Redeeming Invite Credits</h4>
-<p>
-    To redeem an invite credit, make one or more loans up to a maximum value of $25 at 0% interest.  The invite credit will be applied to your chosen loans in the Lending Cart.
-</p>
-<br/>
-<h4>Invite Credit Funding</h4>
-<p>
-    The invite credits are funded by an account that is comprised of donations to Zidisha.  Once repaid, these credits are returned to the donation account in order to allow other lenders to participate in the invite program.
-</p>
-<br/>
-<h4>Terms and Conditions</h4>
-<p>
-    Invite credits may only be used to make loans.  They may not be withdrawn or used for gift cards.
-</p>
-<p>
-    Unlike self-funded Zidisha loans, repayments of loans funded with invite credits are not credited to individual lending accounts, but are instead credited back to the account of the donor(s) who funded the invite credit program.
-</p>
-<p>
-    Invite credits which are not redeemed within one month will expire and be returned to the invite credit donation account so that others may use them.
-<p>
-    The invite credit program is for a limited time only, and we reserve the right to modify or discontinue it at any time.
-</p>
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+	    <div class="page-header">
+	        <h1>Invite Your Friends: How It Works</h1>
+	    </div>
+		<h4>Sending Invite Credits</h4>
+		<p>
+		    Any Zidisha lender may send complimentary lending credits to new lenders.  You may send as many invites and lending credits as you like.  You are also welcome to share your invite link at Facebook, Twitter or other social media.
+		</p>
+		<p>
+		    Once your invitee makes a loan, you will receive a $25 matching invite credit in your own lending account.  The same terms and conditions apply to this matching invite credit as to other invite credits.
+		</p>
+		<h4>Receiving Invite Credits</h4>
+		<p>
+		    Invitees may receive invite credits by clicking the "Redeem" button in the invite email, or by following the invite link that is provided by the inviting member.
+		</p>
+		<h4>Redeeming Invite Credits</h4>
+		<p>
+		    To redeem an invite credit, make one or more loans up to a maximum value of $25 at 0% interest.  The invite credit will be applied to your chosen loans in the Lending Cart.
+		</p>
+		<h4>Invite Credit Funding</h4>
+		<p>
+		    The invite credits are funded by an account that is comprised of donations to Zidisha.  Once repaid, these credits are returned to the donation account in order to allow other lenders to participate in the invite program.
+		</p>
+		<h4>Terms and Conditions</h4>
+		<p>
+		    Invite credits may only be used to make loans.  They may not be withdrawn or used for gift cards.
+		</p>
+		<p>
+		    Unlike self-funded Zidisha loans, repayments of loans funded with invite credits are not credited to individual lending accounts, but are instead credited back to the account of the donor(s) who funded the invite credit program.
+		</p>
+		<p>
+		    Invite credits which are not redeemed within one month will expire and be returned to the invite credit donation account so that others may use them.
+		<p>
+		    The invite credit program is for a limited time only, and we reserve the right to modify or discontinue it at any time.
+		</p>
+	</div>
+</div>
 @stop
--- app/views/lender/invite.blade.php
+++ app/views/lender/invite.blade.php
@@ -1,95 +1,106 @@
 @extends('layouts.master')
 
 @section('page-title')
-Invite Your Friends To Zidisha
+Invite Your Friends
 @stop
 
 @section('content')
-<div class="wrapper" style="text-align: center;">
-    <h2>
-        Send a friend $25 in free lending credit!
-    </h2>
-</div>
+<div class="row">
+    <div class="col-sm-10 col-sm-offset-1">
+        <div class="page-header" style="text-align: center;">
+            <h1>Send a friend $25 to make a loan for free!</h1>
+        </div>
 
-<div>
-    <div style="text-align: center;margin-top: 20px;">
-        <div>
+        <p style="text-align: center;">
             You'll receive a matching $25 credit when they lend.
             &nbsp;&nbsp;&nbsp;&nbsp;
             <a href="{{ route('lender:how-it-works') }}">How it works</a>
-        </div>
-        <br/>
-        <br/>
-
-        <div>
-            {{ BootstrapForm::open(array('route' => 'lender:post-invite')) }}
-            {{ BootstrapForm::populate($form) }}
-
-            {{ BootstrapForm::text('emails', null, ['placeholder' => 'Enter emails separated by commas']) }}
-            {{ BootstrapForm::text('subject') }}
-            {{ BootstrapForm::textarea('note', null, ['placeholder' => 'Add A note']) }}
-            {{ BootstrapForm::submit('submit') }}
-
-            {{ BootstrapForm::close() }}
-        </div>
-        <div>
-            OR
-        </div>
-
-        <div>
-            <input size="50 px" type="text" value="{{ $invite_url }}" readonly="">
-        </div>
-        <p>
-            Share or tweet this link
-                            <span class="share-actions">
-                                &nbsp;&nbsp;
-                                <a href="{{$facebook_url}}">
-                                    <img src="{{ asset('/assets/images/icons/fb.png') }}" alt="Share on Facebook"/>
-                                </a>
-                                <a href="{{$twitter_url}}">
-                                    <img src="{{ asset('/assets/images/icons/twitter.png') }}" alt="Tweet"/>
-                                </a>
-                            </span>
         </p>
+
+        <br/><br/>
+
+        <div class="row">
+            <div class="col-sm-5">
+
+                {{ BootstrapForm::open(array('route' => 'lender:post-invite')) }}
+                {{ BootstrapForm::populate($form) }}
+
+                {{ BootstrapForm::text('emails', null, ['placeholder' => 'Enter emails separated by commas']) }}
+                {{ BootstrapForm::text('subject') }}
+                {{ BootstrapForm::textarea('note', null, ['style' => 'height:100px'], ['placeholder' => 'Add a note']) }}
+                {{ BootstrapForm::submit('Send', ['class' => 'btn btn-primary btn-block']) }}
+
+                {{ BootstrapForm::close() }}
+
+            </div>
+
+            <div class="col-sm-2">
+                <div class="circle" style="margin-top: 30px; margin-bottom: 30px;">
+                    OR
+                </div>
+            </div>
+
+            <div class="col-sm-5">
+                <p>
+                    Share or tweet your personal invite link:
+                </p>
+
+                <p class="text-large">
+                    {{ $invite_url }}
+                </p>
+
+                <p>
+                    <span class="share-actions">
+                        <a href="{{$facebook_url}}">
+                            <img src="{{ asset('/assets/images/icons/fb.png') }}" width="50px" style="margin-right: 15px;" alt="Share on Facebook"/>
+                        </a>
+                        <a href="{{$twitter_url}}">
+                            <img src="{{ asset('/assets/images/icons/twitter.png') }}" width="50px" alt="Tweet"/>
+                        </a>
+                    </span>
+                </p>
+                
+            </div>
+        </div>
+
+        <br/><br/>
+
+        <h3 style="margin-bottom: 25px" style="text-align: center;">
+            <span style="color:#00aeef;">{{$count_joined_invites}}</span> of your {{$count_invites}} invitees have joined
+            Zidisha.
+        </h3>
+
+        <table class="table table-striped no-more-tables">
+            <thead>
+            <tr>
+                <th>Date Invited</th>
+                <th>Email</th>
+                <th>Status</th>
+                <th class="td-profile-image"></th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($invites as $invite)
+            <tr>
+                <td data-title="Date Invited">{{ $invite->getCreatedAt()->format('M j, Y') }}</td>
+                <td data-title="Email">{{ $invite->getEmail() }}</td>
+                <td data-title="Status">
+                    @if($invite->getInvitee())
+                    Joined on {{ $invite->getInvitee()->getCreatedAt()->format('M j, Y') }}
+                    @else
+                    Invite Not Yet Accepted
+                    @endif
+                </td>
+                <td>@if($invite->getInvitee())
+                    <a href="{{ route('lender:public-profile', $invite->getInvitee()->getUser()->getUsername()) }} ">
+                        {{ $invite->getInvitee()->getUser()->getUsername()}}
+                    </a>
+                    @endif
+                </td>
+            </tr>
+            @endforeach
+            </tbody>
+        </table>
     </div>
-
-    <h3 style="margin-bottom: 25px">
-        <span style="color:#00aeef;">{{$count_joined_invites}}</span> of your {{$count_invites}} invitees have joined
-        Zidisha.
-    </h3>
-
-    <table class="table table-striped">
-        <thead>
-        <tr>
-            <th class="td-profile-image"></th>
-            <th>Date Invited</th>
-            <th>Email</th>
-            <th>Status</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($invites as $invite)
-        <tr>
-            <td>@if($invite->getInvitee())
-                <a href="{{ route('lender:public-profile', $invite->getInvitee()->getUser()->getUsername()) }} ">
-                    {{ $invite->getInvitee()->getUser()->getUsername()}}
-                </a>
-                @endif
-            </td>
-            <td>{{ $invite->getCreatedAt()->format('d-m-Y') }}</td>
-            <td>{{ $invite->getEmail() }}</td>
-            <td>
-                @if($invite->getInvitee())
-                Joined on {{ $invite->getInvitee()->getCreatedAt()->format('d-m-Y') }}
-                @else
-                Invite Not Yet Accepted
-                @endif
-            </td>
-        </tr>
-        @endforeach
-        </tbody>
-    </table>
 </div>
-</div>
-
 @stop
