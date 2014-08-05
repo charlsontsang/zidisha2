9e9bd33c1f2dcf5baff9df687a1be23933e969f3
jkurnia <julia@zidisha.org>
2014-08-05 18:36:02 +0000

    style lending groups page

--- app/views/lender/lending-groups.blade.php
+++ app/views/lender/lending-groups.blade.php
@@ -5,39 +5,45 @@ Lending Groups
 @stop
 
 @section('content')
-<h2>Lending Groups</h2>
-<p>
-    Lending Groups maximize their impact by combining forces to do more good. Whether the members of a Lending Group share a common passion, support similar causes, or simply come from the same country, they all join together to make microlending miracles happen. Check them out below — and get involved!
-</p>
-@if(Auth::check() && Auth::getUser()->isLender())
-<a href="{{ route('lender:groups:create') }}" class="btn btn-primary">
-   Start a new Group
-</a>
+<div class="row">
+    <div class="col-md-12 info-page">
+        <div class="page-header">
+            <h1>Lending Groups</h1>
+        </div>
+        <p>
+            Lending Groups maximize their impact by combining forces to do more good. Whether the members of a Lending Group share a common passion, support similar causes, or simply come from the same country, they all join together to make microlending miracles happen. Check them out below — and get involved!
+            <br/><br/>
+        </p>
+        @if(Auth::check() && Auth::getUser()->isLender())
+        <a href="{{ route('lender:groups:create') }}" class="btn btn-primary">
+           Start a new group
+        </a>
+    </div>
+</div>
 @endif
-<br><br>
 @if($paginator != null)
-<table class="table table-striped">
+<table class="table table-striped no-more-tables">
     <thead>
     <tr>
         <th>Group Name</th>
-        <th>Total Impact This month</th>
-        <th>Total Impact last month</th>
-        <th>Total impact all time</th>
-        <th>About This group</th>
+        <th>Impact This Month</th>
+        <th>Impact Last Month</th>
+        <th>Impact All Time</th>
+        <th width="30%">About This Group</th>
         <th></th>
     </tr>
     </thead>
     <tbody>
     @foreach($paginator as $group)
     <tr>
-        <td>{{ $group->getName() }}
+        <td data-title="Group Name">{{ $group->getName() }}
             @if($group->getGroupProfilePicture())
                 <img src="{{ $group->getGroupProfilePicture()->getImageUrl('small-profile-picture') }}" alt=""/>
             @endif
         </td>
-        <td>{{ $groupsImpacts[$group->getId()]['totalImpactThisMonth'] }}</td>
-        <td>{{ $groupsImpacts[$group->getId()]['totalImpactLastMonth'] }}</td>
-        <td>{{ $groupsImpacts[$group->getId()]['totalImpact'] }}</td>
+        <td data-title="Impact This Month">{{ $groupsImpacts[$group->getId()]['totalImpactThisMonth'] }}</td>
+        <td data-title="Impact Last Month">{{ $groupsImpacts[$group->getId()]['totalImpactLastMonth'] }}</td>
+        <td data-title="Impact All Time">{{ $groupsImpacts[$group->getId()]['totalImpact'] }}</td>
         <td>{{ $group->getAbout() }}</td>
         <td><a href="{{ route('lender:group', $group->getId()) }}">View Profile</a></td>
     </tr>
