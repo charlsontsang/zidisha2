d7d228b17c720f4828e3194b4e4a15b98b18ab00
jkurnia <julia@zidisha.org>
2014-08-05 19:35:51 +0000

    style edit lending group page

--- app/views/lender/edit-lending-group.blade.php
+++ app/views/lender/edit-lending-group.blade.php
@@ -5,25 +5,31 @@ Edit Lending Group
 @stop
 
 @section('content')
-{{ BootstrapForm::open(['route' => ['lender:groups:post-edit', $group->getId()], 'translationDomain' => 'lender.group', 'files' => true]) }}
+<div class="row">
+	<div class="col-sm-6 col-sm-offset-3">
+		<div class="page-header">
+		    <h1>Edit Lending Group</h1>
+		</div>
+		{{ BootstrapForm::open(['route' => ['lender:groups:post-edit', $group->getId()], 'translationDomain' => 'lender.group', 'files' => true]) }}
 
-{{ BootstrapForm::populate($form) }}
+		{{ BootstrapForm::populate($form) }}
 
-{{ BootstrapForm::text('name') }}
+		{{ BootstrapForm::text('name') }}
 
-{{ BootstrapForm::text('website') }}
+		{{ BootstrapForm::text('website') }}
 
-{{ BootstrapForm::file('groupProfilePictureId') }}
-@if($group->getGroupProfilePicture())
-    <img src="{{ $group->getGroupProfilePicture()->getImageUrl('small-profile-picture') }}" alt=""/>
-@endif
+		{{ BootstrapForm::file('groupProfilePictureId') }}
+		@if($group->getGroupProfilePicture())
+		    <img src="{{ $group->getGroupProfilePicture()->getImageUrl('small-profile-picture') }}" alt=""/>
+		@endif
 
-{{ BootstrapForm::textarea('about') }}
+		{{ BootstrapForm::textarea('about') }}
 
-{{ BootstrapForm::select('userId', $form->getMembers(), $group->getLeader()->getUser()->getId()) }}
+		{{ BootstrapForm::select('userId', $form->getMembers(), $group->getLeader()->getUser()->getId()) }}
 
-{{ BootstrapForm::submit('save') }}
+		{{ BootstrapForm::submit('save') }}
 
-{{ BootstrapForm::close() }}
-
+		{{ BootstrapForm::close() }}
+	</div>
+</div>
 @stop
