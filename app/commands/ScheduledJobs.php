4871b6bab195d7f128f1fc94945338d41a7747ba
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-08-08 02:42:23 +0530

    Improve scheduleJobs command

--- app/commands/ScheduledJobs.php
+++ app/commands/ScheduledJobs.php
@@ -2,13 +2,15 @@
 
 use Illuminate\Console\Command;
 use Zidisha\ScheduledJob\AbandonedUser;
+use Zidisha\ScheduledJob\ScheduledJob;
+use Zidisha\ScheduledJob\ScheduledJobQuery;
 
 class ScheduledJobs extends Command
 {
 
-    protected $name = 'scheduled-jobs';
+    protected $name = 'ScheduledJobs';
 
-    protected $description = 'This command is to run scheduled cron jobs';
+    protected $description = 'This command is to scheduled cron jobs';
 
     protected $classes = [
         'Zidisha\ScheduledJob\AbandonedUser',
@@ -34,36 +36,38 @@ class ScheduledJobs extends Command
         foreach ($this->classes as $class) {
             $scheduledJobClass = \App::make($class);
             $query = $this->joinQuery($scheduledJobClass);
-//             print_r($query->toSql());
-//            dd();
             $jobs = $query->get();
+            
             foreach ($jobs as $job) {
-                var_dump($job);
+                if ($job->schedule_job_id == null) {
+                    $scheduledJob = new ScheduledJob();
+                    $scheduledJob->setUserId($job->user_id);
+                    $scheduledJob->SetClassKey($scheduledJobClass->getClassKey());
+                    $scheduledJob->save();
+                } else {
+                    $scheduledJob = ScheduledJobQuery::create()
+                        ->findOneById($job->schedule_job_id);
+                    
+                    $scheduledJob->setCount($scheduledJob->getCount() + 1);
+                    $scheduledJob->save();
+                }
             }
-                dd();
         }
     }
 
     public function joinQuery($scheduledJobClass)
     {
-        return $scheduledJobClass->getQuery()
+        $query = $scheduledJobClass->getQuery()
+            ->addSelect('s.id as schedule_job_id')
             ->leftJoin('scheduled_jobs AS s', 'user_id', '=', 's.user_id')
             ->whereRaw('s.start_date = start_date')
             ->whereRaw('s.user_id = user_id')
-            ->whereRaw(
-                "s.last_processed_at IS NULL OR (s.last_processed_at + (s.count || ' months')::interval) > NOW()"
-            )
-            ->whereRaw(
-                "
-                    ( 
-                        (
-                            s.last_processed_at + (s.COUNT || ' months') :: INTERVAL
-                        ) > NOW() OR  
-                        (
-                            s.last_processed_at + (s.COUNT || ' months') :: INTERVAL
-                        ) IS NULL
-                    )
-                "
-            );
+            ->whereRaw('s.count < '.$scheduledJobClass::COUNT);
+        
+            if ($scheduledJobClass::COUNT > 0) {
+                return $query->whereRaw("s.last_processed_at IS NULL OR (s.last_processed_at + (s.count || ' months')::interval) > NOW()");
+            } else {
+                return $query->whereRaw("s.last_processed_at IS NULL");
+            }
     }
 }
