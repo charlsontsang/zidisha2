2f757b30a81df3e210f589bbdc7aa3bbd99b5f0d
Jonas De Taeye <jonasdt@fastmail.fm>
2014-08-08 09:30:28 +0200

    Make getQuery abstract

--- app/Zidisha/ScheduledJob/ScheduledJob.php
+++ app/Zidisha/ScheduledJob/ScheduledJob.php
@@ -3,7 +3,12 @@ namespace Zidisha\ScheduledJob;
 
 use Zidisha\ScheduledJob\Base\ScheduledJob as BaseScheduledJob;
 
-class ScheduledJob extends BaseScheduledJob
+abstract class ScheduledJob extends BaseScheduledJob
 {
     const COUNT = 1;
+
+    /**
+     * @return \Illuminate\Database\Query\Builder
+     */
+    abstract public function getQuery();
 }
