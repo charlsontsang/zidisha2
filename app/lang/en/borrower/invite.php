24d68221ae6b2711f7cd2c8fa19694953c368b07
SingularityGuy <hardikformail@gmail.com>
2014-08-12 00:39:28 +0530

    add lang tags

---
+++ app/lang/en/borrower/invite.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'not-eligible'                   => 'You are not currently eligible to send invite credits to new members, because',
+    'not-eligible-repayRate'         => 'your on-time repayment rate is below the minimum required to qualify for participation in the invite program.',
+    'not-eligible-invitee-repayRate' => 'less than 90% of your current invitees are on time with their repayments.',
+    'not-eligible-invitee-quota'     => 'you already have :maxInviteesWithoutPayment invitees who have not yet made payments.<br/><br/>
+To become eligible to issue more invite credits, you may wait for your current invitees to begin to make repayments, or if any of your invitees do not wish to raise loans, you may remove them from your invitee list to make room for new invitees.<br/><br/>
+You may view the status of your current invitees and remove inactive invitees here: <a href='#'>My Invited Members</a>',
+];
