7a366b38ba36de7c9a30a59f34c0607c2c78dedd
SingularityGuy <hardikformail@gmail.com>
2014-08-12 16:56:42 +0530

    translation file

--- app/lang/en/borrower/invite.php
+++ app/lang/en/borrower/invite.php
@@ -5,5 +5,24 @@ return [
     'not-eligible-invitee-repayRate' => 'less than 90% of your current invitees are on time with their repayments.',
     'not-eligible-invitee-quota'     => 'you already have :maxInviteesWithoutPayment invitees who have not yet made payments.<br/><br/>
 To become eligible to issue more invite credits, you may wait for your current invitees to begin to make repayments, or if any of your invitees do not wish to raise loans, you may remove them from your invitee list to make room for new invitees.<br/><br/>
-You may view the status of your current invitees and remove inactive invitees here: <a href='#'>My Invited Members</a>',
+You may view the status of your current invitees and remove inactive invitees here: <a href=\'#\'>My Invited Members</a>',
+    'invites-message' => 'Here is a list of all members who have accepted your email invite to join Zidisha. For each member you have invited, as long as that member maintains a :minRepaymentRate on-time monthly installment repayment rate, your maximum credit limit will increase by a bonus amount of :currencyCode //TODO.',
+    'success-rate' => 'Invited Member Success Rate',
+    'success-rate-tooltip' => 'This is the percentage of members you have invited who are meeting the on-time repayment rate standard.',
+    'bonus-earned' => 'Total Bonus Earned',
+    'bonus-earned-tooltip' => 'This is the total bonus for inviting new members that will be added to the maximum credit limit when you post your next loan application.  It is the number of your invited members who meet the on-time repayment rate standard, times the current invite bonus amount for your country.',
+    'name' => 'Name',
+    'email' => 'Email',
+    'status' => 'Status',
+    'repayment-rate' => 'On-Time Repayment Rate',
+    'bonus-credit' => 'Bonus Credit Earned',
+    'not-accepted' => 'Invite Not Yet Accepted',
+    'application-not-submitted' => 'Application Not Yet Submitted',
+    'application-pending-verification' => 'Application Pending Verification',
+    'application-decline' => 'Declined',
+    'application-pending-review' => 'Application Pending Review',
+    'no-loan' => 'Activated, No Loan Yet',
+    'fundRaising-loan' => 'FundRaising Loan',
+    'repaying-on-time' => 'Repaying On Time',
+    'past-due' => 'Past Due'
 ];
