b651e6e980a961f1e9f69277a3cf885ae1b76a71
Jonas De Taeye <jonasdt@fastmail.fm>
2014-08-13 18:33:28 +0200

    Remove obsolete method

--- app/Zidisha/Borrower/BorrowerService.php
+++ app/Zidisha/Borrower/BorrowerService.php
@@ -260,12 +260,6 @@ class BorrowerService
         $upload->delete();
     }
 
-    public function makeVolunteerMentor(Borrower $borrower)
-    {
-        $borrower->getUser()->setSubRole('volunteerMentorId');
-        $borrower->save();
-    }
-
     public function validateConnectingFacebookUser($facebookUser)
     {
         $checkUser = $this->userQuery
