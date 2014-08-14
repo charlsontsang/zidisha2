a1c620cf3dd0e3a7b4c3337fa1201dfa045d3ee9
Jonas De Taeye <jonasdt@fastmail.fm>
2014-08-14 19:13:38 +0200

    Fix (invite)transaction schema

--- app/Zidisha/Balance/InviteTransaction.php
+++ app/Zidisha/Balance/InviteTransaction.php
@@ -3,8 +3,21 @@
 namespace Zidisha\Balance;
 
 use Zidisha\Balance\Base\InviteTransaction as BaseInviteTransaction;
+use Zidisha\Currency\Money;
 
 class InviteTransaction extends BaseInviteTransaction
 {
+    public function getAmount()
+    {
+        return Money::create(parent::getAmount(), 'USD');
+    }
 
+    /**
+     * @param Money $money
+     * @return $this|InviteTransaction
+     */
+    public function setAmount($money)
+    {
+        return parent::setAmount($money->getAmount());
+    }
 }
--- app/Zidisha/Balance/Transaction.php
+++ app/Zidisha/Balance/Transaction.php
@@ -7,24 +7,25 @@ use Zidisha\Currency\Money;
 
 class Transaction extends BaseTransaction
 {
-    const FUND_UPLOAD       = 1;
-    const FUND_WITHDRAW     = 2;
-    const LOAN_SENT_LENDER  = 3;
-    const LOAN_BACK_LENDER  = 4;
-    const FEE               = 5;
-    const DISBURSEMENT      = 6;
-    const LOAN_BACK         = 7;
-    const GIFT_REDEEM       = 8;
-    const GIFT_PURCHASE     = 9;
-    const GIFT_DONATE       = 10;
-    const DONATION          = 11;
-    const PAYPAL_FEE        = 12;
-    const REFERRAL_DEBIT    = 13;
-    const REGISTRATION_FEE  = 14;
-    const REFERRAL_CODE     = 15;
-    const LOAN_BID          = 16;
-    const LOAN_OUTBID       = 17;
-    const STRIPE_FEE        = 18;
+    const FUND_UPLOAD          = 1;
+    const FUND_WITHDRAW        = 2;
+    const LOAN_SENT_LENDER     = 3;
+    const LOAN_BACK_LENDER     = 4;
+    const FEE                  = 5;
+    const DISBURSEMENT         = 6;
+    const LOAN_BACK            = 7;
+    const GIFT_REDEEM          = 8;
+    const GIFT_PURCHASE        = 9;
+    const GIFT_DONATE          = 10;
+    const DONATION             = 11;
+    const PAYPAL_FEE           = 12;
+    const REFERRAL_DEBIT       = 13;
+    const REGISTRATION_FEE     = 14;
+    const REFERRAL_CODE        = 15;
+    const LOAN_BID             = 16;
+    const LOAN_OUTBID          = 17;
+    const STRIPE_FEE           = 18;
+    const LENDER_INVITE_CREDIT = 19;
 
     const REFERRAL_CREDIT               = 1;
     const DONATE_BY_ADMIN               = 2;
@@ -46,6 +47,10 @@ class Transaction extends BaseTransaction
         return Money::create(parent::getAmount(), 'USD');
     }
 
+    /**
+     * @param Money $money
+     * @return $this|Transaction
+     */
     public function setAmount($money)
     {
         return parent::setAmount($money->getAmount());
