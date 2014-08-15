66084c56637a366a2c098135bd5e4e3df47e1a62
jkurnia <julia@zidisha.org>
2014-08-15 19:17:08 +0000

    add map link to loan profile

--- app/views/pages/loan.blade.php
+++ app/views/pages/loan.blade.php
@@ -90,7 +90,7 @@
                                 <strong>{{{ $loan->getBorrower()->getName() }}}</strong>
                                 <br/>
                                 <strong>
-                                    {{ $loan->getBorrower()->getProfile()->getCity() }},
+                                    <a href="https://www.google.com/maps/place/{{ $loan->getBorrower()->getProfile()->getCity() }},+{{ $loan->getBorrower()->getCountry()->getName() }}/">{{ $loan->getBorrower()->getProfile()->getCity() }}</a>,
                                     {{ $loan->getBorrower()->getCountry()->getName() }}
                                 </strong>
                                 <br/>
