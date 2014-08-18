6fbbd88b2004490c6a613fe1a2c1dda4bcfd5ace
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-08-18 21:16:46 +0530

    Add language file for auto-lending

---
+++ app/lang/en/lender/auto-lending.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+  'preferences' => [
+      'active' => '',
+      'minimum-interest-rate' => '',
+      'minimum-interest-rate-other' => 'set you minimum interest rate',
+      'preference' => '',
+  ]  
+];
