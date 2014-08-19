4aa5256d664a005459b5c457410e9fbb245d1f0e
jkurnia <julia@zidisha.org>
2014-08-19 15:49:19 +0000

    fix floating footer

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,53 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="favicon.ico">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="shortcut icon" href="favicon.ico">
 
-    <title>Zidisha |  @yield('page-title')</title>
+        <title>Zidisha |  @yield('page-title')</title>
 
 
-    <!-- Bootstrap core CSS -->
-    <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet">
+        <!-- Bootstrap core CSS -->
+        <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet">
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
 
-    <!-- Mixpanel -->
-    {{ \Zidisha\Vendor\Mixpanel::headScript() }}
-</head>
-<body>
+        <!-- Mixpanel -->
+        {{ \Zidisha\Vendor\Mixpanel::headScript() }}
+    </head>
+    <body>
 
-    @include('partials.header')
-    
-    @yield('content-top')
+        @include('partials.header')
+        
+        @yield('content-top')
 
-    <div class="container">
-        @include('partials/_flash')
-        @yield('content')
-    </div>
+        <div class="container">
+            @include('partials/_flash')
+            @yield('content')
+        </div>
 
-    @yield('content-bottom')
+        @yield('content-bottom')
 
-    @include('partials.footer')
+        @include('partials.footer')
 
-    @if(!\Auth::check())
-        @include('partials.login-modal')
-    @endif
-    
-    <script src="{{ asset('assets/vendor/jquery/dist/jquery.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('assets/js/zidisha.js') }}"></script>
-    <script>$('.flash-modal').modal();</script>
-    @yield('script-footer')
-    <!-- Mixpanel -->
-    {{ \Zidisha\Vendor\Mixpanel::bodyScript() }}
+        @if(!\Auth::check())
+            @include('partials.login-modal')
+        @endif
+        
+        <script src="{{ asset('assets/vendor/jquery/dist/jquery.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('assets/js/zidisha.js') }}"></script>
+        <script>$('.flash-modal').modal();</script>
+        @yield('script-footer')
+        <!-- Mixpanel -->
+        {{ \Zidisha\Vendor\Mixpanel::bodyScript() }}
     </body>
 </html>
