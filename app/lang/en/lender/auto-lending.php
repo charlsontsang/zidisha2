1d9c7c60773c99040fe11520bca84bcba8856eab
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-08-19 21:48:31 +0530

    Add 'current-allocated' to language file

--- app/lang/en/lender/auto-lending.php
+++ app/lang/en/lender/auto-lending.php
@@ -6,5 +6,6 @@ return [
       'minimum-interest-rate' => '',
       'minimum-interest-rate-other' => 'set you minimum interest rate',
       'preference' => '',
+      'current-allocated' => '',
   ]  
 ];
