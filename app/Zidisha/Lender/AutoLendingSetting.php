de655469d02a4b70a3f6c1d1cc64b32bff98b86e
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-08-19 21:49:14 +0530

    Add getter and setter for LenderCredit in AutoLendingSetting

--- app/Zidisha/Lender/AutoLendingSetting.php
+++ app/Zidisha/Lender/AutoLendingSetting.php
@@ -2,6 +2,7 @@
 
 namespace Zidisha\Lender;
 
+use Zidisha\Currency\Money;
 use Zidisha\Lender\Base\AutoLendingSetting as BaseAutoLendingSetting;
 
 class AutoLendingSetting extends BaseAutoLendingSetting
@@ -12,4 +13,14 @@ class AutoLendingSetting extends BaseAutoLendingSetting
     const HIGH_NO_COMMENTS = 4;
     const LOAN_RANDOM = 5;
     const AUTO_LEND_AS_PREV_LOAN  = 6;
+
+    public function setLenderCredit($money)
+    {
+        return parent::setLenderCredit($money->getAmount());        
+    }
+
+    public function getLenderCredit()
+    {
+        return Money::create(parent::getLenderCredit(), 'USD');
+    }
 }
