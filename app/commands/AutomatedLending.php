2eb3bca4468071d9dee45a479f9ae5d42fbea3e8
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-08-19 17:57:32 +0530

    Make new AutomatedLending command

---
+++ app/commands/AutomatedLending.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Console\Command;
+
+class AutomatedLending extends Command
+{
+
+    protected $name = 'AutomatedLending';
+
+    protected $description = 'Runs automated lending';
+
+    public function fire()
+    {
+        //TODO: implement fire();
+    }
+
+}
