05093776ca13fa7f0ebfcbb5d24f9e02550b6345
SingularityGuy <hardikformail@gmail.com>
2014-08-21 14:53:43 +0530

    add Facebook User Schema

---
+++ app/Zidisha/User/FacebookUserQuery.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Zidisha\User;
+
+use Zidisha\User\Base\FacebookUserQuery as BaseFacebookUserQuery;
+
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'facebook_users' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class FacebookUserQuery extends BaseFacebookUserQuery
+{
+
+} // FacebookUserQuery
