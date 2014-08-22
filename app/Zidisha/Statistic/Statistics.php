4f648041ed2a6cd6104cfe0c1fe45b2c7809b176
SingularityGuy <hardikformail@gmail.com>
2014-08-22 14:00:28 +0530

    add Statistics model in schema

---
+++ app/Zidisha/Statistic/Statistics.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Zidisha\Statistic;
+
+use Zidisha\Statistic\Base\Statistics as BaseStatistics;
+
+class Statistics extends BaseStatistics
+{
+
+}
---
+++ app/Zidisha/Statistic/StatisticsQuery.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Zidisha\Statistic;
+
+use Zidisha\Statistic\Base\StatisticsQuery as BaseStatisticsQuery;
+
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'statistics' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class StatisticsQuery extends BaseStatisticsQuery
+{
+
+} // StatisticsQuery
