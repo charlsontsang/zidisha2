a4877014713a8eca171a9944532753d89b372359
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-08-24 01:09:51 +0530

    Add Money getters and setters for ForgivenLoanShare

--- app/Zidisha/Loan/ForgivenLoanShare.php
+++ app/Zidisha/Loan/ForgivenLoanShare.php
@@ -2,9 +2,35 @@
 
 namespace Zidisha\Loan;
 
+use Zidisha\Currency\Money;
 use Zidisha\Loan\Base\ForgivenLoanShare as BaseForgivenLoanShare;
 
 class ForgivenLoanShare extends BaseForgivenLoanShare
 {
 
+    public function setAmount($money)
+    {
+        return parent::setAmount($money->getAmount());
+    }
+
+    /**
+     * @return Money
+     */
+    public function getAmount()
+    {
+        return Money::create(parent::getAmount(), 'USD');
+    }
+
+    public function setUsdAmount($money)
+    {
+        return parent::setUsdAmount($money->getAmount());
+    }
+
+    /**
+     * @return Money
+     */
+    public function getUsdAmount()
+    {
+        return Money::create(parent::getUsdAmount(), 'USD');
+    }
 }
