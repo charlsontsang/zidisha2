c81ca044ee82cd52df69a1828f6f35b55a157908
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-08-23 20:24:45 +0530

    Add ForgivenLoanShare model and query class

---
+++ app/Zidisha/Loan/ForgivenLoanShare.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Zidisha\Loan;
+
+use Zidisha\Loan\Base\ForgivenLoanShare as BaseForgivenLoanShare;
+
+class ForgivenLoanShare extends BaseForgivenLoanShare
+{
+
+}
---
+++ app/Zidisha/Loan/ForgivenLoanShareQuery.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Zidisha\Loan;
+
+use Zidisha\Loan\Base\ForgivenLoanShareQuery as BaseForgivenLoanShareQuery;
+
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'forgiven_loan_share' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class ForgivenLoanShareQuery extends BaseForgivenLoanShareQuery
+{
+
+} // ForgivenLoanShareQuery
