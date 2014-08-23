adcc059f915bd1d3f4d4d1badbc075f251b7c66e
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-08-23 17:56:24 +0530

    Add allow-forgive-loan view file

---
+++ app/views/admin/loan/allow-forgive-loan.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.master')
+
+@section('page-title')
+Allow Forgive Loans
+@stop
+
+@section('content')
+<div class="page-header">
+    <h1>Allow Forgive Loans</h1>
+</div>
+
+<a href="{{route('admin:forgiven-loan:index')}}?countryCode={{ $country->getCountryCode() }}">See Forgiven Loans</a>
+<hr/>
+
+<div>
+    {{ BootstrapForm::open(['route' => ['admin:post:allow-forgive-loan', $country->getId()]]) }}
+    
+    {{ BootstrapForm::select('country', $borrowerCountries, $country->getCountryCode(), ['label' => 'Select Country', 'id' => 'countrySelect']) }}
+
+    {{ BootstrapForm::select('loan', $loans, null,['label' => 'Select Country']) }}
+
+    {{ BootstrapForm::textarea('comment', null, ['label' => 'Comment']) }}
+
+    {{ BootstrapForm::submit('Allow Forgiveness') }}
+
+    {{ BootstrapForm::close() }}
+</div>
+@stop
+
+@section('script-footer')
+<script>
+   $('#countrySelect').change(function(e){
+       window.location.href = '{{ route('admin:allow-forgive-loan').'?countryCode=' }}' +  $("#countrySelect option:selected").attr('value');
+   });
+</script>
+@stop
+
