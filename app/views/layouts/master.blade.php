76f013614f1d87337a13c680605ed38795e67b90
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-08-25 21:23:22 +0530

    Add correct bootstrap-growl to project

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -45,7 +45,7 @@
         <script src="{{ asset('assets/vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
         <script src="{{ asset('assets/vendor/bootstrap-3-datepicker/js/bootstrap-datepicker.js') }}"></script>
         <script src="{{ asset('assets/js/zidisha.js') }}"></script>
-        <script src="{{ asset('assets/vendor/bootstrap-growl/jquery.bootstrap-growl.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/remarkable-bootstrap-growl/bootstrap-growl.min.js') }}"></script>
         <script>$('.flash-modal').modal();</script>
         @yield('script-footer')
         @include('partials/_flash')
