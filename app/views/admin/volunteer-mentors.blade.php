18c0767dd20cc1930ac7e816b89ed9da7872ca7d
SingularityGuy <hardikformail@gmail.com>
2014-08-26 19:40:57 +0530

    add new fileds in VM page

--- app/views/admin/volunteer-mentors.blade.php
+++ app/views/admin/volunteer-mentors.blade.php
@@ -23,8 +23,11 @@ Volunteer Mentors
     <thead>
     <tr>
         <th>Borrower</th>
-        <th>Location</th>
-        <th>Date Joined</th>
+        <th>City / Village</th>
+        <th>Number of Assigned Members</th>
+        <th>Assigned Member Status</th>
+        <th>VM Repayment Status</th>
+        <th>Notes</th>
         <th>Volunteer Mentor Status</th>
     </tr>
     </thead>
@@ -33,10 +36,10 @@ Volunteer Mentors
     <tr>
         <td><a href="{{ route('lender:public-profile', $borrower->getUser()->getUserName()) }}">{{
                 $borrower->getFirstName() }} {{ $borrower->getLastName() }}</a>
-            <p>{{ $borrower->getUser()->getUsername() }}</p>
+            <p>{{ $borrower->getProfile()->getPhoneNumber() }}</p>
             <p>{{ $borrower->getUser()->getEmail() }}</p>
         </td>
-        <td>{{ $borrower->getCountry()->getName() }}</td>
+        <td>{{ $borrower->getProfile()->getCity() }}</td>
         <td>{{ $borrower->getUser()->getJoinedAt()->format('M j, Y') }}</td>
         <td>
             <a href="{{ route('admin:remove:volunteer-mentor', $borrower->getId()) }}">Remove Volunteer Mentor</a>
