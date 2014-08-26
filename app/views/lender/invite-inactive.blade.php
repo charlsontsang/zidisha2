21de0a95732e96cd8dfe74e83ef1ef42f21cf131
jkurnia <julia@zidisha.org>
2014-08-26 20:39:57 +0000

    format inactive lending program message

--- app/views/lender/invite-inactive.blade.php
+++ app/views/lender/invite-inactive.blade.php
@@ -5,18 +5,17 @@ Invite Your Friends To Zidisha
 @stop
 
 @section('content')
-<div style="text-align: center;">
-    <h2>
-        We're sorry, the new member invite program is currently inactive.
-    </h2>
-</div>
-
-<div class="post">
-    <div class="entry" style="text-align: center;margin-top: 20px;">
-        <div>
-            It will be resumed when new invite credits become available.
-            &nbsp;&nbsp;
-            <a href="{{ route('lender:how-it-works') }}">Learn more</a>
+<div class="row">
+    <div class="col-md-8 col-md-offset-2 info-page">
+        <div style="text-align: center; margin: 50px;">
+            <h2>
+                We're sorry, the new lender invite program is currently inactive.
+            </h2>
+            <p>
+                It will be resumed when new invite credits become available.
+            </p>
+                <a href="{{ route('lender:how-it-works') }}">How the invite program works</a>
+            </p>
         </div>
     </div>
 </div>
