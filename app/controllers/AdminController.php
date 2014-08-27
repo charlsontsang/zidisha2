b8cc01f4a24c5a95250c49ff4f1ee2e999bfc16a
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-08-27 23:41:04 +0530

    Add lender activate, deactivate, delete routes

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -193,13 +193,132 @@ class AdminController extends BaseController
     public function getLenders()
     {
         $form = $this->lendersForm;
-        $page = Request::query('page') ? : 1;
+        $page = Request::query('page') ?: 1;
 
         $paginator = $form->getQuery()
             ->orderById()
             ->paginate($page, 3);
 
-        return View::make('admin.lenders', compact('paginator', 'form'));
+        $totalLenders = LenderQuery::create()
+            ->count();
+        
+        $activeLenders = LenderQuery::create()
+            ->useUserQuery()
+                ->filterByActive(true)
+            ->endUse()
+            ->count();
+        
+        $activeLendersInPastTwoMonths = LenderQuery::create()
+            ->useUserQuery()
+                ->filterByLastLoginAt(Carbon::now()->subMonths(2), Criteria::GREATER_THAN)
+            ->endUse()
+            ->count();
+        
+        
+        $lenderUsingAutomatedLending = LenderQuery::create()
+            ->useAutoLendingSettingQuery()
+                ->filterByActive(true)
+            ->endUse()
+            ->count();
+        
+        $totalLenderCredit  = DB::select(
+            'SELECT SUM(amount) AS total
+             FROM transactions
+             WHERE user_id IN (SELECT id FROM users WHERE users.sub_role = 0)'
+        );
+        $totalLenderCredit = $totalLenderCredit[0]->total;
+        
+        return View::make(
+            'admin.lenders',
+            compact(
+                'paginator',
+                'form',
+                'totalLenders',
+                'activeLenders',
+                'activeLendersInPastTwoMonths',
+                'lenderUsingAutomatedLending',
+                'totalLenderCredit'
+            )
+        );
+    }
+
+    public function postLastCheckInEmail($lenderId)
+    {
+        $lender = LenderQuery::create()
+            ->findOneById($lenderId);
+
+        if(!$lender) {
+            App::abort(404, 'Lender with this ID not found.');
+        }
+        
+        if (!Input::has('lastCheckInEmail')){
+            App::abort(404, 'Please select a proper date');            
+        }        
+        
+        $lastCheckInEmailDate = Input::get('lastCheckInEmail');
+        $lender->setLastCheckInEmail($lastCheckInEmailDate);
+        $lender->save();
+        
+        \Flash::success('Last check in email date added.');
+        return Redirect::back();
+    }
+    
+    public function postDeleteLender($lenderId)
+    {
+        $lender = LenderQuery::create()
+            ->findOneById($lenderId);
+        
+        if(!$lender) {
+            App::abort(404, 'Lender with this ID not found.');
+        }
+
+        $user = $lender->getUser();
+        $lender->delete();
+        $user->delete();
+
+        \Flash::success('Lender Deleted');
+        return Redirect::back();
+    }
+
+
+    public function postDeactivateLender($lenderId)
+    {
+        $lender = LenderQuery::create()
+            ->findOneById($lenderId);
+
+        if(!$lender) {
+            App::abort(404, 'Lender with this ID not found.');
+        }
+        
+        $user = $lender->getUser();
+        $user->setActive(false);
+        $user->save();
+        
+        $lender->setActive(false);
+        $lender->save();
+        
+        \Flash::success('Lender Deactivated');
+        return Redirect::back();
+    }
+
+    public function postActivateLender($lenderId)
+    {
+        $lender = LenderQuery::create()
+            ->findOneById($lenderId);
+
+        if(!$lender) {
+            App::abort(404, 'Lender with this ID not found.');
+        }
+
+        $user = $lender->getUser();
+        $user->setActive(true);
+        $user->save();
+
+        $lender->setActive(true);
+        $lender->save();
+
+        \Flash::success('Lender Activated');
+        return Redirect::back();
     }
 
     public function getVolunteers()
--- app/views/admin/lenders.blade.php
+++ app/views/admin/lenders.blade.php
@@ -6,6 +6,20 @@ Lenders
 
 @section('content')
 
+<h2>Lender Statistics</h2>
+<div>
+    Total lenders : {{ $totalLenders }}
+    <br/>
+    Active lenders : {{ $activeLenders }}
+    <br/>
+    Logged in during past 2 months : {{ $activeLendersInPastTwoMonths }}
+    <br/>
+    Number of lenders using automated lending : {{ $lenderUsingAutomatedLending }}
+    <br/>
+    Total lender credit available : {{ $totalLenderCredit }}
+</div>
+
+
 <div>
     {{ BootstrapForm::open(array('route' => 'admin:lenders', 'translationDomain' => 'lenders', 'method' => 'get')) }}
     {{ BootstrapForm::populate($form) }}
@@ -22,7 +36,10 @@ Lenders
     <tr>
         <th>Lender</th>
         <th>Location</th>
-        <th>To Do</th>
+        <th>Date Joined</th>
+        <th>Last Login</th>
+        <th>Last check in email</th>
+        <th>Status</th>
     </tr>
     </thead>
     <tbody>
@@ -33,8 +50,58 @@ Lenders
             <p>{{ $lender->getUser()->getUsername() }}</p>
             <p>{{ $lender->getUser()->getEmail() }}</p>
         </td>
-        <td>{{ $lender->getCountry()->getName() }}</td>
-        <td></td>
+        <td>
+            {{ $lender->getCountry()->getName() }}
+        </td>
+        <td>
+            {{ $lender->getUser()->getCreatedAt()->format('M d, Y') }}
+        </td>
+        <td>
+            @if($lender->getUser()->getLastLoginAt())
+                {{ $lender->getUser()->getLastLoginAt()->format('M d, Y') }}
+            @else
+                Lender has not logged in yet
+            @endif
+        </td>
+        
+        <td>
+         {{ BootstrapForm::open(array('route' => ['admin:last-check-in-email:lender', $lender->getId() ])) }}
+         {{ BootstrapForm::datepicker('lastCheckInEmail') }}
+         {{ BootstrapForm::submit('Submit') }}
+         {{ BootstrapForm::close() }}
+         
+         <br/>
+         <hr/>
+         Last check in email :
+         @if($lender->getLastCheckInEmail())
+            {{ $lender->getLastCheckInEmail()->format('m/d/Y') }}         
+         @else
+          Not available 
+         @endif
+        </td>
+        <td>
+            <p>
+                @if($lender->getUser()->getActive()) 
+                    Active
+                @else
+                    Inactive         
+                @endif             
+            </p>
+            
+            @if($lender->getUser()->getActive())
+               {{ BootstrapForm::open(array('route' => ['admin:deactivate:lender', $lender->getId() ])) }}
+               {{ BootstrapForm::submit('Deactivate') }}
+               {{ BootstrapForm::close() }}
+            @else
+               {{ BootstrapForm::open(array('route' => ['admin:activate:lender', $lender->getId() ])) }}
+               {{ BootstrapForm::submit('Activate') }}
+               {{ BootstrapForm::close() }}
+            @endif             
+            <br/>
+           {{ BootstrapForm::open(array('route' => ['admin:delete:lender', $lender->getId() ])) }}
+           {{ BootstrapForm::submit('Delete') }}
+           {{ BootstrapForm::close() }}
+        </td>
     </tr>
     @endforeach
     </tbody>
