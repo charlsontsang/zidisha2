7e607a0be6233762add82b19d6b2bafe2eda17eb
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-08-28 22:00:41 +0530

    Add follower mail in Lender mail language file

--- app/lang/en/lender/mails.php
+++ app/lang/en/lender/mails.php
@@ -2,12 +2,21 @@
 return [
     'new-loan-notification' => [
         'subject' => ':borrowerName has posted a new loan application!',
-        'body' => ':borrowerName fully repaid the loan you funded on :repayDate, and has just posted a new loan application!
+        'lender-body' => ':borrowerName fully repaid the loan you funded on :repayDate, and has just posted a new loan application!
 <br/><br/>
 You may view :borrowerName\'s current loan request at :loanUrl
 <br/><br/>
 Cheers,
 <br/><br/>
+The Zidisha Team',
+
+        'follower-body' => '
+One of the entrepreneurs you are following, :borrowerName, has just posted a new loan application!
+<br/><br/>
+You may view :borrowerName \'s current loan request at :loanUrl
+<br/><br/>
+Cheers,
+<br/><br/>
 The Zidisha Team'
-    ] 
+    ]
 ] ;
