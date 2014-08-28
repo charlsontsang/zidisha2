ccad2fc7de77481ede1f6591f36963e6d45980f1
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-08-28 16:41:07 +0530

    Add borrower mails language file

---
+++ app/lang/en/borrower/mails.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'loan-confirmation' =>  [
+      'subject' => 'Your Loan Application Has Been Published',
+      'loan-posted-body' => 'Dear :borrowerName,<br/><br/> 
+Congratulations!  Your loan application has been posted for funding.  Click <a href=":loanApplicationLink">here</a> to view your loan application page.<br/><br/>
+Please note that your application will be posted for a maximum of :loanApplicationDeadLine days, or until it is fully funded and you choose to accept the bids raised. You may edit your loan application page at any time using the <a href=":loanApplicationLink">Loan Application</a> page.<br/><br/>
+Best of luck in your endeavor,<br/><br/>
+The Zidisha Team'
+  ],
+];
+ 
