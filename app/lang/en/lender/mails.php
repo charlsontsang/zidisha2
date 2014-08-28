646965faa7a25407d6d6559235443f1b24aa4535
Bhargav Joshi <bhargavdjoshi@gmail.com>
2014-08-28 17:58:57 +0530

    Add lender mailer email language file

---
+++ app/lang/en/lender/mails.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'new-loan-notification' => [
+        'subject' => ':borrowerName has posted a new loan application!',
+        'body' => ':borrowerName fully repaid the loan you funded on :repayDate, and has just posted a new loan application!
+<br/><br/>
+You may view :borrowerName\'s current loan request at :loanUrl
+<br/><br/>
+Cheers,
+<br/><br/>
+The Zidisha Team'
+    ] 
+] ;
