63973716aba0c76834330d6bf2a30dfff57c4a0c
jkurnia <julia@zidisha.org>
2014-08-29 20:41:41 +0000

    initial styling of stats page

--- app/views/pages/statistics.blade.php
+++ app/views/pages/statistics.blade.php
@@ -6,107 +6,109 @@ Statistics
 
 
 @section('content')
+<div class="row">
+    <div class="col-md-8 col-md-offset-2 info-page">
+        <div class="page-header">
+            <h1>Statistics</h1>
+        </div>
 
-<div class="page-header">
-    <h1>Statistics</h1>
-     <br/>
-    <h4>Community Statistics</h4>
-    What the whole Zidisha community has achieved so far
-    <br/><br/>
+        <h2>Community Statistics</h2>
+        <p><em>What the whole Zidisha community has achieved so far</em></p>
 
-    <p>Loan money raised:<i class="fa fa-info-circle raisedAmount" data-toggle="tooltip"></i>
-    <br/>
-    USD {{ number_format($totalStatistics['disbursed_amount'], 0, ".", ","); }}
-    </p>
-    <br/>
-    <p>Loan projects funded: <i class="fa fa-info-circle loansFunded" data-toggle="tooltip"></i>
-    <br/>
-    {{ number_format($totalStatistics['raised_count'], 0, ".", ","); }}
-    </p>
-    <br/>
-    <p>Lenders joined:
-    <br/>
-    {{ number_format($totalStatistics['lenders_count'], 0, ".", ","); }}
-    </p><br/>
-    <p>Borrowers joined:
-    <br/>
-    {{ number_format($totalStatistics['borrowers_count'], 0, ".", ",") }}
-    </p><br/>
-    <p>Total members:
-    <br/>
-    {{ number_format($totalStatistics['lenders_count'] + $totalStatistics['borrowers_count'], 0, ".", ",") }}
-    </p><br/>
-    <p>Countries represented by Zidisha members:
-    <br/>
-    {{ number_format($totalStatistics['countries_count'], 0, ".", ",") }}
-    </p><br/>
+        <p>Loan money raised:<i class="fa fa-info-circle raisedAmount" data-toggle="tooltip"></i>
+        <br/>
+        USD {{ number_format($totalStatistics['disbursed_amount'], 0, ".", ","); }}
+        </p>
+        <br/>
+        <p>Loan projects funded:<i class="fa fa-info-circle loansFunded" data-toggle="tooltip"></i>
+        <br/>
+        {{ number_format($totalStatistics['raised_count'], 0, ".", ","); }}
+        </p>
+        <br/>
+        <p>Lenders joined:
+        <br/>
+        {{ number_format($totalStatistics['lenders_count'], 0, ".", ","); }}
+        </p><br/>
+        <p>Borrowers joined:
+        <br/>
+        {{ number_format($totalStatistics['borrowers_count'], 0, ".", ",") }}
+        </p><br/>
+        <p>Total members:
+        <br/>
+        {{ number_format($totalStatistics['lenders_count'] + $totalStatistics['borrowers_count'], 0, ".", ",") }}
+        </p><br/>
+        <p>Countries represented by Zidisha members:
+        <br/>
+        {{ number_format($totalStatistics['countries_count'], 0, ".", ",") }}
+        </p><br/>
 
-    <h4>Lending Statistics</h4>
-    <p>Use the dropdowns below to get filtered performance statistics for all loans funded since our founding in 2009.</p>
-    <br/>
-    Display data for loans disbursed within:
-    <div class="btn btn-default btn-filter" target="#filter-time-periods">
-        {{ $selectedTimePeriod ? array_get($timePeriods, $selectedTimePeriod) : 'All time' }}
-        <i class="fa fa-fw fa-caret-down"></i>
+        <h2>Lending Statistics</h2>
+        <p><em>Use the dropdowns below to get filtered performance statistics for all loans funded since our founding in 2009.</em></p>
+        <span style="font-size: 18px; font-weight: 300;">Display data for loans disbursed within:&nbsp;&nbsp;&nbsp;
+            <div class="btn btn-default btn-filter" target="#filter-time-periods">
+                {{ $selectedTimePeriod ? array_get($timePeriods, $selectedTimePeriod) : 'All time' }}
+               <i class="fa fa-fw fa-caret-down"></i>
+            </div>
+        </span>
+        <br/><br/>
+        <span style="font-size: 18px; font-weight: 300;">Display data for loans in:&nbsp;&nbsp;&nbsp;
+            <div class="btn btn-default btn-filter" target="#filter-countries">
+                {{ $selectedCountry ? $selectedCountry->getName() : 'All Countries' }}
+               <i class="fa fa-fw fa-caret-down"></i>
+            </div>
+        </span>
+        <br/><br/>
+        <p>Loan money raised:<i class="fa fa-info-circle raisedAmountFiltered" data-toggle="tooltip"></i>
+        <br/>
+        USD {{ number_format($lendingStatistics['disbursed_amount'], 0, ".", ","); }}</p>
+        <br/>
+        <p>Loan projects funded:<i class="fa fa-info-circle loansFundedFiltered" data-toggle="tooltip"></i><br/>
+        {{ number_format($lendingStatistics['raised_count'], 0, ".", ","); }}</p>
+        <br/>
+        <p>Average lender interest:<i class="fa fa-info-circle lenderInterest" data-toggle="tooltip"></i>
+        <br/>
+        {{ number_format($lendingStatistics['average_lender_interest'], 1, ".", ","); }}%</p>
+        <br/>
+        @if ($lendingStatistics['repaid_amount'])
+            <p>Principal repaid:<i class="fa fa-info-circle principalRepaid" data-toggle="tooltip"></i>
+            <br/>
+            USD {{ number_format($lendingStatistics['repaid_amount'], 0, ".", ","); }}
+            ( {{ number_format($lendingStatistics['repaid_rate'], 1, ".", ","); }}% of amount disbursed)
+            </p>
+            <br/>
+        @endif
+        @if ($lendingStatistics['outstanding_on_time_amount'])
+            <p>Principal held by borrowers repaying on time (within 30-day threshold):<i class="fa fa-info-circle principalRepaidOnTime" data-toggle="tooltip"></i>
+            <br/>
+            USD {{ number_format($lendingStatistics['outstanding_on_time_amount'], 0, ".", ",") }}
+            ( {{ number_format($lendingStatistics['outstanding_on_time_rate'], 1, ".", ","); }}% of amount disbursed)
+            </p>
+            <br/>
+        @endif
+        @if ($lendingStatistics['outstanding_late_amount'])
+            <p>Principal held by borrowers more than 30 days past due with scheduled repayments:<i class="fa fa-info-circle principalRepaidDue" data-toggle="tooltip"></i>
+            <br/>
+            USD {{ number_format($lendingStatistics['outstanding_late_amount'], 0, ".", ",") }}
+            ( {{ number_format($lendingStatistics['outstanding_late_rate'], 1, ".", ","); }}% of amount disbursed)
+            </p>
+            <br/>
+        @endif
+        @if ($lendingStatistics['forgiven_amount'])
+             <p>Principal that has been forgiven by lenders:<i class="fa fa-info-circle principalForgiven" data-toggle="tooltip"></i>
+             <br/>
+             USD {{ number_format($lendingStatistics['forgiven_amount'], 0, ".", ","); }}
+             ( {{ number_format($lendingStatistics['forgiven_rate'], 1, ".", ","); }}% of amount disbursed)
+             </p>
+             <br/>
+        @endif
+        @if ($lendingStatistics['written_off_amount'])
+            <p>Principal that has been written off:<i class="fa fa-info-circle principalWrittenOff" data-toggle="tooltip"></i>
+            <br/>
+            USD {{ number_format($lendingStatistics['written_off_amount'], 0, ".", ","); }}
+            ( {{ number_format($lendingStatistics['written_off_rate'], 1, ".", ","); }}% of amount disbursed)
+        @endif
+        <p>Want to dive deeper? You can see the individual loan reports that provided the raw data for these statistics <a href="https://www.zidisha.org/index.php?p=114">here, TODO</a>.</p>
     </div>
-    <br/>
-    <br/>Display data for loans in:
-    <div class="btn btn-default btn-filter" target="#filter-countries">
-        {{ $selectedCountry ? $selectedCountry->getName() : 'All Countries' }}
-        <i class="fa fa-fw fa-caret-down"></i>
-    </div>
-    <br/><br/>
-    <p>Loan money raised: <i class="fa fa-info-circle raisedAmountFiltered" data-toggle="tooltip"></i>
-    <br/>
-    USD {{ number_format($lendingStatistics['disbursed_amount'], 0, ".", ","); }}</p>
-    <br/>
-    <p>Loan projects funded: <i class="fa fa-info-circle loansFundedFiltered" data-toggle="tooltip"></i><br/>
-    {{ number_format($lendingStatistics['raised_count'], 0, ".", ","); }}</p>
-    <br/>
-    <p>Average lender interest: <i class="fa fa-info-circle lenderInterest" data-toggle="tooltip"></i>
-    <br/>
-    {{ number_format($lendingStatistics['average_lender_interest'], 1, ".", ","); }}%</p>
-    <br/>
-    @if ($lendingStatistics['repaid_amount'])
-        <p>Principal repaid: <i class="fa fa-info-circle principalRepaid" data-toggle="tooltip"></i>
-        <br/>
-        USD {{ number_format($lendingStatistics['repaid_amount'], 0, ".", ","); }}
-        ( {{ number_format($lendingStatistics['repaid_rate'], 1, ".", ","); }}% of amount disbursed)
-        </p>
-        <br/>
-    @endif
-    @if ($lendingStatistics['outstanding_on_time_amount'])
-        <p>Principal held by borrowers repaying on time (within 30-day threshold): <i class="fa fa-info-circle principalRepaidOnTime" data-toggle="tooltip"></i>
-        <br/>
-        USD {{ number_format($lendingStatistics['outstanding_on_time_amount'], 0, ".", ",") }}
-        ( {{ number_format($lendingStatistics['outstanding_on_time_rate'], 1, ".", ","); }}% of amount disbursed)
-        </p>
-        <br/>
-    @endif
-    @if ($lendingStatistics['outstanding_late_amount'])
-        <p>Principal held by borrowers more than 30 days past due with scheduled repayments: <i class="fa fa-info-circle principalRepaidDue" data-toggle="tooltip"></i>
-        <br/>
-        USD {{ number_format($lendingStatistics['outstanding_late_amount'], 0, ".", ",") }}
-        ( {{ number_format($lendingStatistics['outstanding_late_rate'], 1, ".", ","); }}% of amount disbursed)
-        </p>
-        <br/>
-    @endif
-    @if ($lendingStatistics['forgiven_amount'])
-         <p>Principal that has been forgiven by lenders: <i class="fa fa-info-circle principalForgiven" data-toggle="tooltip"></i>
-         <br/>
-         USD {{ number_format($lendingStatistics['forgiven_amount'], 0, ".", ","); }}
-         ( {{ number_format($lendingStatistics['forgiven_rate'], 1, ".", ","); }}% of amount disbursed)
-         </p>
-         <br/>
-    @endif
-    @if ($lendingStatistics['written_off_amount'])
-        <p>Principal that has been written off: <i class="fa fa-info-circle principalWrittenOff" data-toggle="tooltip"></i>
-        <br/>
-        USD {{ number_format($lendingStatistics['written_off_amount'], 0, ".", ","); }}
-        ( {{ number_format($lendingStatistics['written_off_rate'], 1, ".", ","); }}% of amount disbursed)
-    @endif
-    Want to dive deeper? You can see the individual loan reports that provided the raw data for these statistics <a href="https://www.zidisha.org/index.php?p=114">here, TODO</a>.
-    <br/><br/>
 </div>
 
 <div id="filter-countries" class="hide">
