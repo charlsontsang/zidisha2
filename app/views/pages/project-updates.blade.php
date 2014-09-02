bbe99200f10e95ab1db3c8c6d7aa08ca683f3d0e
jkurnia <julia@zidisha.org>
2014-09-02 21:00:46 +0000

    add nav menus to community pages

--- app/views/pages/project-updates.blade.php
+++ app/views/pages/project-updates.blade.php
@@ -6,7 +6,14 @@ Project Updates
 
 @section('content')
 <div class="row">
-    <div class="col-md-8 col-md-offset-2">
+    <div class="col-sm-3 col-md-4">
+        <ul class="nav side-menu" role="complementary">
+          <h4>Community</h4>
+          @include('partials.nav-links.community-links')
+        </ul>
+    </div>
+
+    <div class="col-sm-9 col-md-8 info-page">
         <div class="page-header">
             <h1>Project Updates</h1>
         </div>
