33e920f9b0110d5ee84222e01637f9cef000704a
jkurnia <julia@zidisha.org>
2014-09-02 21:07:51 +0000

    add lender dashboard links

--- app/views/pages/our-story.blade.php
+++ app/views/pages/our-story.blade.php
@@ -7,14 +7,14 @@ Our Story
 
 @section('content')
 <div class="row">
-        <div class="col-sm-3 col-md-4">
-            <ul class="nav side-menu" role="complementary">
-              <h4>About</h4>
-              @include('partials.nav-links.about-links')
-            </ul>
-        </div>
+    <div class="col-sm-3 col-md-4">
+        <ul class="nav side-menu" role="complementary">
+          <h4>About</h4>
+          @include('partials.nav-links.about-links')
+        </ul>
+    </div>
 
-        <div class="col-sm-9 col-md-8 info-page">
+    <div class="col-sm-9 col-md-8 info-page">
         <div class="page-header">
             <h1>Our Story</h1>
         </div>
