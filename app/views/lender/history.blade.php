a540ac975fff0d22a0f9951201072b37d8cce3c8
jkurnia <julia@zidisha.org>
2014-09-04 00:03:24 +0000

    add sidemenu template to transaction history

--- app/views/lender/history.blade.php
+++ app/views/lender/history.blade.php
@@ -1,63 +1,56 @@
-@extends('layouts.master')
+@extends('layouts.side-menu')
 
 @section('page-title')
 Transaction History
 @stop
 
-@section('content')
-<div class="row">
-    <div class="col-sm-3 col-md-4">
-        <ul class="nav side-menu" role="complementary">
-          <h4>Quick Links</h4>
-            @include('partials.nav-links.lender-links')       
-          </ul>
-    </div>
+@section('menu-title')
+Quick Links
+@stop
 
-    <div class="col-sm-9 col-md-8 info-page">
-        <div class="page-header">
-            <h1>Transaction History</h1>
-        </div>
+@section('menu-links')
+@include('partials.nav-links.lender-links')
+@stop
 
-        <p>Current lending credit: <strong>{{ $currentBalance }}</strong></p>
+@section('page-content')
+<p>Current lending credit: <strong>{{ $currentBalance }}</strong></p>
 
-        <br/><br/>
+<br/><br/>
 
-        <table class="table table-striped no-more-tables">
-            <thead>
-                <tr>
-                    <th>
-                        Date
-                    </th>
-                    <th>
-                        Description
-                    </th>
-                    <th>
-                        Amount
-                    </th>
-                    <th>
-                        Balance
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-            @foreach($paginator as $transaction)
-                <tr>
-                    <td data-title="Date">{{ $transaction->getTransactionDate()->format('M j, Y') }}</td>
-                    <td data-title="Description">
-                        @if($transaction->getLoanId())
-                            <a href="{{ route('loan:index', $transaction->getLoanId()) }}" target="_blank">{{ $transaction->getDescription() }}</a>
-                        @else
-                            {{ $transaction->getDescription() }}
-                        @endif
-                    </td>
-                    <td data-title="Amount">{{ $transaction->getAmount()->getAmount() }}</td>
-                    <td data-title="Balance">{{ $currentBalancePage->getAmount() }}</td>
-                </tr>
-            <?php $currentBalancePage = $currentBalancePage->subtract($transaction->getAmount()); ?>
-            @endforeach
-            </tbody>
-        </table>
-        {{ BootstrapHtml::paginator($paginator)->links() }}
-    </div>
-</div>
+<table class="table table-striped no-more-tables">
+    <thead>
+        <tr>
+            <th>
+                Date
+            </th>
+            <th>
+                Description
+            </th>
+            <th>
+                Amount
+            </th>
+            <th>
+                Balance
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+    @foreach($paginator as $transaction)
+        <tr>
+            <td data-title="Date">{{ $transaction->getTransactionDate()->format('M j, Y') }}</td>
+            <td data-title="Description">
+                @if($transaction->getLoanId())
+                    <a href="{{ route('loan:index', $transaction->getLoanId()) }}" target="_blank">{{ $transaction->getDescription() }}</a>
+                @else
+                    {{ $transaction->getDescription() }}
+                @endif
+            </td>
+            <td data-title="Amount">{{ $transaction->getAmount()->getAmount() }}</td>
+            <td data-title="Balance">{{ $currentBalancePage->getAmount() }}</td>
+        </tr>
+    <?php $currentBalancePage = $currentBalancePage->subtract($transaction->getAmount()); ?>
+    @endforeach
+    </tbody>
+</table>
+{{ BootstrapHtml::paginator($paginator)->links() }}
 @stop
