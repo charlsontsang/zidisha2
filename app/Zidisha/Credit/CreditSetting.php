4e373eef4659971a5409d4dcb947a02f173eae31
Jonas De Taeye <jonasdt@fastmail.fm>
2014-09-05 20:38:48 +0200

    Add comment_credit constant

--- app/Zidisha/Credit/CreditSetting.php
+++ app/Zidisha/Credit/CreditSetting.php
@@ -7,6 +7,7 @@ use Zidisha\Credit\Base\CreditSetting as BaseCreditSetting;
 class CreditSetting extends BaseCreditSetting
 {
 
+    const COMMENT_CREDIT = 1;
     const ON_TIME_REPAYMENT_CREDIT = 2;
     const BORROWER_INVITE_CREDIT = 3;
 }
