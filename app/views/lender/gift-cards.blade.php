6e5e33ec93aa9253e8fb20e74e027d064a288573
jkurnia <julia@zidisha.org>
2014-09-05 00:22:37 +0000

    fix code styling

--- app/views/lender/gift-cards.blade.php
+++ app/views/lender/gift-cards.blade.php
@@ -22,41 +22,41 @@ Gift Cards
 
 <div class="row giftcards">
     <div class="col-sm-2">
-        <div id="giftcard-4-1" class="giftcard_thumbnail">
+        <div id="giftcard-4-1" class="giftcard-thumbnail">
             <label for="giftcard_template_radio-4-1" ><img src="{{ asset('assets/images/gift-card/image4.png'); }}"></label>
             <input name="template" id="giftcard_template_radio-4-1" value="4" checked="checked" type="radio">
         </div>
-        <div id="giftcard-3-1" class="giftcard_thumbnail">
+        <div id="giftcard-3-1" class="giftcard-thumbnail">
             <label for="giftcard_template_radio-3-1" ><img src="{{ asset('assets/images/gift-card/image3.png'); }}"></label>
             <input name="template" id="giftcard_template_radio-3-1" value="3" type="radio">
         </div>
     </div>
     <div class="col-sm-2">
-        <div id="giftcard-2-1" class="giftcard_thumbnail">
+        <div id="giftcard-2-1" class="giftcard-thumbnail">
             <label for="giftcard_template_radio-2-1" ><img src="{{ asset('assets/images/gift-card/image2.png'); }}"/></label>
             <input name="template" id="giftcard_template_radio-2-1" value="2" type="radio">
         </div>
-        <div id="giftcard-5-1" class="giftcard_thumbnail">
+        <div id="giftcard-5-1" class="giftcard-thumbnail">
             <label for="giftcard_template_radio-5-1" ><img src="{{ asset('assets/images/gift-card/image5.png'); }}"/></label>
             <input name="template" id="giftcard_template_radio-5-1" value="5" type="radio">
         </div>
     </div>
     <div class="col-sm-2">
-        <div id="giftcard-6-1" class="giftcard_thumbnail">
+        <div id="giftcard-6-1" class="giftcard-thumbnail">
             <label for="giftcard_template_radio-6-1" ><img src="{{ asset('assets/images/gift-card/image6.png'); }}"/></label>
             <input name="template" id="giftcard_template_radio-6-1" value="6" type="radio">
         </div>
-        <div id="giftcard-7-1" class="giftcard_thumbnail">
+        <div id="giftcard-7-1" class="giftcard-thumbnail">
             <label for="giftcard_template_radio-7-1" ><img src="{{ asset('assets/images/gift-card/image7.png'); }}"/></label>
             <input name="template" id="giftcard_template_radio-7-1" value="7" type="radio">
         </div>
     </div>
     <div class="col-sm-2">
-        <div id="giftcard-8-1" class="giftcard_thumbnail">
+        <div id="giftcard-8-1" class="giftcard-thumbnail">
             <label for="giftcard_template_radio-8-1" ><img src="{{ asset('assets/images/gift-card/image8.png'); }}"/></label>
             <input name="template" id="giftcard_template_radio-8-1" value="8" type="radio">
         </div>
-        <div id="giftcard-9-1" class="giftcard_thumbnail">
+        <div id="giftcard-9-1" class="giftcard-thumbnail">
             <label for="giftcard_template_radio-9-1" ><img src="{{ asset('assets/images/gift-card/image9.png'); }}"/></label>
             <input name="template" id="giftcard_template_radio-9-1" value="9" type="radio">
         </div>
@@ -102,11 +102,11 @@ Gift Cards
 @section('script-footer')
 <script type="text/javascript">
     $(document).ready(function() {
-        $('.giftcard_thumbnail').on('click', function () {
-            $(this).parents().parents('.giftcards').find('div.giftcard_thumbnail').removeClass('selected');
+        $('.giftcard-thumbnail').on('click', function () {
+            $(this).parents().parents('.giftcards').find('div.giftcard-thumbnail').removeClass('selected');
             $(this).addClass('selected');
         });
-        $('div.giftcard_thumbnail:first').addClass('selected');
+        $('div.giftcard-thumbnail:first').addClass('selected');
     });
 </script>
 @stop
