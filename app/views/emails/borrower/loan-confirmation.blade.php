7f3849fc96a8810623900f403c4cd61c5a8e1f11
SingularityGuy <hardikformail@gmail.com>
2014-09-05 20:35:58 +0530

    Refactor BorrowerMailer::sendVerificationMail

--- app/views/emails/borrower/loan-confirmation.blade.php
+++
@@ -1,1 +0,0 @@
-@lang('borrower.mails.loan-confirmation.loan-posted-body', compact('borrowerName', 'loanApplicationLink', 'loanApplicationDeadLine'))
--- app/views/emails/borrower/verification.blade.php
+++
@@ -1,4 +0,0 @@
-<h1>Please click on this link to verify your email address:</h1>
-<br/>
-<hr/>
-<a href="{{ route('borrower:verify', $hashCode) }}">Verify</a>
