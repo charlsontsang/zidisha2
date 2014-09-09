627c29ecf997d09da2ef64be68ff57bc0980921c
jkurnia <julia@zidisha.org>
2014-09-09 22:51:00 +0000

    add vm pages to borrower links

--- app/views/partials/nav-links/borrower-links.blade.php
+++ app/views/partials/nav-links/borrower-links.blade.php
@@ -7,4 +7,10 @@
 <li class="@if (Request::segment(1) == 'credit') active @else '' @endif "><a href="{{ route('borrower:credit') }}">Current Credit</a></li>
 <li class="@if (Request::segment(1) == 'history') active @else '' @endif "><a href="{{ route('borrower:history') }}">Payment History</a></li>
 <li class="@if (Request::segment(1) == 'feature-criteria') active @else '' @endif "><a href="{{ route('page:loan-feature-criteria') }}">How To Have Your Loan Featured</a></li>
+	@if(Auth::getUser()->isVolunteerMentor())
+		<h4>Volunteer Mentor Pages</h4>
+		<li class="@if (Request::segment(1) == 'volunteer-mentor-guidelines') active @else '' @endif "><a href="{{ route('page:volunteer-mentor-guidelines') }}">Volunteer Mentor Guidelines</a></li>
+		<li class="@if (Request::segment(1) == 'volunteer-mentor-code-of-ethics') active @else '' @endif "><a href="{{ route('page:volunteer-mentor-code-of-ethics') }}">Volunteer Mentor Code of Ethics</a></li>
+		<li class="@if (Request::segment(1) == 'volunteer-mentor-faq') active @else '' @endif "><a href="{{ route('page:volunteer-mentor-faq') }}">Volunteer Mentor FAQ</a></li>
+	@endif
 @endif
