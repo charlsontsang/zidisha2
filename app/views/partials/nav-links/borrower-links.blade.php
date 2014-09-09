e8620a8799a57752f8fb13b8afe6650fde680621
jkurnia <julia@zidisha.org>
2014-09-09 23:08:52 +0000

    fix vm heading in drop-down menu

--- app/views/partials/nav-links/borrower-links.blade.php
+++ app/views/partials/nav-links/borrower-links.blade.php
@@ -8,7 +8,7 @@
 <li class="@if (Request::segment(2) == 'history') active @else '' @endif "><a href="{{ route('borrower:history') }}">Payment History</a></li>
 <li class="@if (Request::segment(1) == 'feature-criteria') active @else '' @endif "><a href="{{ route('page:loan-feature-criteria') }}">How To Have Your Loan Featured</a></li>
 	@if(Auth::getUser()->isVolunteerMentor())
-		<h4>Volunteer Mentor Pages</h4>
+		<li><h4>Volunteer Mentor Pages</h4></li>
 		<li class="@if (Request::segment(1) == 'volunteer-mentor-guidelines') active @else '' @endif "><a href="{{ route('page:volunteer-mentor-guidelines') }}">Volunteer Mentor Guidelines</a></li>
 		<li class="@if (Request::segment(1) == 'volunteer-mentor-code-of-ethics') active @else '' @endif "><a href="{{ route('page:volunteer-mentor-code-of-ethics') }}">Volunteer Mentor Code of Ethics</a></li>
 		<li class="@if (Request::segment(1) == 'volunteer-mentor-faq') active @else '' @endif "><a href="{{ route('page:volunteer-mentor-faq') }}">Volunteer Mentor FAQ</a></li>
