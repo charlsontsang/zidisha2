1717f79b2ff4394a70bb87629236c2f6b03d9e25
Jonas De Taeye <jonasdt@fastmail.fm>
2014-09-09 18:48:55 +0200

    Fix button

--- app/views/partials/comments/partial/delete.blade.php
+++ app/views/partials/comments/partial/delete.blade.php
@@ -2,6 +2,6 @@
     {{ BootstrapForm::open(array('action' => [ $controller.'@postDelete', 'id' => $receiver->getId() ], 'translationDomain' => 'borrower.comments')) }}
     {{ BootstrapForm::label(\Lang::get('borrower.comments.delete-comment')) }}
     {{ BootstrapForm::hidden('comment_id', $comment->getId()) }}
-    {{ BootstrapForm::submit('actions.delete', ['class' => 'btn-danger', 'data-loading-text' => \Lang::get('borrower.comments.loading-text.delete')]) }}
+    {{ BootstrapForm::submit('actions.delete', ['class' => 'btn btn-danger', 'data-loading-text' => \Lang::get('borrower.comments.loading-text.delete')]) }}
     {{ BootstrapForm::close() }}
 </div>
