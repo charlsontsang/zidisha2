86f2843f8783b5907c88a83057ce79f00d8c8b30
jkurnia <julia@zidisha.org>
2014-09-09 23:00:17 +0000

    fix borrower sidemenu active class assignment

--- app/views/partials/nav-links/borrower-links.blade.php
+++ app/views/partials/nav-links/borrower-links.blade.php
@@ -1,11 +1,11 @@
 @if(Auth::check() && Auth::getUser()->isBorrower())
-<li class="@if (Request::segment(1) == 'dashboard') active @else '' @endif "><a href="{{ route('borrower:dashboard') }}">Dashboard</a></li>
+<li class="@if (Request::segment(2) == 'dashboard') active @else '' @endif "><a href="{{ route('borrower:dashboard') }}">Dashboard</a></li>
 <li><a href="{{ route('loan:index', Auth::getUser()->getBorrower()->getActiveLoanId()) }}">Public Loan Page</a></li>
-<li class="@if (Request::segment(1) == 'edit-profile') active @else '' @endif "><a href="{{ route('borrower:edit-profile') }}">Edit Profile</a></li>
-<li class="@if (Request::segment(1) == 'invite') active @else '' @endif "><a href="{{ route('borrower:invite') }}">Send Invites</a></li>
-<li class="@if (Request::segment(1) == 'invites') active @else '' @endif "><a href="{{ route('borrower:invites') }}">Your Invites</a></li>
-<li class="@if (Request::segment(1) == 'credit') active @else '' @endif "><a href="{{ route('borrower:credit') }}">Current Credit</a></li>
-<li class="@if (Request::segment(1) == 'history') active @else '' @endif "><a href="{{ route('borrower:history') }}">Payment History</a></li>
+<li class="@if (Request::segment(2) == 'edit-profile') active @else '' @endif "><a href="{{ route('borrower:edit-profile') }}">Edit Profile</a></li>
+<li class="@if (Request::segment(2) == 'invite') active @else '' @endif "><a href="{{ route('borrower:invite') }}">Send Invites</a></li>
+<li class="@if (Request::segment(2) == 'invites') active @else '' @endif "><a href="{{ route('borrower:invites') }}">Your Invites</a></li>
+<li class="@if (Request::segment(2) == 'credit') active @else '' @endif "><a href="{{ route('borrower:credit') }}">Current Credit</a></li>
+<li class="@if (Request::segment(2) == 'history') active @else '' @endif "><a href="{{ route('borrower:history') }}">Payment History</a></li>
 <li class="@if (Request::segment(1) == 'feature-criteria') active @else '' @endif "><a href="{{ route('page:loan-feature-criteria') }}">How To Have Your Loan Featured</a></li>
 	@if(Auth::getUser()->isVolunteerMentor())
 		<h4>Volunteer Mentor Pages</h4>
