0c5565cc19f3cb17fd002cdfd8edcdf4a2af045e
jkurnia <julia@zidisha.org>
2014-09-09 23:13:52 +0000

    fix lender sidemenu active class assignment

--- app/views/partials/nav-links/lender-links.blade.php
+++ app/views/partials/nav-links/lender-links.blade.php
@@ -1,15 +1,15 @@
-<li class="@if (Request::segment(1) == 'dashboard') active @else '' @endif "><a href="{{ route('lender:dashboard') }}">Dashboard</a></li>
+<li class="@if (Request::segment(2) == 'dashboard') active @else '' @endif "><a href="{{ route('lender:dashboard') }}">Dashboard</a></li>
 <li><a href="{{ route('lender:loans') }}">Your Loans</a></li>
-<li class="@if (Request::segment(1) == 'following') active @else '' @endif "><a href="{{ route('lender:following') }}">Following</a></li>
+<li class="@if (Request::segment(2) == 'following') active @else '' @endif "><a href="{{ route('lender:following') }}">Following</a></li>
 <li><a href="{{ route('lender:public-profile', Auth::getUser()->getUsername()) }}">View Profile</a></li>
-<li class="@if (Request::segment(1) == 'edit-profile') active @else '' @endif "><a href="{{ route('lender:edit-profile') }}">Edit Profile</a></li>
-<li class="@if (Request::segment(1) == 'preference') active @else '' @endif "><a href="{{ route('lender:preference') }}">Account Preferences</a></li>
+<li class="@if (Request::segment(3) == 'edit') active @else '' @endif "><a href="{{ route('lender:edit-profile') }}">Edit Profile</a></li>
+<li class="@if (Request::segment(2) == 'preferences') active @else '' @endif "><a href="{{ route('lender:preference') }}">Account Preferences</a></li>
 <li><a href="{{ route('lender:gift-cards') }}">Gift Cards</a></li>
-<li class="@if (Request::segment(1) == 'gift-cards:track') active @else '' @endif "><a href="{{ route('lender:gift-cards:track') }}">Track Gift Cards</a></li>
+<li class="@if (Request::segment(2) == 'gift-cards') active @else '' @endif "><a href="{{ route('lender:gift-cards:track') }}">Track Gift Cards</a></li>
 <li><a href="{{ route('lender:invite') }}">Invite Friends</a></li>
-<li class="@if (Request::segment(1) == 'history') active @else '' @endif "><a href="{{ route('lender:history') }}">Transaction History</a></li>
-<li class="@if (Request::segment(1) == 'funds') active @else '' @endif "><a href="{{ route('lender:funds') }}">Transfer Funds</a></li>
-<li class="@if (Request::segment(1) == 'auto-lending') active @else '' @endif "><a href="{{ route('lender:auto-lending') }}">Autolending</a></li>
+<li class="@if (Request::segment(2) == 'history') active @else '' @endif "><a href="{{ route('lender:history') }}">Transaction History</a></li>
+<li class="@if (Request::segment(2) == 'funds') active @else '' @endif "><a href="{{ route('lender:funds') }}">Transfer Funds</a></li>
+<li class="@if (Request::segment(2) == 'auto-lending') active @else '' @endif "><a href="{{ route('lender:auto-lending') }}">Autolending</a></li>
 <!-- TO DO: move this logic to a controller -->
 <?php
 $lendingGroups = Zidisha\Lender\LendingGroupQuery::create()->getLendingGroupsForLender(\Auth::user()->getLender());
