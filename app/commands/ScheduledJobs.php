07df1c30e9b41647075e53d47bf9452cbb09c06a
SingularityGuy <hardikformail@gmail.com>
2014-09-10 15:42:39 +0530

    Test CronToRepay job

--- app/commands/ScheduledJobs.php
+++ app/commands/ScheduledJobs.php
@@ -16,13 +16,13 @@ class ScheduledJobs extends Command
     protected $classes = [
 //        'Zidisha\ScheduledJob\AbandonedUser',
 //        'Zidisha\ScheduledJob\LoanAboutToExpireReminder',
-        'Zidisha\ScheduledJob\LoanFinalArrear',
+//        'Zidisha\ScheduledJob\LoanFinalArrear',
 //        'Zidisha\ScheduledJob\AgainRepaymentReminder',
 //        'Zidisha\ScheduledJob\LoanFirstArrear',
 //        'Zidisha\ScheduledJob\RepaymentReminder',
 //        'Zidisha\ScheduledJob\MonthlyLoanArrear',
 //        'Zidisha\ScheduledJob\NewLenderIntro',
-//        'Zidisha\ScheduledJob\CronToRepay',
+        'Zidisha\ScheduledJob\CronToRepay',
 //        'Zidisha\ScheduledJob\UnusedFunds',
     ];
     
