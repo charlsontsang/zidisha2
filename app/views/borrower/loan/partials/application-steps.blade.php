e12428f503d108ec8610eaf1a3f6ac2de0fbdb0e
jkurnia <julia@zidisha.org>
2014-09-14 22:30:22 +0000

    application steps

--- app/views/borrower/loan/partials/application-steps.blade.php
+++ app/views/borrower/loan/partials/application-steps.blade.php
@@ -4,7 +4,7 @@
     <?php $i = 1 ?>
     @foreach(array_slice($steps, 0, 4) as $name => $options)
     <div class="col-xs-3 wizard-step {{ $options['class'] }}">
-        <div class="text-center wizard-stepnum">Step {{ $i }}</div>
+        <div class="text-center wizard-stepnum">@lang("borrower.loan-application.title.step") {{ $i }}</div>
         <div class="progress"><div class="progress-bar"></div></div>
         <a href="{{ action($options['action']) }}" class="wizard-dot"></a>
         <div class="wizard-info text-center">@lang("borrower.loan-application.title.$name-page")</div>
