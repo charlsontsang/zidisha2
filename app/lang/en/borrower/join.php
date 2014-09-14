b80cf453f6d7bcaed28bda09fdddb99614a9c60e
jkurnia <julia@zidisha.org>
2014-09-14 20:55:21 +0000

    small fix

--- app/lang/en/borrower/join.php
+++ app/lang/en/borrower/join.php
@@ -64,7 +64,7 @@ return [
     'emails' => [
         'subject' => [
             'confirmation'                  => 'Zidisha application submitted',
-            'volunteer-mentor-confirmation' => 'New assigned member :name',
+            'volunteer-mentor-confirmation' => 'New member: :name',
 
         ]
     ],
