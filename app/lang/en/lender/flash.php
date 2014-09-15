112ebbf3c3f4a1c6b6f4a656dd37701e0b7299f1
jkurnia <julia@zidisha.org>
2014-09-15 10:25:26 -0400

    update error message

--- app/lang/en/lender/flash.php
+++ app/lang/en/lender/flash.php
@@ -3,7 +3,7 @@
 return [
     'preferences' => [
         'success'        => 'Success! Your account settings are updated.',
-        'error'          => 'some error occurred',
+        'error'          => 'Sorry, there was an error. Please try again, and notify service@zidisha.org in case of further difficulty.',
         'stats-projects' => ':count Project|:count Projects',
     ],
 ];
