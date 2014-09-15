a157f98d74d439cff8e1f7dc7284d52800eca73f
jkurnia <julia@zidisha.org>
2014-09-15 10:55:09 -0400

    move comment labels from borrower to common directory

--- app/views/partials/comments/partial/delete.blade.php
+++ app/views/partials/comments/partial/delete.blade.php
@@ -1,7 +1,19 @@
 <div class="comment-form" style="display: none;" data-comment-action="delete" >
     {{ BootstrapForm::open(array('action' => [ $controller.'@postDelete', 'id' => $receiver->getId() ], 'translationDomain' => 'borrower.comments')) }}
-    {{ BootstrapForm::label(\Lang::get('borrower.comments.delete-comment')) }}
+    {{ BootstrapForm::label(\Lang::get('common.comments.
+@lang('borrower.menu.links-title')
+@stop
+
+@section('menu-links')
+@include('partials.nav-links.borrower-links')
+@stopdelete-comment')) }}
     {{ BootstrapForm::hidden('comment_id', $comment->getId()) }}
-    {{ BootstrapForm::submit('actions.delete', ['class' => 'btn btn-danger', 'data-loading-text' => \Lang::get('borrower.comments.loading-text.delete')]) }}
+    {{ BootstrapForm::submit('actions.delete', ['class' => 'btn btn-danger', 'data-loading-text' => \Lang::get('common.comments.
+@lang('borrower.menu.links-title')
+@stop
+
+@section('menu-links')
+@include('partials.nav-links.borrower-links')
+@stoploading-text.delete')]) }}
     {{ BootstrapForm::close() }}
 </div>
