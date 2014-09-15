2b25b9db19ce4c4afc63416e2e2ce22a0ee7ebe4
jkurnia <julia@zidisha.org>
2014-09-15 11:26:55 -0400

    move error message to common validation file

--- app/lang/en/lender/flash.php
+++ app/lang/en/lender/flash.php
@@ -2,8 +2,6 @@
 
 return [
     'preferences' => [
-        'success'        => 'Success! Your account settings are updated.',
-        'error'          => 'Sorry, there was an error. Please try again, and notify service@zidisha.org in case of further difficulty.',
         'stats-projects' => ':count Project|:count Projects',
     ],
 ];
