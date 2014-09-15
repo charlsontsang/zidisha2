e33d1a8cad09fbaa40641226a252d009a234d123
jkurnia <julia@zidisha.org>
2014-09-15 12:48:36 -0400

    remove unused labels

--- app/lang/en/lender/shared-labels.php
+++ app/lang/en/lender/shared-labels.php
@@ -2,10 +2,8 @@
 
 return [
 
-    'follow'                    => 'Follow :name',
-    'follower-count'            => ':count follower|:count followers',
-    'follow-description'        => 'Receive an email when :firstName posts a comment or new loan application.',
-    'follow-login'              => 'Please <a href=":link">log in</a> to follow :name.',
     'update-preference-success' => 'Your preference has been saved.',
     'projects'                  => ':count Project|:count Projects',
+    'follower-count'            => ':count follower|:count followers',
+    
 ];
