5d9077d7e9621a1339d7c37536f62b1ad532a919
jkurnia <julia@zidisha.org>
2014-09-15 10:23:37 -0400

    remove obsolete admin mailer functions
    Conflicts:
    	app/Zidisha/Mail/AdminMailer.php

--- app/Zidisha/Payment/BalanceService.php
+++ app/Zidisha/Payment/BalanceService.php
@@ -68,7 +68,6 @@ class BalanceService
                 $this->transactionService->addWithdrawFundTransaction($con, $amount, $lender);
                 $withdrawalRequest->save($con);
             });
-        $this->adminMailer->sendWithdrawalRequestMail($withdrawalRequest->getAmount());
         $this->lenderMailer->sendWithdrawalRequestMail($lender, $withdrawalRequest);
         return $withdrawalRequest;
     }
