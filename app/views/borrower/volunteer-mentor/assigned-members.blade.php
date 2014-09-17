c1d1406f2d820f40ea998a6241d8f4f02fb5d826
SingularityGuy <hardikformail@gmail.com>
2014-09-17 16:36:06 +0530

    add content in blade file

--- app/views/borrower/volunteer-mentor/assigned-members.blade.php
+++ app/views/borrower/volunteer-mentor/assigned-members.blade.php
@@ -9,5 +9,36 @@
 <div class="page-header">
 <h1>@lang('borrower.text.assigned-members.title')</h1>
 </div>
+<br/><br/>
+
+<h3>@lang('borrower.text.assigned-members.pending')</h3>
+<p>@lang('borrower.text.assigned-members.pending-instructions')</p>
+<br/>
+<table class="table-striped">
+	<tbody>
+
+	</tbody>
+</table>
+<br/><br/><br/>
+
+<h3>@lang('borrower.text.assigned-members.arrears')</h3>
+<p>@lang('borrower.text.assigned-members.arrears-instructions')</p>
+<br/>
+<table class="table-striped">
+	<tbody>
+
+	</tbody>
+</table>
+<br/><br/><br/>
+
+<h3>@lang('borrower.text.assigned-members.current')</h3>
+<p>@lang('borrower.text.assigned-members.current-instructions')</p>
+<br/>
+<table class="table-striped">
+	<tbody>
+
+	</tbody>
+</table>
+<br/><br/><br/>
 @stop
 
