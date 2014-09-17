1cf1df6f11d1a7f757e6f6c6f9781a8cb2eef2e6
Jonas De Taeye <jonasdt@fastmail.fm>
2014-09-17 14:59:43 +0200

    Make admin menu a large drop down

--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -51,52 +51,51 @@
                 </li>
                 @endif
                 @if(Auth::check() && Auth::getUser()->isVolunteerOrAdmin())
-                <li class="dropdown">
+                <li class="dropdown dropdown-large">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        Manage Borrowers <b class="caret"></b>
+                        Admin <b class="caret"></b>
                     </a>
 
-                    <ul class="dropdown-menu">
-                        <li><a href="{{ route('admin:borrowers') }}">Look Up Borrower Account</a></li>
-                        <li><a href="{{ route('admin:volunteer-mentors') }}">Volunteer Mentors</a></li>
-                        <li><a href="{{ route('admin:add:volunteer-mentors') }}">Add Volunteer Mentors</a></li>
-                        <li><a href="{{ route('admin:borrower-activation') }}">Pending Activation</a></li>
-                        <li><a href="{{ route('admin:pending-disbursements') }}">Pending Disbursements</a></li>
-                        <li><a href="{{ route('admin:loans') }}">Find Loans</a></li>
-                        <li><a href="{{ route('admin:repayments') }}">Enter Repayments</a></li>
-                        <li><a href="{{ route('admin:loan-forgiveness:index') }}">Forgiven Loans</a></li>
-                        <li><a href="{{ route('admin:get:translation-feed') }}">Translation Feed</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        Manage Lenders <b class="caret"></b>
-                    </a>
-
-                    <ul class="dropdown-menu">
-                        <li><a href="{{ route('admin:lenders') }}">Look Up Lender Account</a></li>
-                        <li><a href="{{ route('admin:volunteers') }}">Active Staff</a></li>
-                        <li><a href="{{ route('admin:get:gift-cards') }}">Manage Gift Cards</a></li>
-                        <li><a href="{{ route('admin:get:withdrawal-requests') }}">Withdraw Requests</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        Admin<b class="caret"></b>
-                    </a>
-
-                    <ul class="dropdown-menu">
-                        <li><a href="{{ route('admin:dashboard') }}">Dashboard</a></li>
-                        <li><a href="{{ route('admin:translation:index') }}">Translations</a></li>
-                        @if(Auth::getUser()->isAdmin())
-                            <li><a href="{{ route('admin:mail:test-mails') }}">Test Emails</a></li>
-                        @endif
-                        <li><a href="{{ route('admin:sms:test-sms') }}">Test SMS</a></li>
-                        <li><a href="{{ route('admin:test:sift-science') }}">Test Sift Science</a></li>
-                        <li><a href="{{ route('admin:countries') }}">Countries</a></li>
-                        <li><a href="{{ route('admin:exchange-rates') }}">Exchange Rates</a></li>
-                        <li><a href="{{ route('admin:settings') }}">Other Settings</a></li>
-                        <li><a href="{{ route('logout') }}">Log Out</a></li>
+                    <ul class="dropdown-menu dropdown-menu-large row">
+                        <li class="col-sm-4">
+                            <ul>
+                                <li class="dropdown-header">Manage Borrowers</li>
+                                <li><a href="{{ route('admin:borrowers') }}">Look Up Borrower Account</a></li>
+                                <li><a href="{{ route('admin:volunteer-mentors') }}">Volunteer Mentors</a></li>
+                                <li><a href="{{ route('admin:add:volunteer-mentors') }}">Add Volunteer Mentors</a></li>
+                                <li><a href="{{ route('admin:borrower-activation') }}">Pending Activation</a></li>
+                                <li><a href="{{ route('admin:pending-disbursements') }}">Pending Disbursements</a></li>
+                                <li><a href="{{ route('admin:loans') }}">Find Loans</a></li>
+                                <li><a href="{{ route('admin:repayments') }}">Enter Repayments</a></li>
+                                <li><a href="{{ route('admin:loan-forgiveness:index') }}">Forgiven Loans</a></li>
+                                <li><a href="{{ route('admin:get:translation-feed') }}">Translation Feed</a></li>
+                            </ul>
+                        </li>
+                        <li class="col-sm-4">                
+                            <ul>
+                                <li class="dropdown-header">Manage Lenders</li>
+                                <li><a href="{{ route('admin:lenders') }}">Look Up Lender Account</a></li>
+                                <li><a href="{{ route('admin:volunteers') }}">Active Staff</a></li>
+                                <li><a href="{{ route('admin:get:gift-cards') }}">Manage Gift Cards</a></li>
+                                <li><a href="{{ route('admin:get:withdrawal-requests') }}">Withdraw Requests</a></li>
+                            </ul>
+                        </li>
+                        <li class="col-sm-4">
+                           <ul>
+                                <li class="dropdown-header">Other</li>
+                                <li><a href="{{ route('admin:dashboard') }}">Dashboard</a></li>
+                                <li><a href="{{ route('admin:translation:index') }}">Translations</a></li>
+                                @if(Auth::getUser()->isAdmin())
+                                    <li><a href="{{ route('admin:mail:test-mails') }}">Test Emails</a></li>
+                                @endif
+                                <li><a href="{{ route('admin:sms:test-sms') }}">Test SMS</a></li>
+                                <li><a href="{{ route('admin:test:sift-science') }}">Test Sift Science</a></li>
+                                <li><a href="{{ route('admin:countries') }}">Countries</a></li>
+                                <li><a href="{{ route('admin:exchange-rates') }}">Exchange Rates</a></li>
+                                <li><a href="{{ route('admin:settings') }}">Other Settings</a></li>
+                                <li><a href="{{ route('logout') }}">Log Out</a></li>
+                            </ul>
+                        </li>
                     </ul>
                 </li>
                 @endif
