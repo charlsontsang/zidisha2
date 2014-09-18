876c11dc7ecace47c75d1dc371f91c38b413ba1f
SingularityGuy <hardikformail@gmail.com>
2014-09-18 20:38:43 +0530

    Fix UserMailer and AdminMailer

--- app/Zidisha/Mail/UserMailer.php
+++ app/Zidisha/Mail/UserMailer.php
@@ -18,25 +18,24 @@ class UserMailer
 
     public function sentLendingGroupCommentNotification(LendingGroup $lendingGroup, Comment $comment, User $user, $images)
     {
-        $data = [
-            'parameters' => [
-                'groupName'  => $lendingGroup->getName(),
-                'message'    => $comment->getMessage(),
-                'byUserName' => $comment->getUser()->getUsername(),
-                'date'       => $comment->getCreatedAt()->format('d-m-Y'),
-                'groupLink'  => route('lender:group', $lendingGroup->getId()),
-                'images'     => $images,
-            ],
+        $parameters = [
+            'groupName'  => $lendingGroup->getName(),
+            'message'    => $comment->getMessage(),
+            'byUserName' => $comment->getUser()->getUsername(),
+            'date'       => $comment->getCreatedAt()->format('d-m-Y'),
+            'groupLink'  => route('lender:group', $lendingGroup->getId()),
+            'images'     => $images,
         ];
 
         $this->mailer->queue(
             'emails.label-template',
-            $data + [
+            [
                 'to'      => $user->getEmail(),
-                'label'   => 'lender.mails.lending-group-comment-notification.body',
+                'content' => \Lang::get('lender.mails.lending-group-comment-notification.body', $parameters, 'en'),
                 'subject' => \Lang::get(
                     'lender.mails.lending-group-comment-notification.subject',
-                    ['groupName' => $lendingGroup->getName()]
+                    ['groupName' => $lendingGroup->getName()],
+                    'en'
                 ),
             ]
         );
