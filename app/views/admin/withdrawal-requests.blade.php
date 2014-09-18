1ea43c182ec19150d873753e993e209c539f1d76
jkurnia <julia@zidisha.org>
2014-09-18 23:24:19 +0000

    fix merge conflict

--- app/views/admin/withdrawal-requests.blade.php
+++ app/views/admin/withdrawal-requests.blade.php
@@ -17,43 +17,40 @@ Withdraw Requests
 <table class="table table-striped">
     <thead>
     <tr>
-        <th></th>
-        <th>Date</th>
+        <th>Request Date</th>
         <th>Lender</th>
-        <th>Cumulative Amount</th>
+        <th>History</th>
         <th>PayPal Email</th>
         <th>Withdrawal Amount</th>
-        <th></th>
+        <th>Authorize</th>
     </tr>
     </thead>
     <tbody>
     @foreach($paginator as $request)
 
     <tr>
+        <td>{{ $request->getCreatedAt()->format('M j, Y') }}</td>
         <td>
-            @if(!$request->isPaid())
-            {{ BootstrapForm::checkbox('ids['.$request->getId().']', $request->getId(), null, ['class' => 'withdraw-checkbox']) }}
-            @endif
-        </td>
-        <td>{{ $request->getCreatedAt()->format('d-m-Y') }}</td>
-        <td><p><a href="{{ route('lender:public-profile', $request->getLender()->getId()) }}">
+            <p><a href="{{ route('lender:public-profile', $request->getLender()->getUser()->getUserName()) }}">
                 {{ $request->getLender()->getName() }}</a></p>
-        <p>{{ $request->getLender()->getUser()->getEmail() }}</p>
+            <p>{{ $request->getLender()->getUser()->getEmail() }}</p>
         </td>
         <td><p>Uploaded: {{ $uploaded[$request->getLenderId()] }}</p>
             <p>Repaid: {{ $repaid[$request->getLenderId()] }}</p>
             <p>Withdrawn: {{ $withdrawn[$request->getLenderId()]->multiply(-1) }}</p>
         </td>
         <td>{{ $request->getPaypalEmail() }}</td>
-        <td>{{ $request->getAmount() }}</td>
-        <td>
+        <td><p>
+                {{ $request->getAmount() }}
+            </p>
+            <p>
             @if(Auth::getUser()->isAdmin())
-                {{ BootstrapForm::open(['route' => ['admin:post:withdrawal-requests', $request->getId()]]) }}
-                {{ BootstrapForm::populate($form) }}
                 @if($request->isPaid())
                     Paid
                 @else
-                {{ BootstrapForm::submit('Pay') }}
+                    <a href="{{ route('admin:post:withdrawal-requests', $request->getId()) }}">
+                        Confirm payment
+                    </a>
                 @endif
             @else
                 @if($request->isPaid())
@@ -61,6 +58,12 @@ Withdraw Requests
                 @else
                     Not yet paid
                 @endif
+            @endif
+            </p>
+        </td>
+        <td>
+            @if(!$request->isPaid())
+                {{ BootstrapForm::checkbox('ids['.$request->getId().']', $request->getId(), null, ['class' => 'withdraw-checkbox', 'label' => '']) }}
             @endif
         </td>
     </tr>
