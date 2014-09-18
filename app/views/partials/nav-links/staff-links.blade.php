b27a1ed432081e9e2e00813f2f2684516a58a011
jkurnia <julia@zidisha.org>
2014-09-18 20:07:57 +0000

    update volunteer guide link

--- app/views/partials/nav-links/staff-links.blade.php
+++ app/views/partials/nav-links/staff-links.blade.php
@@ -1,4 +1,4 @@
-<li><a href="https://sites.google.com/a/zidisha.org/zidisha-staff/home/country-liaison-intern-guide">Volunteer Guide</a></li>
+<li><a href="https://sites.google.com/a/zidisha.org/zidisha-staff/home">Volunteer Guide</a></li>
 <li><a href="http://zidisha.org/forum/categories/volunteer-conversations.26/">Volunteer Forum</a></li>                                   
 <li class="@if (Request::segment(1) == 'lenders') active @else '' @endif "><a href="{{ route('admin:lenders') }}">Look Up Lender Account</a></li>
 <li class="@if (Request::segment(1) == 'borrowers') active @else '' @endif "><a href="{{ route('admin:borrowers') }}">Look Up Borrower Account</a></li>
