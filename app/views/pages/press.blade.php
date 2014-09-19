7cd7e527243fe8de70f2f91a3745c9081ad1476c
jkurnia <julia@zidisha.org>
2014-09-19 23:41:26 +0000

    transition press page to sidemenu template

--- app/views/pages/press.blade.php
+++ app/views/pages/press.blade.php
@@ -1,156 +1,150 @@
-@extends('layouts.master')
+@extends('layouts.side-menu')
 
 @section('page-title')
 Press
 @stop
 
-@section('content')
-<div class="row">
-        <div class="col-sm-3 col-md-4">
-            <ul class="nav side-menu" role="complementary">
-              <h4>About</h4>
-              @include('partials.nav-links.about-links')
-            </ul>
-        </div>
+@section('menu-title')
+About
+@stop
 
-        <div class="col-sm-9 col-md-8 info-page">
-        <div class="page-header">
-            <h1>Press</h1>
-        </div>
-            
-        <p>We’re super honored to have received great press from publications big and small, far and wide. Here are a few of our favorite shout outs.</p>
-        
-        <br/><br/>
+@section('menu-links')
+@include('partials.nav-links.about-links')
+@stop
 
-        <p><a href="http://blogs.wsj.com/venturecapital/2014/08/25/y-combinator-nonprofit-zidisha-changes-microfinance-equation/" target="_blank"><img src="{{ asset('assets/images/pages/press/wsj_logo.png'); }}" width="50%" /></a></p>
-        <br/>
-        <blockquote>
-            <p>"This type of lending is possible in Africa now because many people have access to the internet."</p>
-            <footer>
-                <cite><a href="http://blogs.wsj.com/venturecapital/2014/08/25/y-combinator-nonprofit-zidisha-changes-microfinance-equation/" target="_blank">The Wall Street Journal</a></cite>
-            </footer>
-        </blockquote>     
-            
-        <hr/>
-        
-        <p><a href="http://techcrunch.com/2014/01/23/zidisha-launches-a-kickstarter-style-micro-lending-platform-for-low-income-entrepreneurs-in-developing-countries/" target="_blank"><img src="{{ asset('assets/images/pages/press/tc_logo.png'); }}" width="20%" /></a></p>
-        <br/>
-        <blockquote>
-            <p>"By enabling the efficient flow of capital across international boundaries and wealth divisions, and by allowing lenders to connect with and send money directly to borrowers, peer-to-peer micro-lending has the ability to have an enormous impact — on a global scale."</p>
-            <footer>
-                <cite><a href="http://techcrunch.com/2014/01/23/zidisha-launches-a-kickstarter-style-micro-lending-platform-for-low-income-entrepreneurs-in-developing-countries/" target="_blank">TechCrunch</a></cite>
-            </footer>
-        </blockquote>     
-            
-        <hr/>
-        
-        <p><a href="http://paulbuchheit.blogspot.com/2014/03/help-me-distribute-100000-to-new.html" target="_blank"><img src="{{ asset('assets/images/pages/press/yc_logo.png'); }}" width="30%" /></a></p>
-        <br/>
-        <blockquote>
-            <p>"I'm excited to have Zidisha as one of our most recent Y Combinator funded non-profit organizations... Just as Airbnb connects travelers directly to hosts, Zidisha connects lenders directly to borrowers, providing not only an affordable loan, but also a personal connection."</p>
-            <footer>
-                <cite><a href="http://paulbuchheit.blogspot.com/2014/03/help-me-distribute-100000-to-new.html" target="_blank">Gmail Creator Paul Buchheit</a></cite>
-            </footer>
-        </blockquote>     
-            
-        <hr/>
+@section('page-content')
+<div class="info-page">
+    <p>We’re super honored to have received great press from publications big and small, far and wide. Here are a few of our favorite shout outs.</p>
+    
+    <br/><br/>
 
-        <p><a href="http://venturebeat.com/2014/03/25/meet-the-tech-for-good-startups-at-y-combinators-demo-day/" target="_blank"><img src="{{ asset('assets/images/pages/press/VB_logo.png'); }}" /></a></p>
-        <br/>
-        <blockquote>
-            <p>"Zidisha is cutting out the middlemen to enable direct lending to the entrepreneurs... So far, the company has processed $1.8 million in loans to 5,000 entrepreneurs in Africa and Asia, and only needs 10 percent in fees to cover its cost (that’s versus the 30-80 percent Kiva requires)."</p>
-            <footer>
-                <cite><a href="http://venturebeat.com/2014/03/25/meet-the-tech-for-good-startups-at-y-combinators-demo-day/" target="_blank">Venture Beat</a></cite>
-            </footer>
-        </blockquote>     
-            
-        <hr/>
-            
-        <p><a href="http://www.huffingtonpost.com/2014/03/04/paul-buchheit_n_4891950.html" target="_blank"><img src="{{ asset('assets/images/pages/press/HP_logo.png'); }}" width="50%" /></a></p>
-        <br/>
-        <blockquote>
-            <p>"Zidisha has committed to completely eliminating the middleman... The organization sees itself as an eBay-style platform because it allows borrowers and lenders to interact directly without involving anyone else."</p>
-            <footer>
-                <cite><a href="http://www.huffingtonpost.com/2014/03/04/paul-buchheit_n_4891950.html" target="_blank">The Huffington Post</a></cite>
-            </footer>
-        </blockquote>     
-            
-        <hr/>
-            
-        <p><a href="http://voices.mckinseyonsociety.com/the-crowdfunding-landscape/" target="_blank"><img src="{{ asset('assets/images/pages/press/MOS_logo.png'); }}" width="307" height="68" /></a></p>
-        <br/>
-        <blockquote>
-            <p>"Zidisha cuts out the middleman by allowing direct peer-to-peer global lending, which capitalizes on the expanding mobile-banking sector in emerging markets."</p>
-            <footer>
-                <cite><a href="http://voices.mckinseyonsociety.com/the-crowdfunding-landscape/" target="_blank">McKinsey On Society</a></cite>
-            </footer>
-        </blockquote>     
-            
-        <hr/>
-            
-        <p><a href="http://www.entrepreneur.com/article/223391" target="_blank"><img src="{{ asset('assets/images/pages/press/entrepreneur_logo.png'); }}" width="287" height="64" /></a></p>
-        <br/>
-        <blockquote>
-            <p>"Because there is no intermediary in charge of the loan, the cost of borrowing is much less than with other microfinance sites."</p>
-            <footer>
-                <cite><a href="http://www.entrepreneur.com/article/223391" target="_blank">Entrepreneur</a></cite>
-            </footer>
-        </blockquote>     
-            
-        <hr/>
-            
-        <p><a href="http://financialaccess.org/blog/2011/07/microfinance-without-mfi-zidisha-tests-boundaries-microlending-methodology" target="_blank"><img src="{{ asset('assets/images/pages/press/fai_logo.png'); }}" width="167" height="72" /></a></p>
-        <br/>
-        <blockquote>
-            <p>"Zidisha deserves attention for its own achievement. It has brought traditional microfinance clients into the same virtual space commonly thought to be the domain of the middle class, and demonstrated that this can be done sustainably."</p>
-            <footer>
-                <cite><a href="http://financialaccess.org/blog/2011/07/microfinance-without-mfi-zidisha-tests-boundaries-microlending-methodology" target="_blank">Financial Access Initiative</a></cite>
-            </footer>
-        </blockquote>     
-            
-        <hr/>
+    <p><a href="http://blogs.wsj.com/venturecapital/2014/08/25/y-combinator-nonprofit-zidisha-changes-microfinance-equation/" target="_blank"><img src="{{ asset('assets/images/pages/press/wsj_logo.png'); }}" width="50%" /></a></p>
+    <br/>
+    <blockquote>
+        <p>"This type of lending is possible in Africa now because many people have access to the internet."</p>
+        <footer>
+            <cite><a href="http://blogs.wsj.com/venturecapital/2014/08/25/y-combinator-nonprofit-zidisha-changes-microfinance-equation/" target="_blank">The Wall Street Journal</a></cite>
+        </footer>
+    </blockquote>     
         
-        <p><a href="http://www.washingtonpost.com/wp-dyn/content/article/2010/10/08/AR2010100806209.html?sub=AR" target="_blank"><img src="{{ asset('assets/images/pages/press/WPlogo.gif'); }}" width="318" height="51" /></a></p>
-        <br/>
-        <blockquote>
-            <p>"Zidisha forgoes using debt collectors, whose use can add upwards of 35 percent interest to the loans they service. Instead, the organization relies on its vetting process and the relationships it establishes with borrowers."</p>
-            <footer>
-                <cite><a href="http://www.washingtonpost.com/wp-dyn/content/article/2010/10/08/AR2010100806209.html?sub=AR" target="_blank">The Washington Post</a></cite>
-            </footer>
-        </blockquote>     
-            
-        <hr/>
-            
-        <p><a href="http://mashable.com/2012/04/24/zidisha/" target="_blank"><img src="{{ asset('assets/images/pages/press/mash_logo.png'); }}" width="213" height="51" /></a></p>
-        <br/>
-        <blockquote>
-            <p>"Zidisha is the world's first direct peer-to-peer micro-lending platform, providing Internet users the chance to directly loan money to entrepreneurs in the developing world."</p>
-            <footer>
-                <cite><a href="http://mashable.com/2012/04/24/zidisha/" target="_blank">Mashable</a></cite>
-            </footer>
-        </blockquote>     
-            
-        <hr/>
-            
-        <p><a href="http://one.org/blog/2012/06/26/zidisha-fostering-entrepreneurship-one-loan-at-a-time/" target="_blank"><img src="{{ asset('assets/images/pages/press/one_logo.png'); }}" width="116" height="98" /></a></p>
-        <br/>
-        <blockquote>
-            <p>"What makes this nonprofit unique is that because it is peer-to-peer, the interest rates for the entrepreneur are very low."</p>
-            <footer>
-                <cite><a href="http://one.org/blog/2012/06/26/zidisha-fostering-entrepreneurship-one-loan-at-a-time/" target="_blank">Bono's ONE Campaign</a></cite>
-            </footer>
-        </blockquote>     
-            
-        <hr/>
-            
-        <p><a href="http://algoso.org/2011/12/16/so-that-thing-kiva-just-started-doing-zidisha-was-already-doing-it/" target="_blank"><img src="{{ asset('assets/images/pages/press/FWW_logo.png'); }}" width="449" height="74" /></a></p>
-        <blockquote>
-            <p>"Zidisha’s success stands as a challenge to the standard microfinance model... they’ve shown that different target populations can be served in different ways.  Zidisha borrowers are aspiring middle class. Many of them are small business owners who form the backbone of a nation’s economy... As their business becomes more successful, this will have outsized impacts on their communities. Zidisha makes that possible."</p>
-            <footer>
-                <cite><a href="http://algoso.org/2011/12/16/so-that-thing-kiva-just-started-doing-zidisha-was-already-doing-it/" target="_blank">Find What Works</a></cite>
-            </footer>
-        </blockquote>     
-    </div>
+    <hr/>
+    
+    <p><a href="http://techcrunch.com/2014/01/23/zidisha-launches-a-kickstarter-style-micro-lending-platform-for-low-income-entrepreneurs-in-developing-countries/" target="_blank"><img src="{{ asset('assets/images/pages/press/tc_logo.png'); }}" width="20%" /></a></p>
+    <br/>
+    <blockquote>
+        <p>"By enabling the efficient flow of capital across international boundaries and wealth divisions, and by allowing lenders to connect with and send money directly to borrowers, peer-to-peer micro-lending has the ability to have an enormous impact — on a global scale."</p>
+        <footer>
+            <cite><a href="http://techcrunch.com/2014/01/23/zidisha-launches-a-kickstarter-style-micro-lending-platform-for-low-income-entrepreneurs-in-developing-countries/" target="_blank">TechCrunch</a></cite>
+        </footer>
+    </blockquote>     
+        
+    <hr/>
+    
+    <p><a href="http://paulbuchheit.blogspot.com/2014/03/help-me-distribute-100000-to-new.html" target="_blank"><img src="{{ asset('assets/images/pages/press/yc_logo.png'); }}" width="30%" /></a></p>
+    <br/>
+    <blockquote>
+        <p>"I'm excited to have Zidisha as one of our most recent Y Combinator funded non-profit organizations... Just as Airbnb connects travelers directly to hosts, Zidisha connects lenders directly to borrowers, providing not only an affordable loan, but also a personal connection."</p>
+        <footer>
+            <cite><a href="http://paulbuchheit.blogspot.com/2014/03/help-me-distribute-100000-to-new.html" target="_blank">Gmail Creator Paul Buchheit</a></cite>
+        </footer>
+    </blockquote>     
+        
+    <hr/>
+
+    <p><a href="http://venturebeat.com/2014/03/25/meet-the-tech-for-good-startups-at-y-combinators-demo-day/" target="_blank"><img src="{{ asset('assets/images/pages/press/VB_logo.png'); }}" /></a></p>
+    <br/>
+    <blockquote>
+        <p>"Zidisha is cutting out the middlemen to enable direct lending to the entrepreneurs... So far, the company has processed $1.8 million in loans to 5,000 entrepreneurs in Africa and Asia, and only needs 10 percent in fees to cover its cost (that’s versus the 30-80 percent Kiva requires)."</p>
+        <footer>
+            <cite><a href="http://venturebeat.com/2014/03/25/meet-the-tech-for-good-startups-at-y-combinators-demo-day/" target="_blank">Venture Beat</a></cite>
+        </footer>
+    </blockquote>     
+        
+    <hr/>
+        
+    <p><a href="http://www.huffingtonpost.com/2014/03/04/paul-buchheit_n_4891950.html" target="_blank"><img src="{{ asset('assets/images/pages/press/HP_logo.png'); }}" width="50%" /></a></p>
+    <br/>
+    <blockquote>
+        <p>"Zidisha has committed to completely eliminating the middleman... The organization sees itself as an eBay-style platform because it allows borrowers and lenders to interact directly without involving anyone else."</p>
+        <footer>
+            <cite><a href="http://www.huffingtonpost.com/2014/03/04/paul-buchheit_n_4891950.html" target="_blank">The Huffington Post</a></cite>
+        </footer>
+    </blockquote>     
+        
+    <hr/>
+        
+    <p><a href="http://voices.mckinseyonsociety.com/the-crowdfunding-landscape/" target="_blank"><img src="{{ asset('assets/images/pages/press/MOS_logo.png'); }}" width="307" height="68" /></a></p>
+    <br/>
+    <blockquote>
+        <p>"Zidisha cuts out the middleman by allowing direct peer-to-peer global lending, which capitalizes on the expanding mobile-banking sector in emerging markets."</p>
+        <footer>
+            <cite><a href="http://voices.mckinseyonsociety.com/the-crowdfunding-landscape/" target="_blank">McKinsey On Society</a></cite>
+        </footer>
+    </blockquote>     
+        
+    <hr/>
+        
+    <p><a href="http://www.entrepreneur.com/article/223391" target="_blank"><img src="{{ asset('assets/images/pages/press/entrepreneur_logo.png'); }}" width="287" height="64" /></a></p>
+    <br/>
+    <blockquote>
+        <p>"Because there is no intermediary in charge of the loan, the cost of borrowing is much less than with other microfinance sites."</p>
+        <footer>
+            <cite><a href="http://www.entrepreneur.com/article/223391" target="_blank">Entrepreneur</a></cite>
+        </footer>
+    </blockquote>     
+        
+    <hr/>
+        
+    <p><a href="http://financialaccess.org/blog/2011/07/microfinance-without-mfi-zidisha-tests-boundaries-microlending-methodology" target="_blank"><img src="{{ asset('assets/images/pages/press/fai_logo.png'); }}" width="167" height="72" /></a></p>
+    <br/>
+    <blockquote>
+        <p>"Zidisha deserves attention for its own achievement. It has brought traditional microfinance clients into the same virtual space commonly thought to be the domain of the middle class, and demonstrated that this can be done sustainably."</p>
+        <footer>
+            <cite><a href="http://financialaccess.org/blog/2011/07/microfinance-without-mfi-zidisha-tests-boundaries-microlending-methodology" target="_blank">Financial Access Initiative</a></cite>
+        </footer>
+    </blockquote>     
+        
+    <hr/>
+    
+    <p><a href="http://www.washingtonpost.com/wp-dyn/content/article/2010/10/08/AR2010100806209.html?sub=AR" target="_blank"><img src="{{ asset('assets/images/pages/press/WPlogo.gif'); }}" width="318" height="51" /></a></p>
+    <br/>
+    <blockquote>
+        <p>"Zidisha forgoes using debt collectors, whose use can add upwards of 35 percent interest to the loans they service. Instead, the organization relies on its vetting process and the relationships it establishes with borrowers."</p>
+        <footer>
+            <cite><a href="http://www.washingtonpost.com/wp-dyn/content/article/2010/10/08/AR2010100806209.html?sub=AR" target="_blank">The Washington Post</a></cite>
+        </footer>
+    </blockquote>     
+        
+    <hr/>
+        
+    <p><a href="http://mashable.com/2012/04/24/zidisha/" target="_blank"><img src="{{ asset('assets/images/pages/press/mash_logo.png'); }}" width="213" height="51" /></a></p>
+    <br/>
+    <blockquote>
+        <p>"Zidisha is the world's first direct peer-to-peer micro-lending platform, providing Internet users the chance to directly loan money to entrepreneurs in the developing world."</p>
+        <footer>
+            <cite><a href="http://mashable.com/2012/04/24/zidisha/" target="_blank">Mashable</a></cite>
+        </footer>
+    </blockquote>     
+        
+    <hr/>
+        
+    <p><a href="http://one.org/blog/2012/06/26/zidisha-fostering-entrepreneurship-one-loan-at-a-time/" target="_blank"><img src="{{ asset('assets/images/pages/press/one_logo.png'); }}" width="116" height="98" /></a></p>
+    <br/>
+    <blockquote>
+        <p>"What makes this nonprofit unique is that because it is peer-to-peer, the interest rates for the entrepreneur are very low."</p>
+        <footer>
+            <cite><a href="http://one.org/blog/2012/06/26/zidisha-fostering-entrepreneurship-one-loan-at-a-time/" target="_blank">Bono's ONE Campaign</a></cite>
+        </footer>
+    </blockquote>     
+        
+    <hr/>
+        
+    <p><a href="http://algoso.org/2011/12/16/so-that-thing-kiva-just-started-doing-zidisha-was-already-doing-it/" target="_blank"><img src="{{ asset('assets/images/pages/press/FWW_logo.png'); }}" width="449" height="74" /></a></p>
+    <blockquote>
+        <p>"Zidisha’s success stands as a challenge to the standard microfinance model... they’ve shown that different target populations can be served in different ways.  Zidisha borrowers are aspiring middle class. Many of them are small business owners who form the backbone of a nation’s economy... As their business becomes more successful, this will have outsized impacts on their communities. Zidisha makes that possible."</p>
+        <footer>
+            <cite><a href="http://algoso.org/2011/12/16/so-that-thing-kiva-just-started-doing-zidisha-was-already-doing-it/" target="_blank">Find What Works</a></cite>
+        </footer>
+    </blockquote>     
 </div>
-
 @stop
