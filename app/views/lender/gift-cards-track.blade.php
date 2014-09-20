5e383cea994ae745601c3dca2d34580082048999
jkurnia <julia@zidisha.org>
2014-09-20 18:49:21 +0000

    use sortable table for gift card tracking

--- app/views/lender/gift-cards-track.blade.php
+++ app/views/lender/gift-cards-track.blade.php
@@ -1,85 +1,70 @@
-@extends('layouts.side-menu')
+@extends('layouts.master')
 
 @section('page-title')
 Track Gift Cards
 @stop
 
-@section('menu-title')
-Quick Links
-@stop
+@section('content')
 
-@section('menu-links')
-@include('partials.nav-links.lender-links')
-@stop
+<h1 class="page-title">Track Gift Cards</h1>
 
-@section('page-content')
-<div class="row">
-    <div class="col-sm-6">
-
-        <div class="row">
-            <div class="col-sm-7">
-                <p>Gift Cards Gifted:</p>
-            </div>
-            <div class="col-sm-5">
-                <p>{{ $countCards }}</p>
-            </div>
-
-            <div class="col-sm-7">
-                Gift Cards Redeemed:
-            </div>
-
-            <div class="col-sm-5">
-                <p>{{ $countRedeemed }}</p>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-sm-4 col-sm-offset-2">
-        <div class="row">
-            <div class="col-sm-7">
-                <a href="{{ route('lender:gift-cards') }}" class="btn btn-primary">
-                    @if ($countCards==0)
-                        Give your first gift card
-                    @else
-                        Give another gift card
-                    @endif
-                </a>
-            </div>
-        </div>
+<div class="panel panel-info">
+    <div class="panel-body">
+        <p>Gift Cards Gifted: <strong>{{ $countCards }}</strong> 
+            <a href="{{ route('lender:gift-cards') }}" class="btn btn-primary pull-right">
+                @if ($countCards==0)
+                    Give your first gift card
+                @else
+                    Give another gift card
+                @endif
+            </a>
+        </p>
+        <p>Gift Cards Redeemed: <strong>{{ $countRedeemed }}</strong></p>
     </div>
 </div>
 
-<br/><br/>
+<div class="panel panel-info">
+    <div class="panel-body">
+        <table class="table table-striped no-more-tables" id="track-cards">
+            <thead>
+                <tr>
+                    <th>Date Gifted</th>
+                    <th>Recipient Name</th>
+                    <th>Delivery Method</th>
+                    <th>Recipient Email
+                    <th>Card Amount</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($cards as $card)
+                <tr>
+                    <td data-title="Date Gifted">{{ $card->getDate()->format('M j, Y') }}</td>
+                    <td data-title="Recipient Name">{{ $card->getRecipientName() }}</td>
+                    <td data-title="Delivery Method">{{ $card->getOrderType() }}</td>
+                    <td data-title="Recipient Email">{{ $card->getRecipientEmail() }}</td>
+                    <td data-title="Card Amount">{{ $card->getCardAmount()->getAmount() }}</td>
+                    <td data-title="Status">
+                        @if($card->getClaimed() == 1)
+                        <span class="label label-success">{{ $card->getStringClaimed() }}</span>
+                    @else
+                        <span class="label label-info">{{ $card->getStringClaimed() }}</span>
+                    </td>
+                    @endif
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+@stop
 
-<table class="table table-striped no-more-tables">
-    <thead>
-    <tr>
-        <th>Date Gifted</th>
-        <th>Recipient Name</th>
-        <th>Delivery Method</th>
-        <th>Recipient Email
-        <th>Card Amount</th>
-        <th>Status</th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach($cards as $card)
-    <tr>
-        <td data-title="Date Gifted">{{ $card->getDate()->format('M j, Y') }}</td>
-        <td data-title="Recipient Name">{{ $card->getRecipientName() }}</td>
-        <td data-title="Delivery Method">{{ $card->getOrderType() }}</td>
-        <td data-title="Recipient Email">{{ $card->getRecipientEmail() }}</td>
-        <td data-title="Card Amount">{{ $card->getCardAmount()->getAmount() }}</td>
-        <td data-title="Status">
-            @if($card->getClaimed() == 1)
-            <span class="label label-success">{{ $card->getStringClaimed() }}</span>
-        @else
-            <span class="label label-info">{{ $card->getStringClaimed() }}</span>
-        </td>
-        @endif
-    </tr>
-    @endforeach
-    </tbody>
-</table>
-
+@section('script-footer')
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#track-cards').dataTable({
+            'searching': true
+        });
+    });
+</script>
 @stop
