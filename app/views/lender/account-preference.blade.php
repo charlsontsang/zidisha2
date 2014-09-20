6b460529c0c7d1814136ac50c15b31db58061674
jkurnia <julia@zidisha.org>
2014-09-20 18:33:45 +0000

    hide display preferences for now

--- app/views/lender/account-preference.blade.php
+++ app/views/lender/account-preference.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.side-menu')
+@extends('layouts.side-menu-simple')
 
 @section('page-title')
 Account Preferences
@@ -16,58 +16,73 @@ Quick Links
 {{ BootstrapForm::open(array('route' => 'lender:post:preference')) }}
 {{ BootstrapForm::populate($form) }}
 
-<h4>Notification Preferences</h4>
+<div class="panel panel-info">
+    <div class="panel-heading">
+        <h3 class="panel-title">
+            Notification Preferences
+        </h3>
+    </div>
+    <div class="panel-body">
+		<p>Send me an email when:</p>
 
-<p>Send me an email when:</p>
+		{{ BootstrapForm::checkbox('notifyLoanFullyFunded', null, null, [
+		    'label' => 'A loan I have bid on is fully funded',
+		]) }}
 
-{{ BootstrapForm::checkbox('notifyLoanFullyFunded', null, null, [
-    'label' => 'A loan I have bid on is fully funded',
-]) }}
+		{{ BootstrapForm::checkbox('notifyLoanAboutToExpire', null, null, [
+		    'label' => 'A loan I have bid on has not been fully funded and is about to expire',
+		]) }}
 
-{{ BootstrapForm::checkbox('notifyLoanAboutToExpire', null, null, [
-    'label' => 'A loan I have bid on has not been fully funded and is about to expire',
-]) }}
+		<!-- TODO 
+		{{ BootstrapForm::checkbox('', null, null, [
+		    'label' => 'A loan I have bid on has expired and the funds have been returned to my account',
+		]) }}
+		-->
 
-<!-- TODO 
-{{ BootstrapForm::checkbox('', null, null, [
-    'label' => 'A loan I have bid on has expired and the funds have been returned to my account',
-]) }}
+		{{ BootstrapForm::checkbox('notifyLoanDisbursed', null, null, [
+		    'label' => 'A loan I have funded is disbursed to a borrower',
+		]) }}
+
+		{{ BootstrapForm::checkbox('notifyComment', null, null, [
+		    'label' => 'A borrower I have funded posts a comment',
+		]) }}
+
+		{{ BootstrapForm::checkbox('notifyLoanApplication', null, null, [
+		    'label' => 'A borrower I have funded before posts a new loan application',
+		]) }}
+
+		{{ BootstrapForm::checkbox('notifyInviteAccepted', null, null, [
+		    'label' => 'One of my new lender invites is accepted',
+		]) }}
+		<br/>
+		<p>I would like to be notified about repayments:</p>
+
+		{{ BootstrapForm::select( 'notifyLoanRepayment', $form->getNotifyLoanRepayment(), null, ['label' => '']) }}
+    </div>
+</div>
+
+<!-- 
+
+<div class="panel panel-info">
+    <div class="panel-heading">
+        <h3 class="panel-title">
+            Display Preferences
+        </h3>
+    </div>
+    <div class="panel-body">
+		{{ BootstrapForm::checkbox('hideLendingActivity', null, null, [
+		    'label' => 'I would like my loan bids and funded loans to be displayed on my public profile.',
+		]) }}
+
+		{{ BootstrapForm::checkbox('hideKarma', null, null, [
+		    'label' => 'I would like my karma score to be displayed on my public profile.',
+		]) }} 
+    </div>
+</div>
+
 -->
-
-{{ BootstrapForm::checkbox('notifyLoanDisbursed', null, null, [
-    'label' => 'A loan I have funded is disbursed to a borrower',
-]) }}
-
-{{ BootstrapForm::checkbox('notifyComment', null, null, [
-    'label' => 'A borrower I have funded posts a comment',
-]) }}
-
-{{ BootstrapForm::checkbox('notifyLoanApplication', null, null, [
-    'label' => 'A borrower I have funded before posts a new loan application',
-]) }}
-
-{{ BootstrapForm::checkbox('notifyInviteAccepted', null, null, [
-    'label' => 'One of my new lender invites is accepted',
-]) }}
-<br/>
-<p>I would like to be notified about repayments:</p>
-
-{{ BootstrapForm::select( 'notifyLoanRepayment', $form->getNotifyLoanRepayment(), null, ['label' => '']) }}
-
-<br/>
-
-<h4>Display Preferences</h4>
-
-{{ BootstrapForm::checkbox('hideLendingActivity', null, null, [
-    'label' => 'I would like my loan bids and funded loans to be displayed on my public profile.',
-]) }}
-
-{{ BootstrapForm::checkbox('hideKarma', null, null, [
-    'label' => 'I would like my karma score to be displayed on my public profile.',
-]) }} 
-
-<br/>
 
 {{ BootstrapForm::submit('Save') }}
 {{ BootstrapForm::close() }}
+
 @stop
