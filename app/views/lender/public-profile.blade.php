d067e5328c0941b4256b042268485de69930efcd
jkurnia <julia@zidisha.org>
2014-09-20 18:26:26 +0000

    initial styling of lender public profile

--- app/views/lender/public-profile.blade.php
+++ app/views/lender/public-profile.blade.php
@@ -5,23 +5,32 @@
 @stop
 
 @section('content')
-<div class="page-header">
-    <h2>{{ $lender->getUser()->getUsername() }}</h2>
-</div>
 <div class="row">
-    <div class="col-sm-4 pull-right mobile-padding">
+    <h1></h1>
+    <div class="col-sm-4 pull-right">
         <img src="{{ $lender->getUser()->getProfilePictureUrl() }}" width="100%">
     </div>
 
-    <div class="col-sm-6">
-        <p>
-            <i class="fa fa-fw fa-map-marker"></i>
-            @if($lender->getProfile()->getCity())
-                {{ $lender->getProfile()->getCity() }},&nbsp;
-            @endif
-            {{ $lender->getCountry()->getName() }}
-        </p>
-        <p>{{ $lender->getProfile()->getAboutMe() }}</p>
+    <div class="col-sm-8">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <h3 class="panel-title">
+                    {{ $lender->getUser()->getUsername() }}
+                </h3>
+            </div>
+            <div class="panel-body">
+                <p>
+                    <i class="fa fa-fw fa-map-marker"></i>
+                    @if($lender->getProfile()->getCity())
+                        {{ $lender->getProfile()->getCity() }},&nbsp;
+                    @endif
+                    {{ $lender->getCountry()->getName() }}
+                </p>
+                <p>
+                    {{ $lender->getProfile()->getAboutMe() }}
+                </p>
+            </div>
+        </div>
     </div>
 </div>
 @stop
