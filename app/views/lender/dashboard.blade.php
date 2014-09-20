7e40faaba3e6fe444fdd7ec5406c7606edef8e68
jkurnia <julia@zidisha.org>
2014-09-20 02:01:59 +0000

    add bottom margin to page title

--- app/views/lender/dashboard.blade.php
+++ app/views/lender/dashboard.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.side-menu')
+@extends('layouts.side-menu-simple')
 
 @section('page-title')
 Dashboard
@@ -13,53 +13,57 @@ Quick Links
 @stop
 
 @section('page-content')
-<h3>Your Project Updates</h3>
-
-<div>
-<h3>Comments on My Loans</h3>
-@if (count($comments))
-    <table class="table">
-        <tbody>
-        @foreach($comments as $comment)
-            <tr>
-                <td colspan="2">
-                    <a href="{{ route('loan:index', $comment->getBorrower()->getActiveLoanId()) }}">
-                        <img src="{{ $comment->getBorrower()->getUser()->getProfilePictureUrl('small-profile-picture') }}" width="100%">
-                    </a>
-                </td>
-                <td>
-                    {{ $comment->getMessage() }}
-                    <br/><br/>
-                    <p class="meta">
-                    @if($comment->getUserId() != $comment->getBorrowerId())
-                        <a href="{{ route('loan:index', $comment->getBorrower()->getActiveLoanId()) }}">
-                         <strong>{{ $comment->getBorrower()->getName() }}</strong>
-                        </a>
-                        {{ $comment->getUser()->getSubObject()->getName() }}
-                        <strong>
-                        {{ $comment->getUser()->getSubObject()->getProfile()->getCity() }},
-                        {{ $comment->getUser()->getSubObject()->getCountry()->getName() }}
-                        </strong>
-                        {{ date("d F Y", $comment->getCreatedAt()->getTimeStamp())  }}
-                    @else
-                        <a href="{{ route('loan:index', $comment->getBorrower()->getActiveLoanId()) }}">
-                         <strong>{{ $comment->getUser()->getSubObject()->getName() }}</strong>
-                        </a>
-                        <strong>
-                        {{ $comment->getUser()->getSubObject()->getProfile()->getCity() }},
-                        {{ $comment->getUser()->getSubObject()->getCountry()->getName() }}
-                        </strong>
-                        {{ date("d F Y", $comment->getCreatedAt()->getTimeStamp()) }}
-                    @endif
-                    </p>
-                </td>
-            </tr>
-        @endforeach
-         </tbody>
-    </table>
-@else
-    <p>Your comment feed is empty.</p>
-    <p><strong><a href="{{ route('lend:index') }}">Make a Loan</a></strong></p>
-@endif
-</div>
+    <div class="panel panel-info">
+        <div class="panel-heading">
+            <h3 class="panel-title">
+                Your Project Updates
+            </h3>
+        </div>
+        <div class="panel-body">
+            @if (count($comments))
+                <table class="table">
+                    <tbody>
+                    @foreach($comments as $comment)
+                        <tr>
+                            <td colspan="2">
+                                <a href="{{ route('loan:index', $comment->getBorrower()->getActiveLoanId()) }}">
+                                    <img src="{{ $comment->getBorrower()->getUser()->getProfilePictureUrl('small-profile-picture') }}" width="100%">
+                                </a>
+                            </td>
+                            <td>
+                                {{ $comment->getMessage() }}
+                                <br/><br/>
+                                <p class="meta">
+                                @if($comment->getUserId() != $comment->getBorrowerId())
+                                    <a href="{{ route('loan:index', $comment->getBorrower()->getActiveLoanId()) }}">
+                                     <strong>{{ $comment->getBorrower()->getName() }}</strong>
+                                    </a>
+                                    {{ $comment->getUser()->getSubObject()->getName() }}
+                                    <strong>
+                                    {{ $comment->getUser()->getSubObject()->getProfile()->getCity() }},
+                                    {{ $comment->getUser()->getSubObject()->getCountry()->getName() }}
+                                    </strong>
+                                    {{ date("d F Y", $comment->getCreatedAt()->getTimeStamp())  }}
+                                @else
+                                    <a href="{{ route('loan:index', $comment->getBorrower()->getActiveLoanId()) }}">
+                                     <strong>{{ $comment->getUser()->getSubObject()->getName() }}</strong>
+                                    </a>
+                                    <strong>
+                                    {{ $comment->getUser()->getSubObject()->getProfile()->getCity() }},
+                                    {{ $comment->getUser()->getSubObject()->getCountry()->getName() }}
+                                    </strong>
+                                    {{ date("d F Y", $comment->getCreatedAt()->getTimeStamp()) }}
+                                @endif
+                                </p>
+                            </td>
+                        </tr>
+                    @endforeach
+                     </tbody>
+                </table>
+            @else
+                <p>Your comment feed is empty.</p>
+                <p><strong><a href="{{ route('lend:index') }}">Make a Loan</a></strong></p>
+            @endif
+        </div>
+    </div>
 @stop
