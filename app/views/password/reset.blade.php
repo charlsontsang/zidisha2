5d548d2c62bda815415cc5e42d6b078aea3079f6
jkurnia <julia@zidisha.org>
2014-09-21 21:20:16 +0000

    use white panel in reset password page

--- app/views/password/reset.blade.php
+++ app/views/password/reset.blade.php
@@ -2,36 +2,36 @@
 
 @section('content')
 <div class="row">
-    <div style="margin-top: 40px;"></div>
-
     <div class="col-md-offset-3 col-md-6">
-        <div class="page-header">
-            <h1>Reset your password</h1>
+        <div class="highlight highlight-panel">
+            <div class="page-header">
+                <h1>Reset your password</h1>
+            </div>
+            {{ Form::open(array('url' => 'password/reset')) }}
+
+            <div class="form-group">
+                {{ Form::label('username', \Lang::get('borrower.reminders.username-or-password')) }}
+                {{ Form::text('username', null, array('class' => 'form-control')) }}
+                {{ $errors->first('username', '<div class="alert alert-danger">:message</div>') }}
+            </div>
+
+            <div class="form-group">
+                {{ Form::label('password', \Lang::get('borrower.reminders.create-password')) }}
+                {{ Form::password('password', array('class' => 'form-control')) }}
+                {{ $errors->first('password',  '<div class="alert alert-danger">:message</div>') }}
+            </div>
+
+            <div class="form-group">
+                {{ Form::label('password_confirmation', 'Confirm Password') }}
+                {{ Form::password('password_confirmation', array('class' => 'form-control')) }}
+            </div>
+
+            {{ Form::hidden('token', $token) }}
+
+            <button type="submit" class="btn btn-default">Submit</button>
+
+            {{ Form::close() }}
         </div>
-        {{ Form::open(array('url' => 'password/reset')) }}
-
-        <div class="form-group">
-            {{ Form::label('username', \Lang::get('borrower.reminders.username-or-password')) }}
-            {{ Form::text('username', null, array('class' => 'form-control')) }}
-            {{ $errors->first('username', '<div class="alert alert-danger">:message</div>') }}
-        </div>
-
-        <div class="form-group">
-            {{ Form::label('password', \Lang::get('borrower.reminders.create-password')) }}
-            {{ Form::password('password', array('class' => 'form-control')) }}
-            {{ $errors->first('password',  '<div class="alert alert-danger">:message</div>') }}
-        </div>
-
-        <div class="form-group">
-            {{ Form::label('password_confirmation', 'Confirm Password') }}
-            {{ Form::password('password_confirmation', array('class' => 'form-control')) }}
-        </div>
-
-        {{ Form::hidden('token', $token) }}
-
-        <button type="submit" class="btn btn-default">Submit</button>
-
-        {{ Form::close() }}
     </div>
 </div>
 @stop
