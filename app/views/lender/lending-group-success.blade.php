8a6c71769f381b9a45b3f21fda01ad68b6d2bd11
jkurnia <julia@zidisha.org>
2014-09-22 16:42:44 -0400

    use white panel for loan and group success pages

--- app/views/lender/lending-group-success.blade.php
+++ app/views/lender/lending-group-success.blade.php
@@ -5,47 +5,48 @@
 @stop
 
 @section('content')
-<div class="row">
-    <div class="col-sm-6 loan-body">
-        @if($group->getGroupProfilePicture())
-            <div class="pull-left profile-image" href="{{ route('lender:group', $group->getId()) }}"
-                style="background-image:url({{ $group->getGroupProfilePicture()->getImageUrl('small-profile-picture') }})" width="100%">
-            </div>
-        @else
-            <div class="pull-left profile-image" href="{{ route('lender:group', $group->getId()) }}"
-                style="background-image:url('/assets/images/carousel/mary.jpg')" width="100%">
-            </div>
-        @endif
-    </div>
+<div class="highlight highlight-panel">
+    <div class="row">
+        <div class="col-sm-6 loan-body">
+            @if($group->getGroupProfilePicture())
+                <div class="pull-left profile-image" href="{{ route('lender:group', $group->getId()) }}"
+                    style="background-image:url({{ $group->getGroupProfilePicture()->getImageUrl('small-profile-picture') }})" width="100%">
+                </div>
+            @else
+                <div class="pull-left profile-image" href="{{ route('lender:group', $group->getId()) }}"
+                    style="background-image:url('/assets/images/carousel/mary.jpg')" width="100%">
+                </div>
+            @endif
+        </div>
 
-    <div class="col-sm-6">
-        <div class="lead">
-            <p>
-                <br/><br/>
-                {{{ $successMessage }}}
-            </p>
-            <p>
-                Want to recruit more members for {{{ $group->getName() }}}?  Share the group page:
-            </p>
-            <p>
-                <a href="{{$facebookUrl}}" class="btn btn-facebook btn-social share-window">
-                    <i class="fa fa-fw fa-facebook"></i>Share
-                </a>
-                <a href="{{$twitterUrl}}" class="btn btn-twitter btn-social share-window">
-                    <i class="fa fa-fw fa-twitter"></i>Tweet
-                </a>
-                <a href="{{$mailUrl}}" class="btn btn-danger btn-social">
-                    <i class="fa fa-fw fa-envelope-o"></i>Email
-                </a>
+        <div class="col-sm-6">
+            <div class="lead">
+                <p>
+                    <br/><br/>
+                    {{{ $successMessage }}}
+                </p>
+                <p>
+                    Want to recruit more members for {{{ $group->getName() }}}?  Share the group page:
+                </p>
+                <p>
+                    <a href="{{$facebookUrl}}" class="btn btn-facebook btn-social share-window">
+                        <i class="fa fa-fw fa-facebook"></i>Share
+                    </a>
+                    <a href="{{$twitterUrl}}" class="btn btn-twitter btn-social share-window">
+                        <i class="fa fa-fw fa-twitter"></i>Tweet
+                    </a>
+                    <a href="{{$mailUrl}}" class="btn btn-danger btn-social">
+                        <i class="fa fa-fw fa-envelope-o"></i>Email
+                    </a>
+                </p>
+            </div>
+            <p style="font-size: 18px !important;">
+                <br/>
+                <a href="{{ route('lender:group', $group->getId()) }}">Go to group page</a>
             </p>
         </div>
-        <p style="font-size: 18px !important;">
-            <br/>
-            <a href="{{ route('lender:group', $group->getId()) }}">Go to group page</a>
-        </p>
     </div>
 </div>
-
 @stop
 
 @section('script-footer')
--- app/views/pages/loan-success.blade.php
+++ app/views/pages/loan-success.blade.php
@@ -5,41 +5,43 @@
 @stop
 
 @section('content')
-<div class="row">
-    <div class="col-sm-6 loan-body">
-        <div class="pull-left profile-image" href="{{ route('loan:index', $loan->getId()) }}"
-            style="background-image:url({{ $loan->getBorrower()->getUser()->getProfilePictureUrl('large-profile-picture') }})" width="100%">
+<div class="highlight highlight-panel">
+    <div class="row">
+        <div class="col-sm-6 loan-body">
+            <div class="pull-left profile-image" href="{{ route('loan:index', $loan->getId()) }}"
+                style="background-image:url({{ $loan->getBorrower()->getUser()->getProfilePictureUrl('large-profile-picture') }})" width="100%">
+            </div>
         </div>
-    </div>
 
-    <div class="col-sm-6">
-        <div class="lead">
-            <p>
-                <br/><br/>
-                You just supported {{{ $loan->getBorrower()->getFirstName() }}}!
-            </p>
-            <p>
-                Did you know that sharing a loan story makes it 3x more likely to be fully funded?
-            </p>
-            <p>
-                Help {{{ $loan->getBorrower()->getFirstName() }}} by sharing with your friends:
-            </p>
-            <p>
-                <a href="{{$facebookUrl}}" class="btn btn-facebook btn-social share-window">
-                    <i class="fa fa-fw fa-facebook"></i>Share
-                </a>
-                <a href="{{$twitterUrl}}" class="btn btn-twitter btn-social share-window">
-                    <i class="fa fa-fw fa-twitter"></i>Tweet
-                </a>
-                <a href="{{$mailUrl}}" class="btn btn-danger btn-social">
-                    <i class="fa fa-fw fa-envelope-o"></i>Email
-                </a>
+        <div class="col-sm-6">
+            <div class="lead">
+                <p>
+                    <br/><br/>
+                    You just supported {{{ $loan->getBorrower()->getFirstName() }}}!
+                </p>
+                <p>
+                    Did you know that sharing a loan story makes it 3x more likely to be fully funded?
+                </p>
+                <p>
+                    Help {{{ $loan->getBorrower()->getFirstName() }}} by sharing with your friends:
+                </p>
+                <p>
+                    <a href="{{$facebookUrl}}" class="btn btn-facebook btn-social share-window">
+                        <i class="fa fa-fw fa-facebook"></i>Share
+                    </a>
+                    <a href="{{$twitterUrl}}" class="btn btn-twitter btn-social share-window">
+                        <i class="fa fa-fw fa-twitter"></i>Tweet
+                    </a>
+                    <a href="{{$mailUrl}}" class="btn btn-danger btn-social">
+                        <i class="fa fa-fw fa-envelope-o"></i>Email
+                    </a>
+                </p>
+            </div>
+            <p style="font-size: 18px !important;">
+                <br/>
+                <a href="{{ route('lend:index') }}">Make another loan</a>
             </p>
         </div>
-        <p style="font-size: 18px !important;">
-            <br/>
-            <a href="{{ route('lend:index') }}">Make another loan</a>
-        </p>
     </div>
 </div>
 
