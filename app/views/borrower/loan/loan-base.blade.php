a3da3901db99d06796d913c7ae12f9bd327fa937
jkurnia <julia@zidisha.org>
2014-09-23 21:59:16 +0000

    fix loan base template

--- app/views/borrower/loan/loan-base.blade.php
+++ app/views/borrower/loan/loan-base.blade.php
@@ -14,28 +14,4 @@
 
 
 @section('page-content')
-    <div class="page-header">
-        
-        <a href="{{ action('LoanController@getIndex', [ 'loanId' => $loan->getId() ]) }}" class="btn btn-primary">
-            @lang('borrower.loan.page.public-loan-page')
-        </a>
-        
-        <span class="dropdown pull-right">
-            
-            @if(count($loans) > 1)
-            <a class="btn btn-default" data-toggle="dropdown" href="#">
-                @lang('borrower.loan.page.your-loans')
-            </a>
-            <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
-                @foreach($loans as $_loan)
-                <li role="presentation" class="{{ $loan == $_loan ? 'active'  : ''}}">
-                    <a role="menuitem" tabindex="-1" href="{{ route('borrower:loan', $_loan->getId()) }}">
-                        {{ $_loan->getAppliedAt()->format('M j, Y') }}
-                    </a>
-                </li>
-                @endforeach
-            </ul>
-            @endif
-        </span>
-    </div>
 @stop
