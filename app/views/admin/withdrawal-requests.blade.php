013fc619a22ccc79e6258ef8aca2964ebaa4ee9c
jkurnia <julia@zidisha.org>
2014-09-23 23:54:27 +0000

    style withdraw page

--- app/views/admin/withdrawal-requests.blade.php
+++ app/views/admin/withdrawal-requests.blade.php
@@ -5,72 +5,77 @@ Withdraw Requests
 @stop
 
 @section('content')
-<div class="page-header">
-    <h1>Withdraw Requests</h1>
-</div>
-@if(Auth::getUser()->isAdmin())
-    {{ BootstrapForm::open(['route' => ['admin:post:paypal-withdrawal-requests'], 'id' => 'paypal-mass-payment-form']) }}
-    {{ BootstrapForm::populate($form) }}
-    <button id="paypal-mass-payment" class="btn btn-primary" type="submit">Process payments</button>
-    {{ BootstrapForm::close() }}
-@endif
-<table class="table table-striped" id="withdrawals">
-    <thead>
-    <tr>
-        <th>Request Date</th>
-        <th>Lender</th>
-        <th>History</th>
-        <th>PayPal Email</th>
-        <th>Withdrawal Amount</th>
-        <th>Authorize</th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach($paginator as $request)
+<h1 class="page-title">Withdraw Requests</h1>
 
-    <tr>
-        <td>{{ $request->getCreatedAt()->format('M j, Y') }}</td>
-        <td>
-            <p><a href="{{ route('lender:public-profile', $request->getLender()->getUser()->getUserName()) }}">
-                {{ $request->getLender()->getName() }}</a></p>
-            <p>{{ $request->getLender()->getUser()->getEmail() }}</p>
-        </td>
-        <td><p>Uploaded: {{ $uploaded[$request->getLenderId()] }}</p>
-            <p>Repaid: {{ $repaid[$request->getLenderId()] }}</p>
-            <p>Withdrawn: {{ $withdrawn[$request->getLenderId()]->multiply(-1) }}</p>
-        </td>
-        <td>{{ $request->getPaypalEmail() }}</td>
-        <td><p>
-                {{ $request->getAmount() }}
-            </p>
-            <p>
-            @if(Auth::getUser()->isAdmin())
-                @if($request->isPaid())
-                    Paid
-                @else
-                    <a href="{{ route('admin:post:withdrawal-requests', $request->getId()) }}">
-                        Confirm payment
-                    </a>
-                @endif
-            @else
-                @if($request->isPaid())
-                    Paid
-                @else
-                    Not yet paid
-                @endif
-            @endif
-            </p>
-        </td>
-        <td>
-            @if(!$request->isPaid())
-                {{ BootstrapForm::checkbox('ids['.$request->getId().']', $request->getId(), null, ['class' => 'withdraw-checkbox', 'label' => '']) }}
-            @endif
-        </td>
-    </tr>
-    {{ BootstrapForm::close() }}
-    @endforeach
-    </tbody>
-</table>
+<div class="panel panel-info">
+    <div class="panel-heading">
+        @if(Auth::getUser()->isAdmin())
+            {{ BootstrapForm::open(['route' => ['admin:post:paypal-withdrawal-requests'], 'id' => 'paypal-mass-payment-form']) }}
+            {{ BootstrapForm::populate($form) }}
+            <button id="paypal-mass-payment" class="btn btn-primary" type="submit">Process payments</button>
+            {{ BootstrapForm::close() }}
+        @endif
+    </div>
+    <div class="panel-body">
+        <table class="table table-striped" id="withdrawals">
+            <thead>
+            <tr>
+                <th>Request Date</th>
+                <th>Lender</th>
+                <th>History</th>
+                <th>PayPal Email</th>
+                <th>Withdrawal Amount</th>
+                <th>Authorize</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($paginator as $request)
+
+            <tr>
+                <td>{{ $request->getCreatedAt()->format('M j, Y') }}</td>
+                <td>
+                    <p><a href="{{ route('lender:public-profile', $request->getLender()->getUser()->getUserName()) }}">
+                        {{ $request->getLender()->getName() }}</a></p>
+                    <p>{{ $request->getLender()->getUser()->getEmail() }}</p>
+                </td>
+                <td><p>Uploaded: {{ $uploaded[$request->getLenderId()] }}</p>
+                    <p>Repaid: {{ $repaid[$request->getLenderId()] }}</p>
+                    <p>Withdrawn: {{ $withdrawn[$request->getLenderId()]->multiply(-1) }}</p>
+                </td>
+                <td>{{ $request->getPaypalEmail() }}</td>
+                <td><p>
+                        {{ $request->getAmount() }}
+                    </p>
+                    <p>
+                    @if(Auth::getUser()->isAdmin())
+                        @if($request->isPaid())
+                            Paid
+                        @else
+                            <a href="{{ route('admin:post:withdrawal-requests', $request->getId()) }}">
+                                Confirm payment
+                            </a>
+                        @endif
+                    @else
+                        @if($request->isPaid())
+                            Paid
+                        @else
+                            Not yet paid
+                        @endif
+                    @endif
+                    </p>
+                </td>
+                <td>
+                    @if(!$request->isPaid())
+                        {{ BootstrapForm::checkbox('ids['.$request->getId().']', $request->getId(), null, ['class' => 'withdraw-checkbox', 'label' => '']) }}
+                    @endif
+                </td>
+            </tr>
+            {{ BootstrapForm::close() }}
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 {{ BootstrapHtml::paginator($paginator)->links() }}
 @stop
 
