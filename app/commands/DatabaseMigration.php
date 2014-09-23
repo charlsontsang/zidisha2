cd9234a0b5ffb75a1da8cf886616da13a6d4d127
Jonas De Taeye <jonasdt@fastmail.fm>
2014-09-23 21:11:57 +0200

    Remove empty constructor

--- app/commands/DatabaseMigration.php
+++ app/commands/DatabaseMigration.php
@@ -26,16 +26,6 @@ class DatabaseMigration extends Command {
     protected $con;
 
 	/**
-	 * Create a new command instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-	}
-
-	/**
 	 * Execute the console command.
 	 *
 	 * @return mixed
