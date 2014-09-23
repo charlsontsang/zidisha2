fbdd89061553d9d9ff1a433e032421d27dca2078
jkurnia <julia@zidisha.org>
2014-09-23 21:38:04 +0000

    move no loan view to sidemenu template

--- app/views/borrower/loan/loan-no-loans.blade.php
+++ app/views/borrower/loan/loan-no-loans.blade.php
@@ -1,16 +1,17 @@
 @extends('layouts.master')
 
-@section('content')
-
-<h1>
-    @lang('borrower.loan.page.no-loan')
-</h1>
-
-<a class="btn btn-primary" href="{{ route('borrower:loan-application') }}">
-    @lang('borrower.loan.page.apply')
-</a>
-
-<p>
-    {{ \Lang::get('borrower.loan.no-loan.no-loan-message') }}
-</p>
+@section('page-content')
+<div class="panel panel-info">
+    <div class="panel-heading">
+        <h4>
+            @lang('borrower.loan.page.no-loan')
+        </h4>
+    </div>
+    <div class="panel-body">
+        {{ \Lang::get('borrower.loan.no-loan.no-loan-message') }} 
+        <a class="btn btn-primary pull-right" href="{{ route('borrower:loan-application') }}">
+		    @lang('borrower.loan.page.apply')
+		</a>
+    </div>
+</div>
 @stop
