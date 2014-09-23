8297d6ac1261823cf92e32c8b276095f6e58e821
jkurnia <julia@zidisha.org>
2014-09-23 14:47:56 +0000

    revert home controller

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends BaseController {
     {
         $country = Utility::getCountryCodeByIP();
 
-        $country = CountryQuery::create()->findOneById('1');
+        $country = CountryQuery::create()->findOneById($country['id']);
 
         if ($country && $country->isBorrowerCountry()) {
             return $this->getBorrowerHome($country);
