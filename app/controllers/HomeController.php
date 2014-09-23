e05f21e8b7ca99465691be8eed0c19c13300c3ce
jkurnia <julia@zidisha.org>
2014-09-23 14:47:08 +0000

    use white panel in borrower homepage

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends BaseController {
     {
         $country = Utility::getCountryCodeByIP();
 
-        $country = CountryQuery::create()->findOneById($country['id']);
+        $country = CountryQuery::create()->findOneById('1');
 
         if ($country && $country->isBorrowerCountry()) {
             return $this->getBorrowerHome($country);
