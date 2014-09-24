244408ff113b14230fd6953bcb905a3292847cb6
jkurnia <julia@zidisha.org>
2014-09-24 18:58:22 +0000

    use white panel for create lending group

--- app/views/lender/groups/create.blade.php
+++ app/views/lender/groups/create.blade.php
@@ -1,25 +1,30 @@
 @extends('layouts.master')
 
 @section('page-title')
-Start a new Lending Group
+Start a New Lending Group
 @stop
 
 @section('content')
-<h2>Start a new Lending Group</h2>
+<div class="row">
+	<div class="col-sm-8 col-sm-offset-2">
+		<div class="highlight highlight-panel">
+			<h2>Start a New Lending Group</h2>
 
-{{ BootstrapForm::open(array('route' => 'lender:groups:post-create', 'translationDomain' => 'lender.group', 'files' => true)) }}
-{{ BootstrapForm::populate($form) }}
+			{{ BootstrapForm::open(array('route' => 'lender:groups:post-create', 'translationDomain' => 'lender.group', 'files' => true)) }}
+			{{ BootstrapForm::populate($form) }}
 
-{{ BootstrapForm::text('name') }}
+			{{ BootstrapForm::text('name') }}
 
-{{ BootstrapForm::text('website') }}
+			{{ BootstrapForm::text('website') }}
 
-{{ BootstrapForm::file('groupProfilePictureId') }}
+			{{ BootstrapForm::file('groupProfilePictureId') }}
 
-{{ BootstrapForm::textarea('about') }}
+			{{ BootstrapForm::textarea('about') }}
 
-{{ BootstrapForm::submit('save') }}
+			{{ BootstrapForm::submit('save') }}
 
-{{ BootstrapForm::close() }}
-
+			{{ BootstrapForm::close() }}
+		</div>
+	</div>
+</div>
 @stop
