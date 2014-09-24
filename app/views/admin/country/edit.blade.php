36d66fdc183fe25163a5223ec2935f7fdac0d8bb
jkurnia <julia@zidisha.org>
2014-09-24 00:06:50 +0000

    style fx rate page

--- app/views/admin/country/edit.blade.php
+++ app/views/admin/country/edit.blade.php
@@ -8,33 +8,35 @@ Edit Country: {{ $country->getName() }}
 <div class="page-header">
     <h1>Edit Country: {{ $country->getName() }}</h1>
 </div>
-<div>
-    {{ BootstrapForm::open(['route' => ['admin:post:edit:country', $country->getId()]]) }}
-    {{ BootstrapForm::populate($form) }}
+<div class="panel panel-info">
+    <div class="panel-body">
+        {{ BootstrapForm::open(['route' => ['admin:post:edit:country', $country->getId()]]) }}
+        {{ BootstrapForm::populate($form) }}
 
-    {{ BootstrapForm::label('Borrower Country') }}
+        {{ BootstrapForm::label('Borrower Country') }}
 
-    {{ BootstrapForm::radio('borrower_country', '1', null, ['label' => 'Yes']) }}
-    {{ BootstrapForm::radio('borrower_country', '0', null, ['label' => 'No']) }}
+        {{ BootstrapForm::radio('borrower_country', '1', null, ['label' => 'Yes']) }}
+        {{ BootstrapForm::radio('borrower_country', '0', null, ['label' => 'No']) }}
 
-    {{ BootstrapForm::text('dialing_code', null, ['label' => 'Dialing Code']) }}
+        {{ BootstrapForm::text('dialing_code', null, ['label' => 'Dialing Code']) }}
 
-    {{ BootstrapForm::text('phone_number_length', null, ['label' => 'Phone Number Length']) }}
+        {{ BootstrapForm::text('phone_number_length', null, ['label' => 'Phone Number Length']) }}
 
-    {{ BootstrapForm::text('registration_fee', null, ['label' => 'Registration Fee', 'prepend' => $form->getCurrency()]) }}
-    
-    {{ BootstrapForm::text('installment_amount_step', null, ['label' => 'Installment Amount Step', 'prepend' => $form->getCurrency()]) }}
-    
-    {{ BootstrapForm::text('loan_amount_step', null, ['label' => 'Loan Amount Step', 'prepend' => $form->getCurrency()]) }}
+        {{ BootstrapForm::text('registration_fee', null, ['label' => 'Registration Fee', 'prepend' => $form->getCurrency()]) }}
+        
+        {{ BootstrapForm::text('installment_amount_step', null, ['label' => 'Installment Amount Step', 'prepend' => $form->getCurrency()]) }}
+        
+        {{ BootstrapForm::text('loan_amount_step', null, ['label' => 'Loan Amount Step', 'prepend' => $form->getCurrency()]) }}
 
-    {{ BootstrapForm::select('installment_period', $form->getInstallmentPeriods(), $form->getDefaultInstallmentPeriod(), ['label' => 'Installment Period']) }}
+        {{ BootstrapForm::select('installment_period', $form->getInstallmentPeriods(), $form->getDefaultInstallmentPeriod(), ['label' => 'Installment Period']) }}
 
-    {{ BootstrapForm::textarea('repayment_instructions', null, ['label' => 'Repayment Instructions', 'description' => "This instruction is included in disbursement confirmation emails sent to borrowers and displayed in borrowers' accounts."]) }}
+        {{ BootstrapForm::textarea('repayment_instructions', null, ['label' => 'Repayment Instructions', 'description' => "This instruction is included in disbursement confirmation emails sent to borrowers and displayed in borrowers' accounts."]) }}
 
-    {{ BootstrapForm::textarea('accept_bids_note', null, ['label' => 'Accept Bids Note']) }}
+        {{ BootstrapForm::textarea('accept_bids_note', null, ['label' => 'Accept Bids Note']) }}
 
-    {{ BootstrapForm::submit('Save Changes') }}
+        {{ BootstrapForm::submit('Save Changes') }}
 
-    {{ BootstrapForm::close() }}
+        {{ BootstrapForm::close() }}
+    </div>
 </div>
 @stop
--- app/views/admin/country/index.blade.php
+++ app/views/admin/country/index.blade.php
@@ -13,33 +13,36 @@ Countries
     <li class="{{ $otherCountries ? '' : 'active' }}"><a href="{{ route('admin:countries') }}">Borrower Countries</a></li>
     <li class="{{ !$otherCountries ? '' : 'active' }}"><a href="{{ route('admin:countries') . '?other_countries=true' }}">Other Countries</a></li>
 </ul>
-
-<table class="table table-striped">
-    <thead>
-    <tr>
-        <th>Country Name</th>
-        <th>Dialing Code</th>
-        <th>Registration Fee</th>
-        <th>Installment Period</th>
-        <th>Edit</th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach($countries as $country)
-    <tr>
-        <td><a href="{{ route('admin:edit:country', $country->getId()) }}"> {{ $country->getName() }} </a></td>
-        <td>{{ $country->getDialingCode() }}</td>
-        <td>{{ $country->getRegistrationFee() }}</td>
-        <td>{{ $country->getInstallmentPeriod() }}</td>
-        @if(Auth::getUser()->isAdmin())
-            <td>
-                <a href="{{ route('admin:edit:country', $country->getId()) }}">
-                    <i class="fa fa-pencil-square-o fa-lg"></i>
-                </a>
-            </td>
-        @endif
-    </tr>
-    @endforeach
-    </tbody>
-</table>
+<div class="panel panel-info">
+    <div class="panel-body">
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>Country Name</th>
+                <th>Dialing Code</th>
+                <th>Registration Fee</th>
+                <th>Installment Period</th>
+                <th>Edit</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($countries as $country)
+            <tr>
+                <td><a href="{{ route('admin:edit:country', $country->getId()) }}"> {{ $country->getName() }} </a></td>
+                <td>{{ $country->getDialingCode() }}</td>
+                <td>{{ $country->getRegistrationFee() }}</td>
+                <td>{{ $country->getInstallmentPeriod() }}</td>
+                @if(Auth::getUser()->isAdmin())
+                    <td>
+                        <a href="{{ route('admin:edit:country', $country->getId()) }}">
+                            <i class="fa fa-pencil-square-o fa-lg"></i>
+                        </a>
+                    </td>
+                @endif
+            </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 @stop
