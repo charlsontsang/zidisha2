7508a59646829d1c0db0bed2a5311ff1c91009a5
jkurnia <julia@zidisha.org>
2014-09-24 18:04:35 +0000

    remove unused variable

--- app/controllers/LenderController.php
+++ app/controllers/LenderController.php
@@ -149,13 +149,10 @@ class LenderController extends BaseController
             ->filterByLender($lender)
             ->count();
 
-        $totalLentAmount = TransactionQuery::create()
-            ->getTotalLentAmount($userId);
-
         $comments = $this->borrowerCommentService->getAllCommentForLender($lender);
 
         return View::make('lender.dashboard', compact('currentBalance', 'numberOfInvitesSent',  
-                'totalLentAmount', 'comments'
+                'comments'
             ));
     }
 
