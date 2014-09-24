25fa4c51611ed8034e5b66ed524b590e5eedc494
SingularityGuy <hardikformail@gmail.com>
2014-09-24 16:47:32 +0530

    make city not required in lender's editform

--- app/Zidisha/Lender/Form/EditProfile.php
+++ app/Zidisha/Lender/Form/EditProfile.php
@@ -27,7 +27,7 @@ class EditProfile extends AbstractForm
             'username'  => 'required|alpha_num_space',
             'email'     => 'required|email|uniqueUserEmail:' . $this->lender->getId(),
             'password'  => 'confirmed',
-            'city'      => 'required|alpha_num',
+            'city'      => 'alpha_num',
             'firstName' => '',
             'lastName'  => '',
             'aboutMe'   => '',
