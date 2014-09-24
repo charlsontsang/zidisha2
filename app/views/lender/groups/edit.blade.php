5c5139555d8d611bc504dfcd63bb75676577c871
jkurnia <julia@zidisha.org>
2014-09-24 18:57:12 +0000

    use white panel for edit lending group page

--- app/views/lender/groups/edit.blade.php
+++ app/views/lender/groups/edit.blade.php
@@ -6,30 +6,32 @@ Edit Lending Group
 
 @section('content')
 <div class="row">
-	<div class="col-sm-6 col-sm-offset-3">
-		<div class="page-header">
-		    <h1>Edit Lending Group</h1>
+	<div class="col-sm-8 col-sm-offset-2">
+		<div class="highlight highlight-panel">
+			<div class="page-header">
+			    <h1>Edit Lending Group</h1>
+			</div>
+			{{ BootstrapForm::open(['route' => ['lender:groups:post-edit', $group->getId()], 'translationDomain' => 'lender.group', 'files' => true]) }}
+
+			{{ BootstrapForm::populate($form) }}
+
+			{{ BootstrapForm::text('name') }}
+
+			{{ BootstrapForm::text('website') }}
+
+			{{ BootstrapForm::file('groupProfilePictureId') }}
+			@if($group->getGroupProfilePicture())
+			    <img src="{{ $group->getGroupProfilePicture()->getImageUrl('small-profile-picture') }}" alt=""/>
+			@endif
+
+			{{ BootstrapForm::textarea('about') }}
+
+			{{ BootstrapForm::select('userId', $form->getMembers(), $group->getLeader()->getUser()->getId()) }}
+
+			{{ BootstrapForm::submit('save') }}
+
+			{{ BootstrapForm::close() }}
 		</div>
-		{{ BootstrapForm::open(['route' => ['lender:groups:post-edit', $group->getId()], 'translationDomain' => 'lender.group', 'files' => true]) }}
-
-		{{ BootstrapForm::populate($form) }}
-
-		{{ BootstrapForm::text('name') }}
-
-		{{ BootstrapForm::text('website') }}
-
-		{{ BootstrapForm::file('groupProfilePictureId') }}
-		@if($group->getGroupProfilePicture())
-		    <img src="{{ $group->getGroupProfilePicture()->getImageUrl('small-profile-picture') }}" alt=""/>
-		@endif
-
-		{{ BootstrapForm::textarea('about') }}
-
-		{{ BootstrapForm::select('userId', $form->getMembers(), $group->getLeader()->getUser()->getId()) }}
-
-		{{ BootstrapForm::submit('save') }}
-
-		{{ BootstrapForm::close() }}
 	</div>
 </div>
 @stop
