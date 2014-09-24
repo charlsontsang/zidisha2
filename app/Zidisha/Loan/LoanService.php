2e7d802197ba7e9171fdfda8cc013ba276996da3
Jonas De Taeye <jonasdt@fastmail.fm>
2014-09-25 00:45:17 +0200

    Rename BorrowerComment::$isReschedulingReason to $rescheduleId

--- app/Zidisha/Loan/LoanService.php
+++ app/Zidisha/Loan/LoanService.php
@@ -1306,8 +1306,9 @@ class LoanService
         foreach ($repaymentScheduleInstallments['delete'] as $repaymentScheduleInstallment) {
             $deleteIds[] = $repaymentScheduleInstallment->getInstallment()->getId();
         }
-        
-        PropelDB::transaction(function($con) use ($loan, $data, $repaymentSchedule, $deleteIds) {
+
+        /** @var Reschedule $reschedule */
+        $reschedule = PropelDB::transaction(function($con) use ($loan, $data, $repaymentSchedule, $deleteIds) {
             $deletedInstallmentCount = InstallmentQuery::create()
                 ->filterById($deleteIds)
                 ->delete($con);
@@ -1332,6 +1333,8 @@ class LoanService
 
             $loan->clearInstallments();
             $loan->save($con);
+
+            return $reschedule;
         });
         
         $borrower = $loan->getBorrower();
@@ -1339,7 +1342,7 @@ class LoanService
         $this->borrowerCommentService->postComment(
             [
                 'message' => $data['reason'],
-                'isReschedulingReason' => true
+                'rescheduleId' => $reschedule->getId(),
             ],
             $borrower->getUser(),
             $borrower
