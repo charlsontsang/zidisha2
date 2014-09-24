111657da8f7af19ee81b56cc73dc276e6e7ba748
SingularityGuy <hardikformail@gmail.com>
2014-09-25 00:23:47 +0530

    add text on image

--- app/views/pages/404.blade.php
+++ app/views/pages/404.blade.php
@@ -2,6 +2,29 @@
 
 @section('content')
 <br/>
-<br/>
-<img src="{{ asset('assets/images/404-background.jpg'); }}" />
+<style type="text/css">
+.image {
+    position:relative;
+}
+.image .text {
+    position:absolute;
+    text-align: center;
+    top:40%;
+    left:25%;
+    width:500px;
+    color:#ffffff;
+    font-size: xx-large;
+}
+</style>
+<tr>
+<div class="image">
+  <img alt="" src="{{ asset('assets/images/404-background.jpg'); }}" />
+  <div class="text">
+    <p>Oops!</p>
+    <p>We couldn't find that page</p>
+    <p>ANYWHERE</p>
+  </div>
+</div>
+</tr>
 @stop
+
